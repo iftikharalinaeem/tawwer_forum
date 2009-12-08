d24d50a379d47048df5135692e755ea97c580c7c
mark <mark@vanillaforums.com>
2009-12-08 17:49:40 -0600

    Deleteforum typo

--- plugins/vfoptions/views/deleteforum.php
+++ plugins/vfoptions/views/deleteforum.php
@@ -9,7 +9,7 @@ echo $this->Form->Errors();
       <div class="Warning"><?php echo Gdn::Translate("Warning: All of your data will be lost. Once you delete, there is absolutely no recovery of any kind."); ?></div>
    </li>
    <li>
-      <div class="Info"><strong>Forum Name:</strong> <?php echo $this->Site->Name; ?>.vanillaforums.com</div>
+      <div class="Info"><strong>Forum Name:</strong> <?php echo $this->Site->Name; ?></div>
    </li>
 </ul>
 <?php echo $this->Form->Close('Delete Forum');
