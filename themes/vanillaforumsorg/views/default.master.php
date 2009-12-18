93b6c9bd8f525462abfe327167ecdaa8f47600a0
mark <mark@vanillaforums.com>
2009-12-17 18:32:03 -0600

    Working on the new vforg theme.

---
+++ themes/vanillaforumsorg/views/default.master.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-ca">
+<head>
+   <?php $this->RenderAsset('Head'); ?>
+</head>
+<body id="<?php echo $BodyIdentifier; ?>" class="<?php echo $this->CssClass; ?>">
+	<div class="Banner">
+		<div class="BannerWrapper">
+			<div class="UserOptions">
+				<div>
+					<?php
+						$Session = Gdn::Session();
+						$Authenticator = Gdn::Authenticator();
+						if ($Session->IsValid()) {
+							$Name = $Session->User->Name;
+							$CountNotifications = $Session->User->CountNotifications;
+							if (is_numeric($CountNotifications) && $CountNotifications > 0)
+								$Name .= '<span>'.$CountNotifications.'</span>';
+								
+							echo Anchor($Name, '/profile/'.$Session->UserID.'/'.$Session->User->Name, 'Username');
+	
+							$Inbox = 'Inbox';
+							$CountUnreadConversations = $Session->User->CountUnreadConversations;
+							if (is_numeric($CountUnreadConversations) && $CountUnreadConversations > 0)
+								$Inbox .= '<span>'.$CountUnreadConversations.'</span>';
+					
+							echo Anchor($Inbox, '/messages/all', 'Inbox');
+	
+							if ($Session->CheckPermission('Garden.Settings.Manage'))
+								echo Anchor('Dashboard', '/garden/settings', 'Dashboard');
+							
+							echo Anchor('Sign Out', $Authenticator->SignOutUrl(), 'Leave');
+						} else {
+							echo Anchor('Sign In', $Authenticator->SignInUrl($this->SelfUrl), 'SignIn Popup');
+							echo Anchor('Register', $Authenticator->RegisterUrl($this->SelfUrl), 'Register');
+						}
+					?>
+				</div>
+			</div>
+			<h1><a href="<?php echo Url('/'); ?>"><span>Vanilla</span></a></h1>
+			<ul>
+				<li class="Download"><?php echo Anchor('Download', '/download'); ?></li>
+				<li class="Hosting"><?php echo Anchor('Hosting', '/hosting'); ?></li>
+				<li class="Documentation"><?php echo Anchor('Documentation', '/docs'); ?></li>
+				<li class="Community"><?php echo Anchor('Community', '/discussions'); ?></li>
+				<li class="Addons"><?php echo Anchor('Addons', '/addons'); ?></li>
+				<li class="Blog"><?php echo Anchor('Blog', '/blog'); ?></li>
+				<li class="Home"><?php echo Anchor('Home', '/'); ?></li>
+			</ul>
+		</div>
+	</div>
+   <div id="Frame">
+      <div id="Body">
+         <div id="Content"><?php $this->RenderAsset('Content'); ?></div>
+         <div id="Panel"><?php $this->RenderAsset('Panel'); ?></div>
+      </div>
+      <div id="Foot">
+			<div><?php
+				printf(Gdn::Translate('Powered by %s'), '<a href="http://vanillaforums.org"><span>Vanilla</span></a>');
+			?></div>
+		</div>
+   </div>
+</body>
+</html>
