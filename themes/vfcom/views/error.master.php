761247f574220f04564d1bcc7b47101de9810b71
mark <mark@vanillaforums.com>
2009-12-21 09:31:13 -0600

    Added nice error screen for VFCom theme.

---
+++ themes/vfcom/views/error.master.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-ca">
+<head>
+   <title>Boing</title>
+   <?php
+   if ($CssPath !== FALSE)
+      echo '<link rel="stylesheet" type="text/css" href="',Asset($CssPath),'" />';
+   ?>
+</head>
+<body>
+   <div id="Content">
+      <div class="SplashInfo">
+         <h1>Boing</h1>
+         <p>Something funky happened. We've logged the issue and we will take care of it!</p>
+      </div>
+   </div>
+</body>
+</html>
