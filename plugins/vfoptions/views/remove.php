9b3c5066cb06d7a40840ea33ced3b1ab7d70378f
mark <mark@vanillaforums.com>
2010-01-05 14:38:32 -0600

    text

--- plugins/vfoptions/views/remove.php
+++ plugins/vfoptions/views/remove.php
@@ -2,7 +2,7 @@
 $About = ArrayValue(0, $this->RequestArgs, '');
 $Domain = str_replace(array('http://', '/'), array('', ''), Gdn::Config('Garden.Domain', 'your_forum_name.vanillaforums.com'));
 ?>
-<h1>Upgrade Removal</h1>
+<h1>Remove Upgrade</h1>
 <div class="Legal">
    <p>You will be charged for the remainder of your current billing cycle. Are you sure you want to remove this upgrade?</p>
 </div>
