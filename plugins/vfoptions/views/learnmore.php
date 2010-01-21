a6ccdec90faeeb7c6646f8d8e8e4ec35f7e15fe3
mark <mark@vanillaforums.com>
2010-01-20 19:00:54 -0600

    Added buy now button to custom css.

--- plugins/vfoptions/views/learnmore.php
+++ plugins/vfoptions/views/learnmore.php
@@ -51,14 +51,11 @@ if ($About == 'adremoval') {
    <div class="Legal">
       <p>The Custom CSS upgrade allows you to completely change the appearance of your forum using css code. You can change banners, colors, fonts, and the layout of your forum.</p>
       <p>You will be able to test changes before they are applied for everyone to see, and you can quickly and easily revert to previous revisions, or revert right back to our default style. <strong>Please note that the Custom CSS upgrade does not allow you to upload templates or custom themes.</strong></p>
-      <p><strong>Coming Soon!</strong></p>
    </div>
    <?php
-   /*
    echo $this->Form->Errors();
    echo $this->Form->Open();
-   echo $this->Form->Close('Get Started →');
-   */
+   echo $this->Form->Close('Buy Now →');
 } else if ($About == 'fileuploading') {
    ?>
    <div class="Legal">
