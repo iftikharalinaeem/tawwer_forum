e0c3a4c2b6b94117598b9fad8a2596046721f617
mark <mark@vanillaforums.com>
2010-01-25 12:34:15 -0600

    Find db's without a message table.

--- utils/message_cleanup.php
+++ utils/message_cleanup.php
@@ -7,16 +7,10 @@ while ($Row = mysql_fetch_assoc($Data)) {
 	 $DBName = $Row['DatabaseName'];
 	 $SiteID = $Row['SiteID'];
 	 mysql_select_db($DBName, $Cnn);
-	 $MData = mysql_query('select * from GDN_Message', $Cnn);
-	 if (mysql_num_rows($MData) > 0) {
-		  while ($M = mysql_fetch_array($MData)) {
-				if (substr($M['Content'], 0, 46) == "<p>Here's some stuff you might want to do:</p>") {
-					 echo 'DB: '.$DBName.'; MessageID: '.$M['MessageID']."\n";
-					 mysql_query('delete from GDN_Message where MessageID = '.$M['MessageID'], $Cnn);
-				}
-		  }
+	 $TableData = mysql_query("show tables like 'GDN_Message'", $Cnn);
+	 if (mysql_num_rows($TableData) == 0) {
+		  echo 'DB: '.$DBName."\n";
 	 }
-	 mysql_free_result($MData);
 }
-mysql_free_result($Data);
+mysql_free_result($TableData);
 mysql_close($Cnn);
