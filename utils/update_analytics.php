e9294782317d97856c3dd4bb3af3e326c938c3d9
mark <mark@vanillaforums.com>
2010-02-01 11:20:47 -0600

    added metalab design theme. added analytics conf updater.

---
+++ utils/update_analytics.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ Used for updating conf files in all forums during the update of 2010-01-20
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-12713112-1");
+pageTracker._setDomainName(".vanillaforums.com");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+*/
+
+if ($DirectoryHandle = opendir('/srv/www/vhosts')) {
+    while (($Item = readdir($DirectoryHandle)) !== FALSE) {
+		  $File = '/srv/www/vhosts/' . $Item . '/conf/config.php';
+		  if (file_exists($File)) {
+				$Contents = file_get_contents($File);
+				if (strpos($Contents, "\$Configuration['Plugins']['GoogleAnalytics']['TrackerCode'])") === 0) {
+					 echo 'Updating: '.$File."\n";
+					 break;
+
+	 				$Contents = str_replace(
+	 					 "// Plugins",
+	 					 "// Plugins
+\$Configuration['Plugins']['GoogleAnalytics']['TrackerCode'] = 'UA-12713112-1';                                                                                            
+\$Configuration['Plugins']['GoogleAnalytics']['TrackerDomain'] = '.vanillaforums.com';",
+	 					 $Contents
+	 				);
+	 				file_put_contents($File, $Contents);
+				}
+				
+		  }
+    }
+    closedir($DirectoryHandle);
+}
