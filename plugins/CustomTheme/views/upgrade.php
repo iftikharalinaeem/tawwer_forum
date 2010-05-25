41f216fb362ad891e9e081313f307e0592746b19
Mark O'Sullivan <mark@vanillaforums.com>
2010-05-25 10:42:38 -0400

    Added CustomTheme plugin.

---
+++ plugins/CustomTheme/views/upgrade.php
@@ -0,0 +1,8 @@
+<?php if (!defined('APPLICATION')) exit(); ?>
+<h1>Upgrade</h1>
+<div class="Legal">
+   <p>
+      You must upgrade to the <strong>Plus</strong> plan in order to apply custom themes.
+      Click your "Account" link on the top-right of the screen to upgrade now.
+   </p>
+</div>
