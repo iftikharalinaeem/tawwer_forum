da6b43c895e4b390b7340735dde256f9431c61bc
Tim Gunter <icyliquid@Vanillabook.local>
2010-05-26 12:47:05 -0400

    Links were not webroot aware

--- plugins/FileUpload/views/link_files.php
+++ plugins/FileUpload/views/link_files.php
@@ -6,7 +6,7 @@
       ?>
             <tr>
                <td><img src="<?php echo $this->GetWebResource('images/gear.png'); ?>"/></td>
-               <td><a href="/discussion/download/<?php echo $Media->MediaID;?>/<?php echo $Media->Name; ?>"><?php echo $Media->Name; ?></a></td>
+               <td><a href="<?php echo Url("/discussion/download/{$Media->MediaID}/{$Media->Name}"); ?>"><?php echo $Media->Name; ?></a></td>
                <td>(<?php echo Gdn_Format::Bytes2String($Media->Size, 0); ?>)</td>
             </tr>
       <?php
