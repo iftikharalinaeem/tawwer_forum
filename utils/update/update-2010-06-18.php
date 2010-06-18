b6a0ed203eb753d6139322e9ad85e65a7a179b34
root <root@hardy2.(none)>
2010-06-18 19:09:38 +0000

    Fixes

--- utils/update/update-2010-06-18.php
+++ utils/update/update-2010-06-18.php
@@ -1,4 +1,4 @@
-#!/usr/local/bin/php
+#!/usr/bin/php
 <?php
 
 require_once('foreach.php');
@@ -6,13 +6,12 @@ require_once('foreach.php');
 define("VERBOSE", TRUE);
 define("LAME", FALSE);
 
-define('DATABASE_HOST', 'localhost');
+define('DATABASE_HOST', 'vfdb1');
 define('DATABASE_USER', 'root');
-define('DATABASE_PASSWORD', 'okluijfire4');
-define('DATABASE_MAIN', 'vanilla.tim');
+define('DATABASE_PASSWORD', 'Va2aWu5A');
+define('DATABASE_MAIN', 'vfcom');
 
-$Tasks = new TaskList('launch','/www/vanilla/');
-$Tasks->RunSelectiveRegex('/^vanilla$/',array(
-   'uncache',
+$Tasks = new TaskList('launch','/srv/www/vhosts');
+$Tasks->RunAll(array(
    'repairprovider'
 ));
