ce07af4313c6f33f3523969f6bec45c052da43e9
Tim Gunter <tim@vanillaforums.com>
2010-07-04 19:23:11 -0400

    SourcecodePath Retargeter, some minor fixed

---
+++ utils/update/retarget.php
@@ -0,0 +1,18 @@
+#!/usr/bin/php
+<?php
+
+require_once('foreach.php');
+
+define("VERBOSE", TRUE);
+define("LAME", FALSE);
+
+define('DATABASE_HOST', 'vfdb1');
+define('DATABASE_USER', 'root');
+define('DATABASE_PASSWORD', 'Va2aWu5A');
+define('DATABASE_MAIN', 'vfcom');
+
+$Tasks = new TaskList('maintain','/srv/www/vhosts');
+$Tasks->RunClientFromCLI($argv[1], array(
+   'uncache',
+   'retarget'
+));
