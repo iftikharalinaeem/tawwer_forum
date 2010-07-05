c629c3a13bbd28c20b147cfeb51e6727347ff5b6
Tim Gunter <tim@vanillaforums.com>
2010-07-05 10:48:40 -0400

    check for enough args

--- utils/update/retarget.php
+++ utils/update/retarget.php
@@ -11,6 +11,8 @@ define('DATABASE_USER', 'root');
 define('DATABASE_PASSWORD', 'Va2aWu5A');
 define('DATABASE_MAIN', 'vfcom');
 
+if ($argc < 2) exit();
+
 $Tasks = new TaskList('maintain','/srv/www/vhosts');
 $Tasks->RunClientFromCLI($argv[1], array(
    'uncache',
