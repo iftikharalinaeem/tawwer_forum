f4c5e24adc26c90e2e367d71e3c8db8157bc3142
Tim Gunter <tim@vanillaforums.com>
2010-07-06 13:22:12 -0400

    Allow admins to edit users' sigs

--- plugins/Signatures/views/signature.php
+++ plugins/Signatures/views/signature.php
@@ -5,16 +5,23 @@ echo $this->Form->Open();
 echo $this->Form->Errors();
 ?>
 <ul>
+   <?php
+      if (isset($this->Data['Plugin-Signatures-ForceEditing']) && $this->Data['Plugin-Signatures-ForceEditing'] != FALSE) {
+   ?>
+         <div class="Warning"><?php echo sprintf(T("You are editing %s's signature"),$this->Data['Plugin-Signatures-ForceEditing']); ?></div>
+   <?php
+      }
+   ?>
    <li>
       <?php
-         echo $this->Form->Label('My Settings');
+         echo $this->Form->Label('Settings');
          echo $this->Form->CheckBox('Plugin.Signature.HideAll','Hide signatures?');
          echo $this->Form->CheckBox('Plugin.Signature.HideImages','Strip images out of signatures?');
       ?>
    </li>
    <li>
       <?php
-         echo $this->Form->Label('My Signature Code', 'Plugin.Signature.Sig');
+         echo $this->Form->Label('Signature Code', 'Plugin.Signature.Sig');
          echo $this->Form->TextBox('Plugin.Signature.Sig', array('MultiLine' => TRUE));
       ?>
    </li>
