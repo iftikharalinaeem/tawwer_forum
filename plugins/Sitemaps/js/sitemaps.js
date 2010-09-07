1e3af836f1ac4a09add7a791f4600292f24d37bd
Tim Gunter <tim@vanillaforums.com>
2010-09-07 11:24:45 -0400

    Fix references to gdn.definition('WebRoot')

--- plugins/Sitemaps/js/sitemaps.js
+++ plugins/Sitemaps/js/sitemaps.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-   var SitemapsBuildURL = gdn.definition('WebRoot')+'/plugin/sitemaps/build';
+   var SitemapsBuildURL = gdn.url('/plugin/sitemaps/build');
    jQuery.ajax({
       url: SitemapsBuildURL,
       type: 'POST'
