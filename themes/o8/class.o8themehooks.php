c375b2ea0c789b13d144dca5b8efacac6f73a7f7
Mark O'Sullivan <mark@vanillaforums.com>
2010-10-10 03:42:41 -0400

    Added o8 theme.

---
+++ themes/o8/class.o8themehooks.php
@@ -0,0 +1,29 @@
+<?php if (!defined('APPLICATION')) exit();
+class o8ThemeHooks implements Gdn_IPlugin {
+	
+   public function Setup() {
+		// Set the order for the modules (make sure new discussion module is before content).
+		SaveToConfig('Modules.Vanilla.Content', array('MessageModule', 'Notices', 'NewDiscussionModule', 'Content', 'Ads'));
+   }
+
+   public function OnDisable() {
+      return TRUE;
+   }
+   
+   public function CategoriesController_Render_Before($Sender) {
+		$Sender->AddModule('NewDiscussionModule', 'Content');
+   }
+   
+   public function DiscussionsController_Render_Before($Sender) {
+		$Sender->AddModule('NewDiscussionModule', 'Content');
+   }
+
+   public function DiscussionController_Render_Before($Sender) {
+		$Sender->AddModule('NewDiscussionModule', 'Content');
+   }
+
+   public function DraftsController_Render_Before($Sender) {
+		$Sender->AddModule('NewDiscussionModule', 'Content');
+   }
+   
+}
