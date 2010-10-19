1deb252d2668d922db7100e67197f149ebb0333a
Todd Burry <todd@vanillaforums.com>
2010-10-19 12:20:32 -0400

    Fixed some Facebook plugin bugs.

--- plugins/Facebook/class.facebook.plugin.php
+++ plugins/Facebook/class.facebook.plugin.php
@@ -10,8 +10,8 @@ Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
 
 // Define the plugin:
 $PluginInfo['Facebook'] = array(
-	'Name' => 'Facebook',
-   'Description' => 'This plugin integrates Vanilla with Facebook',
+	'Name' => 'Vanilla Facebook',
+   'Description' => 'This plugin integrates Vanilla with Facebook. This plugin requires you to register your application with Facebook and will not work until you have configured it properly.',
    'Version' => '0.1a',
    'RequiredApplications' => array('Vanilla' => '2.0.12a'),
    'RequiredTheme' => FALSE,
--- plugins/Facebook/views/settings.php
+++ plugins/Facebook/views/settings.php
@@ -8,7 +8,13 @@ echo $this->Form->Errors();
 <ul>
    <li>
       <?php
-         echo T('You have to set up a Facebook application to use this plugin.', 'You have to set up a Facebook application to use this plugin. You can set up an application at <a href="http://www.facebook.com/developers/apps.php">http://www.facebook.com/developers/apps.php</a>.');
+         echo '<p>', T('You have to set up a Facebook application to use this plugin.', 'You have to set up a Facebook application to use this plugin. You can set up an application at <a href="http://www.facebook.com/developers/apps.php">http://www.facebook.com/developers/apps.php</a>.'), '</p>';
+
+         echo '<p>', T('Make sure you enter the following information for your application.', 'When you register your application you can enter what you want for most fields, but make sure you enter the following information.'), '</p>';
+
+         echo '<p>';
+         echo '<b>Site URL</b>: ', rtrim(Gdn::Request()->Domain(), '/').'/';
+         echo '</p>';
       ?>
    </li>
    <li>
