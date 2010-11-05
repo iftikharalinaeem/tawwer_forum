dc999e07264973f4f45e3d3c0cb2371fc0e20c6e
Mark O'Sullivan <mark@vanillaforums.com>
2010-11-05 10:56:42 -0400

    CSS Cleanup on private community plugin.

--- plugins/PrivateCommunity/class.privatecommunity.plugin.php
+++ plugins/PrivateCommunity/class.privatecommunity.plugin.php
@@ -23,12 +23,13 @@ class PrivateCommunityPlugin extends Gdn_Plugin {
    public function RoleController_AfterRolesInfo_Handler($Sender) {
       $Private = C('Garden.PrivateCommunity');
       echo '<div style="padding: 10px 0;">';
+      $Style = array('style' => 'background: #ff0; padding: 2px 4px; margin: 0 10px 2px 0; display: inline-block;');
       if ($Private) {
-         echo Wrap('Your community is currently <strong>PRIVATE</strong>.', 'span', array('style' => 'background: #ff0; padding: 2px 4px;'));
-         echo Wrap(Anchor('Switch to PUBLIC', 'settings/privatecommunity/on/'.Gdn::Session()->TransientKey(), 'SmallButton').' - All visitors will be able to see the content in your community.', 'div');
+         echo Wrap('Your community is currently <strong>PRIVATE</strong>.', 'span', $Style);
+         echo Wrap(Anchor('Switch to PUBLIC', 'settings/privatecommunity/on/'.Gdn::Session()->TransientKey(), 'SmallButton').'(Everyone will see inside your community)', 'div');
       } else {
-         echo Wrap('Your community is currently <strong>PUBLIC</strong>.', 'span', array('style' => 'background: #ff0; padding: 2px 4px;'));
-         echo Wrap(Anchor('Switch to PRIVATE', 'settings/privatecommunity/off/'.Gdn::Session()->TransientKey(), 'SmallButton').' - Only signed-in users will be able to see the content in your community.', 'div');
+         echo Wrap('Your community is currently <strong>PUBLIC</strong>.', 'span', $Style);
+         echo Wrap(Anchor('Switch to PRIVATE', 'settings/privatecommunity/off/'.Gdn::Session()->TransientKey(), 'SmallButton').'(Only members will see inside your community)', 'div');
       }
       echo '</div>';
    }
