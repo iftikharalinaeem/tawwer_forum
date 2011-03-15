c94eb670b1ab53deacaa1dd54afd28d3237e1401
Mark O'Sullivan <mark@vanillaforums.com>
2011-03-15 17:34:55 -0400

    Infractions first commit.

---
+++ plugins/Infractions/views/aboutinfractions.php
@@ -0,0 +1,30 @@
+<?php if (!defined('APPLICATION')) exit(); ?>
+
+<h1>About Infractions</h1>
+
+<div class="Legal">
+   <p>
+      <strong>What is an Infraction</strong>
+      <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus tincidunt luctus. Etiam quis quam vulputate odio dapibus consequat. Proin luctus, velit vel posuere laoreet, purus ligula porta diam, aliquet fringilla magna turpis a ligula. Sed mattis vulputate arcu, nec tempus lorem adipiscing sed. Aliquam erat volutpat. Aliquam quis metus ullamcorper ligula fringilla posuere. Phasellus ultrices augue vel lacus tincidunt consequat.
+   </p>
+
+   <p>
+      <strong>Where can I review my infractions</strong>
+      <br />Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus elit risus, varius nec tempus ut, ullamcorper at velit. Aliquam vel massa massa, sed pharetra ligula. Suspendisse consequat, nulla quis eleifend blandit, odio tellus laoreet augue, et interdum est nisl nec ante. Aliquam a auctor est. Mauris sodales tincidunt sodales. Cras posuere rutrum tellus ullamcorper malesuada. Cras aliquet risus sed neque dignissim suscipit ac vel massa.
+   </p>
+   
+   <p>
+      <strong>How can I appeal an infraction?</strong>
+      <br />Proin sit amet sem sapien, nec dignissim massa. Vivamus ac nibh ac ante consequat lacinia in sit amet nibh.
+   </p>
+      
+   <p>
+      <strong>How do infractions affect my account?</strong>
+      <br />Nulla pretium posuere arcu. Donec et dapibus erat. Sed posuere gravida magna eget lacinia. Suspendisse sit amet ligula ac eros sollicitudin pellentesque vitae eget ipsum. Suspendisse fermentum dolor vitae ligula tincidunt euismod. Aenean id nulla eget mauris tempus imperdiet faucibus at lectus.
+   </p>
+   
+   <p>
+      <strong>When do infractions expire?</strong>
+      <br />Proin tincidunt imperdiet ipsum, vitae egestas mi ultricies a. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer velit metus, tincidunt in cursus rutrum, tincidunt in ligula. Quisque tincidunt aliquet metus non dignissim. Quisque rhoncus nulla vel augue euismod non sollicitudin urna mattis. Nam ipsum dolor, venenatis in sodales non, venenatis nec velit.
+   </p>
+</div>
---
+++ plugins/Infractions/views/infractionform.php
@@ -0,0 +1,219 @@
+<?php if (!defined('APPLICATION')) exit();
+/*
+   $UcContext = ucfirst($this->Data['Plugin.Flagging.Data']['Context']);
+   $ElementID = $this->Data['Plugin.Flagging.Data']['ElementID'];
+   $URL = $this->Data['Plugin.Flagging.Data']['URL'];
+   $Title = sprintf("Flag this %s",ucfirst($this->Data['Plugin.Flagging.Data']['Context']));
+*/
+?>
+<script type="text/javascript">
+   jQuery(document).ready(function($) {
+      setAutoBan = function() {
+         var showAutoBan = false;
+
+         // Don't do anything if the warning box is checked
+         if ($('#Form_Plugin-dot-Infraction-dot-Warning:checked').val() !== undefined) {
+            showAutoBan = false;
+         } else {
+            // Define the current # of points that the user has
+            var activePoints = $('ul.InfractionOverview li.Active strong').text()*1;
+            if (typeof(activePoints) != 'number')
+               activePoints = 0;
+            
+            // See what # of points has been assigned
+            var checkedRadio = $(':radio:checked');
+            var newPoints = 0;
+            if (checkedRadio.attr('id') == 'Form_Plugin-dot-Infraction-dot-Reason4') {
+               newPoints = $('#Form_Plugin-dot-Infraction-dot-Points').val()*1;
+            } else {
+               newPoints = checkedRadio.parents('ul.InfractionOptions li').find('span.Points').text()*1;
+            }
+            if (typeof(newPoints) != 'number')
+               newPoints = 0;
+               
+            if (newPoints + activePoints >= 6)
+               showAutoBan = true;
+            
+            $('div.AutoBan p').hide();
+            if (newPoints + activePoints >= 8) {
+               $('div.AutoBan p.PermaBan').show();
+            } else if (newPoints + activePoints >= 6) {
+               $('div.AutoBan p.TempBan').show();
+            }
+         }
+         var autoBan = $('div.AutoBan');
+         if (showAutoBan && !autoBan.is(':visible'))
+            autoBan.slideDown('fast');
+         else if (!showAutoBan && autoBan.is(':visible'))
+            autoBan.slideUp('fast');
+         else if (!showAutoBan)
+            autoBan.hide();
+      }
+
+      // Based on changes to the form, display/hide autoban form.
+      setAutoBan();
+      $(':radio').change(function() { setAutoBan(); });
+      $('#Form_Plugin-dot-Infraction-dot-Warning').change(function() {
+         setAutoBan();
+         // Style the points if this is a warning
+         if ($(this).is(':checked'))
+            $('ul.InfractionOptions li span.Points, ul.InfractionOptions li span.Points input').css('color', '#ddd');
+         else
+            $('ul.InfractionOptions li span.Points, ul.InfractionOptions li span.Points input').css('color', '#000');
+         
+      });
+      $('#Form_Plugin-dot-Infraction-dot-Points').blur(function() { setAutoBan(); });
+      
+   });
+</script>
+<style type="text/css">
+   div.Popup form ul.InfractionOptions li label,
+   div.Popup form ul li label.CheckBoxLabel {
+      font-size: 12px;
+      margin: 0;
+      padding: 4px 0;
+   }
+   div.Popup form ul li label.CheckBoxLabel {
+      margin: 6px 0;
+      border: 2px dashed #ddd;
+      border-radius: 6px;
+      -moz-border-radius: 6px;
+      -webkit-border-radius: 6px;
+      background: #ffd;
+   }
+   li.Headings span {
+      font-weight: bold;
+      font-size: 14px;
+   }
+   span.Reason,
+   span.Points,
+   span.Expires {
+      font-size: 12px;
+      display: inline-block;
+      width: 70px;
+      text-align: center;
+   }
+   span.Reason {
+      text-align: left;
+      width: 260px;
+   }
+   span.Expires {
+      width: auto;
+      text-align: left;
+   }
+   input.TinyInput {
+      width: 20px;
+   }
+   span.Points input.TinyInput {
+      text-align: center;
+   }
+   input.CustomInput {
+      width: 220px;
+   }
+   ul.InfractionOptions li:nth-child(even) {
+      background: #eee;
+   }
+   ul.InfractionOptions select {
+      font-size: 12px;
+   }
+   div.InfractionPopup h3 {
+      background: #EEEEEE;
+      border-top: 1px solid #888888;
+      border-bottom: 0;
+      margin-top: 16px;
+      padding: 2px 4px;
+   }
+   #Popup textarea.TextBox {
+      width: 98%;
+   }
+   input.NoteInput,
+   input.BanReasonInput {
+      width: 98%;
+   }
+</style>
+<h3>New Infraction</h3>
+<ul>
+   <li>
+      <?php
+         echo $this->Form->Checkbox('Plugin.Infraction.Warning', 'Make this a warning (no points assigned)');
+      ?>
+   </li>
+</ul>
+<ul class="InfractionOptions">
+   <li class="Headings">
+      <span class="Reason">Reason</span>
+      <span class="Points">Points</span>
+      <span class="Expires">Expires</span>
+   </li>
+   <li>
+      <span class="Reason"><?php echo $this->Form->Radio('Plugin.Infraction.Reason', 'Minor Offense', array('value' => 'Minor Offense')); ?></span>
+      <span class="Points">2</span>
+      <span class="Expires">30 days - <?php echo Gdn_Format::Date(strtotime('+ 30 days')); ?></span>
+   </li>
+   <li>
+      <span class="Reason"><?php echo $this->Form->Radio('Plugin.Infraction.Reason', 'Serious Offense', array('value' => 'Serious Offense')); ?></span>
+      <span class="Points">3</span>
+      <span class="Expires">60 days - <?php echo Gdn_Format::Date(strtotime('+ 60 days')); ?></span>
+   </li>
+   <li>
+      <span class="Reason"><?php echo $this->Form->Radio('Plugin.Infraction.Reason', 'Alternate Account', array('value' => 'Alternate Account')); ?></span>
+      <span class="Points">8</span>
+      <span class="Expires">Never</span>
+   </li>
+   <li>
+      <span class="Reason"><?php echo $this->Form->Radio('Plugin.Infraction.Reason', 'Spamming', array('value' => 'Spamming')); ?></span>
+      <span class="Points">8</span>
+      <span class="Expires">Never</span>
+   </li>
+   <?php if (Gdn::Session()->CheckPermission('Garden.Admin.Only')) { ?>
+   <li>
+      <span class="Reason">
+         <?php echo $this->Form->Radio('Plugin.Infraction.Reason', '', array('value' => 'Custom')); ?>
+         <?php echo $this->Form->Input('Plugin.Infraction.CustomReason', 'text', array('class' => 'CustomInput')); ?>
+      </span>
+      <span class="Points"><?php echo $this->Form->Input('Plugin.Infraction.Points', 'text', array('class' => 'TinyInput')); ?></span>
+      <span class="Expires">
+         <?php echo $this->Form->Input('Plugin.Infraction.Expires', 'text', array('class' => 'TinyInput')); ?>
+         <?php echo $this->Form->DropDown('Plugin.Infraction.ExpiresRange', array(
+            'Hours' => 'Hours',
+            'Days' => 'Days',
+            'Weeks' => 'Weeks',
+            'Months' => 'Months',
+            'Never' => 'Never'
+         )); ?>
+      </span>
+   </li>
+   <?php } ?>
+</ul>
+<?php
+// Will this infraction cause the user to be auto-banned?
+?>
+<div class="AutoBan">
+   <h3>Automatic Ban</h3>
+   <p class="TempBan">Assigning this infraction will cause the user to be automatically banned for 2 weeks.</p>
+   <p class="PermaBan">Assigning this infraction will cause the user to be banned permanently.</p>
+   <ul>
+      <li>
+         <?php
+            echo $this->Form->Label('Provide a reason for banishment', 'Plugin.Infraction.BanReason');
+            echo $this->Form->Input('Plugin.Infraction.BanReason', 'text', array('class' => 'BanReasonInput'));
+         ?>
+      </li>
+   </ul>
+</div>
+<h3>Message To User</h3>
+<ul>
+   <li>
+      <?php
+         echo $this->Form->Label('Administrative Note', 'Plugin.Infraction.Note');
+         echo $this->Form->Input('Plugin.Infraction.Note', 'text', array('class' => 'NoteInput'));
+      ?>
+   </li>
+   <li>
+      <?php
+         echo $this->Form->Label('Private Message To User', 'Plugin.Infraction.Message');
+         echo $this->Form->TextBox('Plugin.Infraction.Message', array('Multiline' => TRUE));
+      ?>
+   </li>
+</ul>
+<?php echo $this->Form->Button('Give Infraction');
