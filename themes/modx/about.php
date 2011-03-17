a71d3e4e9451d33fb41e5b13a3e51c24e38478ce
Mark O'Sullivan <mark@vanillaforums.com>
2011-03-17 13:03:24 -0400

    Added modx theme.

---
+++ themes/modx/about.php
@@ -0,0 +1,10 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$ThemeInfo['modx'] = array(
+   'Name' => 'Mod X',
+   'Description' => "Custom theme for modx.com.",
+   'Version' => '1.0',
+   'Author' => "Mark O'Sullivan",
+   'AuthorEmail' => 'mark@vanillaforums.com',
+   'AuthorUrl' => 'http://markosullivan.ca'
+);
