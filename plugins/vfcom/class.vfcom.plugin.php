77319ffe2fded441e415a64fda7c174068463807
Tim Gunter <tim@vanillaforums.com>
2011-03-18 17:06:40 -0400

    Change output to include frontend server name

--- plugins/vfcom/class.vfcom.plugin.php
+++ plugins/vfcom/class.vfcom.plugin.php
@@ -50,8 +50,9 @@ class VfcomPlugin extends Gdn_Plugin {
       
       if (stristr(Gdn::Session()->User->Email, 'vanillaforums.com') && Gdn::Session()->User->Admin) {
          echo '<div style="text-align:left;margin:10px;padding:5px;font-size:14px;background-color:white;color:gray;">';
-         echo "<div>Frontend: ".GetValue('HTTP_X_HANDLING_SERVER', $_SERVER, 'unknown')."</div>";
-         echo "<div>".print_r($_SERVER,true)."</div>";
+         echo "<div>Upstream: ".GetValue('HTTP_X_UPSTREAM', $_SERVER, 'unknown')."</div>";
+         echo "<div>Frontend: ".C('VanillaForums.Frontend', 'unknown')."</div>";
+         //echo "<div>".print_r($_SERVER,true)."</div>";
          echo '</div>';
       }
    }
