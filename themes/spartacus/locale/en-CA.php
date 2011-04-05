f93e945d675b50b265794a030f79b229af393f44
Todd Burry <todd@vanillaforums.com>
2011-04-05 12:08:12 -0400

    Added custom locale settings for spartacus.

---
+++ themes/spartacus/locale/en-CA.php
@@ -0,0 +1,3 @@
+<?php if (!defined('APPLICATION')) exit();
+$Definition['Welcome, Stranger!'] = 'Greetings, lanista!';
+$Definition['GuestModule.Message'] = 'You are unknown to this ludus. Please introduce yourself.';
