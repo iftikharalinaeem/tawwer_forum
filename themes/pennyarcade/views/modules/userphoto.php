73d02f1e39a3791218f96d283b90b2987f820a6e
Mark O'Sullivan <mark@vanillaforums.com>
2011-04-19 09:22:22 -0400

    Penny Arcade theme.

--- themes/pennyarcade/views/modules/userphoto.php
+++ themes/pennyarcade/views/modules/userphoto.php
@@ -4,15 +4,15 @@ $RoleCss = strtolower(implode(' role-', $this->_Sender->Roles));
 if ($RoleCss != '')
    $RoleCss = ' role-'.$RoleCss;
 
-
 $Photo = $this->User->Photo != '' ? Gdn_Upload::Url(ChangeBasename($this->User->Photo, 'p%s')) : 'themes/pennyarcade/design/images/profile_user.png';
+$Jailed = $this->User->Jailed == '1' ? Img('themes/pennyarcade/design/images/jailed-180.png', array('alt' => 'Jailed', 'class' => 'Jailed')) : '';
 ?>
 <div class="Box PhotoBox">
    <h4>Profile</h4>
    <div class="ProfileUser<?php echo $RoleCss; ?>">
       <?php
       echo Wrap($this->User->Name, 'div', array('class' => 'Name'));
-      echo Wrap('<span class="Rank"></span>'.Img($Photo), 'div', array('class' => 'Photo'));
+      echo Wrap('<span class="Rank"></span>'.$Jailed.Img($Photo), 'div', array('class' => 'Photo'));
       if ($this->User->About != '') {
          echo '<div class="Status" id="Status">';
          echo Gdn_Format::Display($this->User->About);
