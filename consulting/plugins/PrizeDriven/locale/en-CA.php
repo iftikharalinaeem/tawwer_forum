38e46f54fcd63f69aee2b9ad222c6fbc1e4bc7ba
Todd Burry <todd@vanillaforums.com>
2011-04-27 10:26:18 -0400

    PrizeDriven plugin.

---
+++ consulting/plugins/PrizeDriven/locale/en-CA.php
@@ -0,0 +1,25 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$Definition['Discussions'] = 'Competitions';
+$Definition['Discussion'] = 'Competition';
+$Definition['Start a New Discussion'] = 'Start a New Competion';
+$Definition['Discussion Title'] = 'Competition Name';
+$Definition['All Discussions'] = 'All Competitions';
+$Definition['My Discussions'] = 'My Competitions';
+$Definition['Back to Discussions'] = 'Back to Competitions';
+$Definition['bookmarked discussion'] = 'bookmarked competition';
+$Definition['Bookmarked Discussions'] = 'Bookmarked Competitions';
+$Definition['Delete Discussion'] = 'Delete Competition';
+$Definition['discussion'] = 'competition';
+$Definition['Discussion Title'] = 'Competition Title';
+$Definition['Discussions'] = 'Competitions';
+$Definition['My Discussions'] = 'My Competitions';
+$Definition['No discussions were found.'] = 'No competitions were found.';
+
+$Definition['Notify me when people comment on my bookmarked discussions.'] = 'Notify me when people comment on my bookmarked competitions.';
+$Definition['Notify me when people comment on my discussions.'] = 'Notify me when people comment on my competitions.';
+$Definition['Notify me when people mention me in discussion titles.'] = 'Notify me when people mention me in competition titles.';
+$Definition['Post Discussion'] = 'Post Competition';
+$Definition['&larr; All Discussions'] = '&larr; All Competitions';
+$Definition['Edit Discussion'] = 'Edit Competition';
+$Definition['In this Discussion'] = 'In this Competition';
