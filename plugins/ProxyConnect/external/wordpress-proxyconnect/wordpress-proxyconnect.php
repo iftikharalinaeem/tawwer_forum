691c9f8b3cd9d8f4b3e5085aa6884c90cc47a052
Tim Gunter <tim@vanillaforums.com>
2011-05-27 01:00:25 -0400

    Update filenames for new WP plugin format

--- plugins/ProxyConnect/external/wordpress-proxyconnect/wordpress-proxyconnect.php
+++ plugins/ProxyConnect/external/wordpress-proxyconnect/wordpress-proxyconnect.php
@@ -1,20 +1,18 @@
 <?php
 /*
-Plugin Name: Vanilla ProxyConnect for WP2
-Plugin URI: http://vanillaforums.org/addons/
-Description: Vanilla ProxyConnect allows users to create and manage their accounts & sessions through Wordpress, and be automatically signed into a related Vanilla forum.
-Version: 1.2.0
-Author: Tim Gunter
-Author URI: http://www.vanillaforums.com/
-*/
-
-/*
-Copyright 2008, 2009 Vanilla Forums Inc.
-This file is part of the Vanilla ProxyConnect plugin for WordPress 2.9.
-The Vanilla ProxyConnect plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-The Vanilla ProxyConnect plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with the Vanilla ProxyConnect plugin.  If not, see <http://www.gnu.org/licenses/>.
-Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
+   Plugin Name: Vanilla ProxyConnect for WP2
+   Plugin URI: http://vanillaforums.org/addons/
+   Description: Vanilla ProxyConnect allows users to create and manage their accounts & sessions through Wordpress, and be automatically signed into a related Vanilla forum.
+   Version: 1.2.0
+   Author: Tim Gunter
+   Author URI: http://www.vanillaforums.com/
+   
+   # Copyright 2008, 2009 Vanilla Forums Inc.
+   # This file is part of the Vanilla ProxyConnect plugin for WordPress 2.9.
+   # The Vanilla ProxyConnect plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+   # The Vanilla ProxyConnect plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+   # You should have received a copy of the GNU General Public License along with the Vanilla ProxyConnect plugin.  If not, see <http://www.gnu.org/licenses/>.
+   # Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
 */
 
 define('VANILLA_COOKIE', 'Vanilla'); // <-- You might need to change this if you've customized your cookie name in vanilla.
