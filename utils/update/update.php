abfada3a798dfb27271ebb18f7f1c3f414352ab2
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-06-06 13:28:20 -0400

    Allows updates to be run on single or multiple forums

--- utils/update/update.php
+++ utils/update/update.php
@@ -12,9 +12,15 @@ require_once("includes/runner.php");
 $Tasks = new TaskList();
 $Tasks->Clients('/srv/www/vhosts');
 $Tasks->Perform(TaskList::ACTION_CACHE);
-$Tasks->Perform(TaskList::ACTION_TARGET);
 
-$Tasks->Run(TaskList::MODE_TARGET, array(
+if ($RunForAll) {
+   $RunMode = TaskList::MODE_TARGET;
+} else {
+   $RunMode = TaskList::MODE_TARGET;
+   $Tasks->Perform(TaskList::ACTION_TARGET);
+}
+
+$Tasks->Run($RunMode, array(
    'global/backup',
    'global/offline',
    'global/uncache',
