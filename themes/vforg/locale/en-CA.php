45a3954cdd0718de2cbad0191e2f1e008398bb0f
Todd Burry <todd@vanillaforums.com>
2011-06-09 23:41:01 -0400

    Getting vforg theme ready for Q&A and no votes.

---
+++ themes/vforg/locale/en-CA.php
@@ -0,0 +1,3 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$Definition['Unanswered Questions'] = 'Unanswered';
--- themes/vforg/views/post/discussion.php
+++ themes/vforg/views/post/discussion.php
@@ -1,82 +1,71 @@
 <?php if (!defined('APPLICATION')) exit();
 $Session = Gdn::Session();
 $CancelUrl = '/vanilla/discussions';
-//if (C('Vanilla.Categories.Use') && $this->CategoryID > 0 && $this->CategoryData->NumRows() > 0) {
-//   foreach ($this->CategoryData->Result() as $Cat) {
-//      if ($Cat->CategoryID == $this->CategoryID) {
-//         $CancelUrl = '/vanilla/discussions/'.$Cat->Code;
-//         break;
-//      }
-//   }
-//}
+if (C('Vanilla.Categories.Use') && is_object($this->Category))
+   $CancelUrl = '/vanilla/discussions/0/'.$this->Category->CategoryID.'/'.Gdn_Format::Url($this->Category->Name);
+
 ?>
 <div id="DiscussionForm">
-   <h1><?php echo property_exists($this, 'Discussion') ? 'Edit Question' : 'Ask a Question'; ?></h1>
+   <h1><?php echo $this->Data('Title'); ?></h1>
    <?php
       echo $this->Form->Open();
-
+      echo $this->Form->Errors();
       $this->FireEvent('BeforeFormInputs');
 
-      echo $this->Form->Errors();
-   ?>
-   <div class="PostHelp">
-      <div class="Help HelpTitle">
-         <h4>How to Ask</h4>
-         <ul>
-            <li>We prefer questions that can be <em>answered</em>, not just discussed.</li>
-            <li>Provide details.</li>
-            <li>Write clearly and descriptively.</li>
-         </ul>
-         <div class="Examples">
-            <div class="Example Bad"><strong>Bad:</strong> HELP!!!!!!</div>
-            <div class="Example Good"><strong>Good:</strong> Error during install process: "no input file specified"</div>
-         </div>
-      </div>
-      <div class="Help HelpFormat">
-         <h4>How to Format</h4>
-         <ul>
-            <li>HTML is allowed</li>
-            <li>Place code samples in &lt;code&gt; tags</li>
-            <li>Provide details like:
-               <ul>
-                  <li>Exact error messages</li>
-                  <li>Browser & version</li>
-                  <li>PHP version</li>
-                  <li>MySQL version</li>
-               </ul>
-            </li>
-         </ul>
-      </div>
-      <div class="Help HelpTags">
-         <h4>How to Tag</h4>
-         <p>A tag is a keyword that categorizes your question with similar questions.</p>
-         <ul>
-            <li>When possible, use existing tags</li>
-            <li>Favour popular tags</li>
-            <li>Use common abbreviations</li>
-            <li>Don't include synonyms</li>
-            <li>Combine multiple words into a single word with dashes</li>
-            <li>Max 5 tags</li>
-            <li>Max 25 characters per tag</li>
-            <li>Tag characters: a-z 0-9 + # _ .</li>
-            <li>Separate tags with a space</li>
-         </ul>
-      </div>
-   </div>
-   <?php
-      echo $this->Form->Label('Question Title', 'Name');
-      echo $this->Form->TextBox('Name', array('maxlength' => 100));
+      echo '<div class="FormInputs">';
 
-      echo $this->Form->Label('Ask your question', 'Body');
-      echo $this->Form->TextBox('Body', array('MultiLine' => TRUE));
-      $this->FireEvent('BeforeFormButtons');
-      echo $this->Form->Button((property_exists($this, 'Discussion')) ? 'Save' : 'Ask Question');
-      if (!property_exists($this, 'Discussion') || !is_object($this->Discussion) || (property_exists($this, 'Draft') && is_object($this->Draft))) {
-         echo $this->Form->Button('Save Draft');
+      include dirname(__FILE__).'/formhelp.php';
+
+		echo '<div class="P">';
+			echo $this->Form->Label('Discussion Title', 'Name');
+			echo Wrap($this->Form->TextBox('Name', array('maxlength' => 100, 'class' => 'InputBox BigInput')), 'div', array('class' => 'TextBoxWrapper'));
+		echo '</div>';
+
+      if ($this->ShowCategorySelector === TRUE) {
+			echo '<div class="P">';
+				echo '<div class="Category">';
+				echo $this->Form->Label('Category', 'CategoryID'), ' ';
+				echo $this->Form->DropDown('CategoryID', $this->CategoryData, array('TextField' => 'Name', 'ValueField' => 'CategoryID'));
+				echo '</div>';
+			echo '</div>';
       }
-      echo $this->Form->Button('Preview');
+
+      $this->FireEvent('BeforeBodyInput');
+		echo '<div class="P">';
+	      echo Wrap($this->Form->TextBox('Body', array('MultiLine' => TRUE)), 'div', array('class' => 'TextBoxWrapper'));
+		echo '</div>';
+
+      $Options = '';
+      // If the user has any of the following permissions (regardless of junction), show the options
+      // Note: I need to validate that they have permission in the specified category on the back-end
+      // TODO: hide these boxes depending on which category is selected in the dropdown above.
+      if ($Session->CheckPermission('Vanilla.Discussions.Announce'))
+         $Options .= '<li>'.$this->Form->CheckBox('Announce', T('Announce'), array('value' => '1')).'</li>';
+
+      if ($Session->CheckPermission('Vanilla.Discussions.Close'))
+         $Options .= '<li>'.$this->Form->CheckBox('Closed', T('Close'), array('value' => '1')).'</li>';
+
+		$this->EventArguments['Options'] = &$Options;
+		$this->FireEvent('DiscussionFormOptions');
+
+      if ($Options != '') {
+			echo '<div class="P">';
+	         echo '<ul class="List Inline PostOptions">' . $Options .'</ul>';
+			echo '</div>';
+      }
+
+      echo '<div class="Buttons">';
+      $this->FireEvent('BeforeFormButtons');
+      echo $this->Form->Button((property_exists($this, 'Discussion')) ? 'Save' : 'Post Discussion', array('class' => 'Button DiscussionButton'));
+      if (!property_exists($this, 'Discussion') || !is_object($this->Discussion) || (property_exists($this, 'Draft') && is_object($this->Draft))) {
+         echo $this->Form->Button('Save Draft', array('class' => 'Button DraftButton'));
+      }
+      echo $this->Form->Button('Preview', array('class' => 'Button PreviewButton'));
       $this->FireEvent('AfterFormButtons');
-      echo Anchor(Gdn::Translate('Cancel'), $CancelUrl, 'Cancel');
+      echo Anchor(T('Cancel'), $CancelUrl, 'Cancel');
+      echo '</div>';
+
+      echo '<div>';
       echo $this->Form->Close();
    ?>
 </div>
