46904b6b0b3129599d37b1bce5e39825713e978a
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-06-13 13:46:42 -0400

    Pusher updates

--- infrastructure/push/functions.php
+++ infrastructure/push/functions.php
@@ -1,1 +1,35 @@
 <?php
+
+/*
+Copyright 2011, Tim Gunter
+This file is part of Push.
+Push is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Push is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Push.  If not, see <http://www.gnu.org/licenses/>.
+Contact Tim Gunter at tim [at] vanillaforums [dot] com
+*/
+
+function GetValue($Key, &$Collection, $Default = FALSE, $Remove = FALSE) {
+	$Result = $Default;
+	if(is_array($Collection) && array_key_exists($Key, $Collection)) {
+		$Result = $Collection[$Key];
+      if($Remove)
+         unset($Collection[$Key]);
+	} elseif(is_object($Collection) && property_exists($Collection, $Key)) {
+		$Result = $Collection->$Key;
+      if($Remove)
+         unset($Collection->$Key);
+   }
+		
+   return $Result;
+}
+
+function RandomString($Length, $Characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') {
+   $CharLen = strlen($Characters) - 1;
+   $String = '' ;
+   for ($i = 0; $i < $Length; ++$i) {
+     $Offset = rand() % $CharLen;
+     $String .= substr($Characters, $Offset, 1);
+   }
+   return $String;
+}
