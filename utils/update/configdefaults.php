ee47f117c39555842f1e3d98dca265839ddf1882
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-06-15 17:35:48 -0400

    Add runner for config defaults

---
+++ utils/update/configdefaults.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+
+require_once("includes/runner.php");
+
+/**
+ * Set up tasklist object
+ *  - Open configuration files
+ *  - Connect to database
+ *
+ */
+$Tasks = new TaskList();
+$Tasks->Clients('/srv/www/vhosts');
+$Tasks->Perform(TaskList::ACTION_CACHE);
+
+$RunForAll = $Tasks->GetConsoleOption('all', FALSE);
+if ($RunForAll) {
+   $RunMode = TaskList::MODE_CHUNKED;
+} else {
+   $RunMode = TaskList::MODE_TARGET;
+   $Tasks->Perform(TaskList::ACTION_TARGET);
+}
+
+$Tasks->Run($RunMode, array(
+   'maintain/config'
+));
