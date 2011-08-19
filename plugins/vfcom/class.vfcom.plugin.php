9d0d7912de4487f185d6072afec807f79c44c623
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-08-19 14:28:24 -0400

    Update VFCom plugin comments

--- plugins/vfcom/class.vfcom.plugin.php
+++ plugins/vfcom/class.vfcom.plugin.php
@@ -1,18 +1,30 @@
 <?php if (!defined('APPLICATION')) exit();
-/*
-Copyright 2008, 2009 Vanilla Forums Inc.
-This file is part of Garden.
-Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
-Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
-*/
+
+/**
+ * Infrastructure Hosting
+ * 
+ * This plugin provides the hooks and management tools needed to run Vanilla in 
+ * Infrastructure Mode.
+ * 
+ *  Autostatic CDN
+ *  Domain Switching
+ *  Custom Routes (static)
+ *  Config Load/Save Hooks
+ *  Upload Hooks
+ *  Infrastructure Control Panel
+ * 
+ * @author Tim Gunter <tim@vanillaforums.com>
+ * @copyright 2003 Vanilla Forums, Inc
+ * @license Prorietary
+ * @package Api
+ * @since 1.0
+ */
 
 // Define the plugin:
 $PluginInfo['vfcom'] = array(
    'Name' => 'Infrastructure Hosting',
-   'Description' => "This plugin provides the hooks and management tools need to run Vanilla in Infrastructure Mode.",
-   'Version' => '1.3',
+   'Description' => "This plugin provides the hooks and management tools needed to run Vanilla in Infrastructure Mode.",
+   'Version' => '1.4',
    'MobileFriendly' => TRUE,
    'RequiredApplications' => FALSE,
    'RequiredTheme' => FALSE, 
