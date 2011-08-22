0ec72d508c49467636d9b8e1e0b3f0cc6e4dc476
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-08-22 16:44:32 -0400

    Fix naming of access token header.

--- plugins/vfcom/class.vfcom.plugin.php
+++ plugins/vfcom/class.vfcom.plugin.php
@@ -48,8 +48,8 @@ class VfcomPlugin extends Gdn_Plugin {
    
    public function __construct() {
       // Get (and protect) and Infrastructure API token
-      $this->DataAPIToken = GetValue('X_FRONTEND_TOKEN', $_SERVER, NULL);
-      unset($_SERVER['X_FRONTEND_TOKEN']);
+      $this->DataAPIToken = GetValue('HTTP_X_FRONTEND_TOKEN', $_SERVER, NULL);
+      unset($_SERVER['HTTP_X_FRONTEND_TOKEN']);
       
       $ClientParts = explode('.', CLIENT_NAME);
       // Name of this client (usually the <prefix> part of http://<prefix>.<hostname>.com)
