76f41e7cd95a18d9c68d0723a13a8dd756a2b71d
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2011-10-19 08:19:55 -0400

    This is useless garbage

--- plugins/downtime/view.php
+++
@@ -1,4 +0,0 @@
-<div class="Info">
-   <div style="color: #444; font-size: 110%; font-weight: bold;">Scheduled Maintenance</div>
-   We're currently making some changes to this site. Please check back later!
-</div>
