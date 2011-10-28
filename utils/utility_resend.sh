ae3ff4ea609e68f569aecab2112283747bd01b33
Todd Burry <todd@vanillaforums.com>
2011-10-27 20:16:24 -0400

    Remove colouring from utility_resend.sh.

--- utils/utility_resend.sh
+++ utils/utility_resend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /srv/clients
-find *.vanillaforums.com -maxdepth 0 -exec echo -n -e "\033[35m{}:\033[30m " ';' -exec curl -L http://{}/utility/resendemails ';' -exec echo '' ';'
+find *.vanillaforums.com -maxdepth 0 -exec echo -n -e "{}: " ';' -exec curl -L http://{}/utility/resendemails ';' -exec echo '' ';'
