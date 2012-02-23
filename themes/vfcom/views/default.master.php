a94d76c9b77ebf1b8e2d7b0943542716375a3506
Mark O'Sullivan <mark@vanillaforums.com>
2012-02-23 11:14:49 -0500

    vfcom design overhaul

--- themes/vfcom/views/default.master.php
+++ themes/vfcom/views/default.master.php
@@ -9,78 +9,114 @@ $Session = Gdn::Session();
    <meta name="google-site-verification" content="T7dDWEaTeqt989RCxDJTfoOkbOADnRWLLJTauXxMHVA" />
 </head>
 <body id="<?php echo $BodyIdentifier; ?>" class="<?php echo $this->CssClass; ?>">
-<div id="Head" class="Wrapper">
-   <div class="Center">
-      <div class="Logo">
-         <a href="/"><i class="Sprite SpriteLogo"><span>Vanilla Forums</span></i></a>
-      </div>
-      <div class="VFMenu">
-			<div class="Home"><?php echo Anchor('<i class="Sprite SpriteHome"></i> Home', '/', '', array('SSL' => FALSE)); ?></div>
-			<div class="Plans"><?php echo Anchor('<i class="Sprite SpritePlans"></i> Plans &amp; Pricing', '/plans', '', array('SSL' => FALSE)); ?></div>
-			<div class="Solutions"><?php echo Anchor('<i class="Sprite SpriteSolutions"></i> Solutions', '/solutions', '', array('SSL' => FALSE)); ?></div>
-			<div class="Features"><?php echo Anchor('<i class="Sprite SpriteFeatures"></i> Features', '/features', '', array('SSL' => FALSE)); ?></div>
-			<div class="Blog"><?php echo Anchor('<i class="Sprite SpriteBlog"></i> Blog', '/blog', '', array('SSL' => FALSE)); ?></div>
-			<div class="<?php echo $Session->IsValid() ? 'Account' : 'SignIn'; ?>"><?php
-			$Text = 'Sign In';
-			$Link = SignInUrl();
-			if ($Session->IsValid()) {
-				$Text = 'Sign Out';
-				$Link = SignOutUrl();
-				
-				$Text = 'Account';
-				$Link = 'account';
-			}
-			echo Anchor('<i class="Sprite SpriteSignIn"></i> '.$Text, $Link, '', array('SSL' => TRUE)); ?></div>
-      </div>
-	</div>
-	<div class="Divider"></div>
-	<div class="SubNav Wrapper">
-		<div class="Center"><?php
-			echo Anchor('All Support Discussions', '/discussions', 'Home');
-			echo '&nbsp;';
-			echo Gdn_Theme::Link('dashboard');
-			if ($Session->IsValid())
-				echo Anchor('Account', '/account', 'Account');
-				
-			echo Gdn_Theme::Link('profile', 'Profile', '<a href="%url" class="Profile">Profile</a>');
-			if ($Session->IsValid())
-				echo Anchor('Sign Out', SignOutUrl(), 'SignOut');
-		?></div>
+<div class="Head">
+	<div class="Wrapper">
+      <div class="InnerWrapper">
+         <div class="Center">
+            <h1 class="Logo">
+               <a title="Discussion Forums Evolved, VanillaForums.com" href="/">Discussion Forums Evolved, VanillaForums.com</a>
+            </h1>
+            <div class="Menus">
+               <div class="AccountMenu">
+                  <?php
+                  if ($Session->IsValid()) {
+                     echo Gdn_Theme::Link('dashboard');
+                     echo Anchor('Support', '/help', 'Support');
+                     // Show account link if user has an account
+                     $Session->User->CountNotifications = 12;
+                     if (isset($Session->User->AccountID) && is_numeric($Session->User->AccountID) && $Session->User->AccountID > 0)
+                        echo Anchor('Account', '/account', 'Account');
+
+                     echo Gdn_Theme::Link('profile', 'Profile', '<a href="%url" class="Profile">%text</a>');
+                     echo Anchor('Sign Out', SignOutUrl(), 'SignOut', array('SSL' => TRUE));
+                  } else {
+                     echo Anchor('Sign In', SignInUrl(), 'SignIn', array('SSL' => TRUE));
+                  }
+                  ?>
+               </div>
+               <div class="VFMenu">
+                  <?php
+                  echo Anchor(Sprite('SpHome').'Home', '/', 'Home', array('SSL' => FALSE));
+                  echo Anchor(Sprite('SpPlans').'Plans &amp; Pricing', '/plans', 'Plans', array('SSL' => FALSE));
+                  echo Anchor(Sprite('SpFeatures').'Features', '/features', 'Features', array('SSL' => FALSE));
+                  echo Anchor(Sprite('SpResources').'Resources', '/resources', 'Resources', array('SSL' => FALSE));
+                  echo Anchor(Sprite('SpBlog').'Blog', '/blog', 'Blog', array('SSL' => FALSE));
+                  ?>
+               </div>
+            </div>
+            <?php if (!$Session->IsValid()) echo Anchor('Sign Up', 'plans', 'GreenButton SignUpButton'); ?>
+         </div>
+		</div>
 	</div>
 </div>
-
-   <div id="Frame">
-      <div id="Body">
-         <div id="Content"><?php
-			if (in_array(strtolower($this->ControllerName), array('discussionscontroller', 'categoriescontroller'))) {
-				echo '<div class="SearchForm">';
-				$Form = Gdn::Factory('Form');
-				$Form->InputPrefix = '';
-				echo 
-					$Form->Open(array('action' => Url('/search'), 'method' => 'get')),
-					$Form->TextBox('Search'),
-					$Form->Button('Search', array('Name' => '')),
-					$Form->Close()
-					.'</div>';
-			}
-			$this->RenderAsset('Content');
-			?></div>
-         <div id="Panel"><?php $this->RenderAsset('Panel'); ?></div>
+<div class="Divider"></div>
+<div class="Body">
+	<div class="Wrapper">
+      <div class="InnerWrapper">
+         <div class="Center">
+            <div id="Panel"><?php $this->RenderAsset('Panel'); ?></div>
+            <div id="Content"><?php
+            if (in_array(strtolower($this->ControllerName), array('discussionscontroller', 'categoriescontroller'))) {
+               echo '<div class="SearchForm">';
+               $Form = Gdn::Factory('Form');
+               $Form->InputPrefix = '';
+               echo 
+                  $Form->Open(array('action' => Url('/search'), 'method' => 'get')),
+                  $Form->TextBox('Search'),
+                  $Form->Button('Search', array('Name' => '')),
+                  $Form->Close()
+                  .'</div>';
+            }
+            $this->RenderAsset('Content');
+            ?></div>
+         </div>
       </div>
    </div>
-
-<div id="Foot" class="Foot Wrapper">
-	<div class="Center">
-      <?php
-      echo Anchor('Partner/Referral Program', '/info/partnerprogram', '', array('SSL' => FALSE));
-      echo Anchor('Terms of Service', '/info/termsofservice', '', array('SSL' => FALSE));
-      echo Anchor('Privacy Policy', '/info/privacy', '', array('SSL' => FALSE));
-      echo Anchor('Refund Policy', '/info/refund', '', array('SSL' => FALSE));
-      echo Anchor("We're Hiring!", '/info/hiring', '', array('SSL' => FALSE));
-      echo Anchor('About Us', '/info/faq', '', array('SSL' => FALSE));
-      echo Anchor('Contact Us', '/info/contact', '', array('SSL' => FALSE));
-		$this->RenderAsset('Foot');
-      ?>
+</div>
+<div class="Foot">
+	<div class="Wrapper">
+		<div class="Center">
+         <div class="Columns">
+            <div class="Column4">
+               <strong>About Us</strong>
+               <?php
+               echo Anchor('Contact Us', '/info/contact', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor("We're Hiring!", '/info/hiring', '', array('SSL' => FALSE));
+					echo '<br />'.Anchor('Press Releases', 'http://vanillaforums.totemapp.com/');
+               echo '<br />'.Anchor('Follow us on Facebook', 'http://www.facebook.com/vanillaforums', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Follow us on Twitter', 'http://www.twitter.com/vanilla', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Read Our Blog', '/blog', '', array('SSL' => FALSE));
+               ?>
+            </div>
+            <div class="Column4">
+               <strong>Features</strong>
+               <?php
+               echo Anchor('Features', '/features', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Support Communities', '/features/support-communities', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Migrate Legacy Forum', '/features/legacy-migration', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Comments Integration', '/features/blog-comments', '', array('SSL' => FALSE));
+               ?>
+            </div>
+            <div class="Column4">
+               <strong>Resources</strong>
+               <?php
+               echo Anchor('Resources', '/resources', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Case Studies', '/case-studies', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Professional Services', '/professional-services', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Free Version', '/free-version', '', array('SSL' => FALSE));
+               ?>
+            </div>
+				<div class="Column4">
+					<strong>Legal Stuff</strong>
+					<?php
+               echo Anchor('Privacy Policy', '/info/privacy', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Terms of Service', '/info/termsofservice', '', array('SSL' => FALSE));
+               echo '<br />'.Anchor('Refund Policy', '/info/refund', '', array('SSL' => FALSE));
+					?>
+				</div>
+         </div>
+         <?php $this->RenderAsset('Foot'); ?>
+		</div>
 	</div>
 </div>
 <?php $this->FireEvent('AfterBody'); ?>
@@ -90,6 +126,6 @@ document.write(unescape("%3Cscript src='" + ((document.location.protocol=="https
 SnapABug.setButton("http://vanillaforums.com/applications/vfcom/design/images/help-tab.png");
 SnapABug.addButton("34737bd0-1d78-43ac-be67-b2769cb5f6ae","0","30%");
 </script>
-		*/ ?>
+*/ ?>
 </body>
 </html>
