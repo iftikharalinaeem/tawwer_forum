828935ec356d81a2c897fbead569c45adc199ece
Mark O'Sullivan <mark@vanillaforums.com>
2012-02-28 16:24:06 -0500

    Changing to less ugly layout on bestof

--- plugins/Reactions/views/bestof.php
+++ plugins/Reactions/views/bestof.php
@@ -11,22 +11,15 @@ function ReactionFilterButton($Name, $Code, $CurrentReactionType) {
     .'<span class="ReactLabel">'.$Name.'</span>'
     .'</a>';
 }   
-?>
-<div class="Tabs BestOfTabs">
-   <ul>
-      <li class="Active"><?php echo Anchor(T('Best Of...'), '/bestof', 'TabLink'); ?></li>
-   </ul>
-   <div class="SubTab ReactionFilters">
-      <?php
-      echo ReactionFilterButton('Everything', 'everything', $CurrentReactionType);
-      $ReactionTypeData = $this->Data('ReactionTypeData');
-      foreach ($ReactionTypeData as $Key => $ReactionType) {
-         // decho($ReactionType);
-         echo ReactionFilterButton(GetValue('Name', $ReactionType, ''), GetValue('UrlCode', $ReactionType, ''), $CurrentReactionType);
-      }
-      ?>
-   </div>
-</div>
-<div class="BestOfData">
-<?php include_once('datalist.php'); ?>
-</div>
+echo Wrap($this->Data('Title'), 'h1');
+echo '<div class="ReactionFilters">';
+   echo ReactionFilterButton('Everything', 'everything', $CurrentReactionType);
+   $ReactionTypeData = $this->Data('ReactionTypeData');
+   foreach ($ReactionTypeData as $Key => $ReactionType) {
+      // decho($ReactionType);
+      echo ReactionFilterButton(GetValue('Name', $ReactionType, ''), GetValue('UrlCode', $ReactionType, ''), $CurrentReactionType);
+   }
+echo '</div>
+<div class="BestOfData">';
+include_once('datalist.php');
+echo '</div>';
