b13d20dd4f68b003455b4c242f9aeef6732601c4
Todd Burry <todd@vanillaforums.com>
2012-02-28 22:49:17 -0500

    Added the insightful reaction.

--- plugins/Reactions/structure.php
+++ plugins/Reactions/structure.php
@@ -16,13 +16,13 @@ $St
    ->Column('TagID', 'int')
    ->Column('Attributes', 'text', TRUE)
    ->Column('Sort', 'smallint', TRUE)
-   ->Column('Active', 'tinyint(1)', 1)
+   ->Column('Active', 'tinyint(1)', 0)
    ->Set();
 
 $St->Table('UserTag')
    ->Column('RecordType', array('Discussion', 'Discussion-Total', 'Comment', 'Comment-Total', 'User', 'User-Total', 'Activity', 'Activity-Total', 'ActivityComment', 'ActivityComment-Total'), FALSE, 'primary')
    ->Column('RecordID', 'int', FALSE, 'primary')
-   ->Column('TagID', 'int', FALSE, 'primary')
+   ->Column('TagID', 'int', FALSE, array('primary', 'key'))
    ->Column('UserID', 'int', FALSE, array('primary', 'key'))
    ->Column('DateInserted', 'datetime')
    ->Column('Total', 'int', 0)
@@ -43,37 +43,53 @@ $Rm->DefineReactionType(array('UrlCode' => 'Promote', 'Name' => 'Promote', 'Sort
 
 $Rm->DefineReactionType(array('UrlCode' => 'OffTopic', 'Name' => 'Off Topic', 'Sort' => 1, 'Class' => 'Bad', 'IncrementColumn' => 'Score', 'IncrementValue' => -1, 'Points' => -1,
    'Description' => "Off topic posts are not releveant to the topic being discussed. If a post gets enough off-topic votes then it will be buried so it won't derail the discussion."));
+$Rm->DefineReactionType(array('UrlCode' => 'Insightful', 'Name' => 'Insightful', 'Sort' => 2, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
+   'Description' => "Insightful comments bring new information or perspective to the discussion and increase the value of the conversation as a whole."));
 
-$Rm->DefineReactionType(array('UrlCode' => 'Disagree', 'Name' => 'Disagree', 'Sort' => 2, 'Class' => 'Bad', 
+$Rm->DefineReactionType(array('UrlCode' => 'Disagree', 'Name' => 'Disagree', 'Sort' => 3, 'Class' => 'Bad', 
    'Description' => "Users that disagree with a post can give their opinion with this reaction. Since a disagreement is highly subjective, this reaction desn't promote or bury the post or give any points."));
-$Rm->DefineReactionType(array('UrlCode' => 'Agree', 'Name' => 'Agree', 'Sort' => 3, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
+$Rm->DefineReactionType(array('UrlCode' => 'Agree', 'Name' => 'Agree', 'Sort' => 4, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
    'Description' => "Users that agree with a post can give their option with this reaction."));
 
-$Rm->DefineReactionType(array('UrlCode' => 'Dislike', 'Name' => 'Dislike', 'Sort' => 4, 'Class' => 'Bad', 'IncrementColumn' => 'Score', 'IncrementValue' => -1, 'Points' => -1,
+$Rm->DefineReactionType(array('UrlCode' => 'Dislike', 'Name' => 'Dislike', 'Sort' => 5, 'Class' => 'Bad', 'IncrementColumn' => 'Score', 'IncrementValue' => -1, 'Points' => -1,
    'Description' => "A dislike is a general disapproval of a post. Enough dislikes will bury a post."));
-$Rm->DefineReactionType(array('UrlCode' => 'Like', 'Name' => 'Like', 'Sort' => 5, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
+$Rm->DefineReactionType(array('UrlCode' => 'Like', 'Name' => 'Like', 'Sort' => 6, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
    'Description' => "A like is a general approval of a post. Enough likes will promote a post."));
 
-$Rm->DefineReactionType(array('UrlCode' => 'Down', 'Name' => 'Vote Down', 'Sort' => 6, 'Class' => 'Bad', 'IncrementColumn' => 'Score', 'IncrementValue' => -1, 'Points' => -1,
+$Rm->DefineReactionType(array('UrlCode' => 'Down', 'Name' => 'Vote Down', 'Sort' => 7, 'Class' => 'Bad', 'IncrementColumn' => 'Score', 'IncrementValue' => -1, 'Points' => -1,
    'Description' => "A down vote is a general disapproval of a post. Enough dislikes will bury a post."));
-$Rm->DefineReactionType(array('UrlCode' => 'Up', 'Name' => 'Vote Up', 'Sort' => 7, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
+$Rm->DefineReactionType(array('UrlCode' => 'Up', 'Name' => 'Vote Up', 'Sort' => 8, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
    'Description' => "An up vote is a general disapproval of a post. Enough dislikes will bury a post."));
 
-$Rm->DefineReactionType(array('UrlCode' => 'WTF', 'Name' => 'WTF', 'Sort' => 8, 'Class' => 'Bad', 'IncrementColumn' => 'Score', 'IncrementValue' => -1, 'Points' => -1,
+$Rm->DefineReactionType(array('UrlCode' => 'WTF', 'Name' => 'WTF', 'Sort' => 9, 'Class' => 'Bad', 'IncrementColumn' => 'Score', 'IncrementValue' => -1, 'Points' => -1,
    'Description' => 'WTF stands for "What the F___?" You usually react this way when a post makes absolutely no sense.'));
-$Rm->DefineReactionType(array('UrlCode' => 'Awesome', 'Name' => 'Awesome', 'Sort' => 9, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
+$Rm->DefineReactionType(array('UrlCode' => 'Awesome', 'Name' => 'Awesome', 'Sort' => 10, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
    'Description' => 'Awesome posts amaze you. You want to repeat them to your friends and remember them later.'));
-$Rm->DefineReactionType(array('UrlCode' => 'LOL', 'Name' => 'LOL', 'Sort' => 10, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
+$Rm->DefineReactionType(array('UrlCode' => 'LOL', 'Name' => 'LOL', 'Sort' => 11, 'Class' => 'Good', 'IncrementColumn' => 'Score', 'Points' => 1,
    'Description' => 'For posts that make you "laugh out loud." Funny content is almost always good and is rewarded with points and promotion.'));
 
-
+if (!$ReactionTypeExists) {
+   // Activate the default reactions.
+   $Defaults = array('Spam', 'Abuse', 'Troll', 'Promote', 'OffTopic', 'Disagree', 'Agree', 'Like');
+   $Sql->Update('ReactionType')
+      ->Set('Active', 1)
+      ->WhereIn('UrlCode', $Defaults)
+      ->Put();
+   Gdn::Cache()->Remove('ReactionTypes');
+}
 
 if (class_exists('BadgeModel')) {
    // Define some badges for the reactions.
    $BadgeModel = new BadgeModel();
 
-   $Reactions = array('Agree' => 'Agrees', 'Like' => 'Likes', 'Up' => 'Up Votes', 'Awesome' => 'Awesomes', 'LOL' => 'LOLs'); 
+   $Reactions = array('Insightful' => 'Insightfuls', 'Agree' => 'Agrees', 'Like' => 'Likes', 'Up' => 'Up Votes', 'Awesome' => 'Awesomes', 'LOL' => 'LOLs', 'WTF' => 'WTFs'); 
    $Thresholds = array(1 => 5, 2 => 25, 3 => 100, 4 => 250, 5 => 500);
+   $Sentences = array(
+       1 => "We like that.",
+       2 => "You're posting some good content. Great!",
+       3 => "When you're liked this much, you'll be an MVP in no time!",
+       4 => "Looks like you're popular around these parts.",
+       5 => "It ain't no fluke, you post great stuff and we're lucky to have you here.");
 
    foreach ($Reactions as $Class => $NameSuffix) {
       $ClassSlug = strtolower($Class);
@@ -81,18 +97,21 @@ if (class_exists('BadgeModel')) {
          $Points = round($Threshold / 10);
          if ($Points < 10)
             $Points = 10;
+         
+         $Sentence = $Sentences[$Level];
 
          //foreach ($Likes as $Count => $Body) {
          $BadgeModel->Define(array(
              'Name' => "$Threshold $NameSuffix",
              'Slug' => "$ClassSlug-$Threshold",
              'Type' => 'Reaction',
-             'Body' => '',
+             'Body' => "You received $Threshold $NameSuffix. $Sentence",
              'Photo' => "http://badges.vni.la/100/$ClassSlug-$Level.png",
              'Points' => $Points,
              'Threshold' => $Threshold,
              'Class' => $Class,
-             'Level' => $Level
+             'Level' => $Level,
+             'CanDelete' => 0
          ));
       }
    }
