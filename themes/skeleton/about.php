3c76f2c0d7bcd063967b3e296c9cac854e99ee65
lincolnwebs <lincolnwebs@gmail.com>
2012-03-20 09:16:19 -0400

    Skeleton theme

---
+++ themes/skeleton/about.php
@@ -0,0 +1,13 @@
+<?php if (!defined('APPLICATION')) exit();
+/**
+ * @copyright 2012 Vanilla Forums Inc.
+ */
+$ThemeInfo['skeleton'] = array(
+   'Name' => '',
+   'Site' => '.vanillaforums.com',
+   'Description' => "Custom theme for ",
+   'Version' => '1.0',
+   'Author' => "",
+   'AuthorEmail' => '@vanillaforums.com',
+   'AuthorUrl' => 'http://'
+);
---
+++ themes/skeleton/class.skeletonthemehooks.php
@@ -0,0 +1,7 @@
+<?php if (!defined('APPLICATION')) exit();
+/**
+ * 
+ */
+class SkeletonThemeHooks implements Gdn_IPlugin {
+   public function Setup() {   }	
+}
