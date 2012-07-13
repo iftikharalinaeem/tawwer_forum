1f2b4c08515848245d975f69e72706d4ba99596b
Mark O'Sullivan <mark@vanillaforums.com>
2012-07-13 16:09:38 -0400

    Adding VanillaBulletin theme

---
+++ themes/vanillabulletin/about.php
@@ -0,0 +1,10 @@
+<?php if (!defined('APPLICATION')) exit();
+$ThemeInfo['vanillabulletin'] = array(
+   'Name' => 'Vanilla Bulletin',
+   'Description' => "Vanilla, all dressed up like vBulletin.",
+   'Version' => '1.0',
+   'Author' => "Todd Burry (and Mark O'Sullivan)",
+   'AuthorEmail' => 'todd@vanillaforums.com',
+   'AuthorUrl' => 'http://vanillaforums.com',
+   'Site' => 'iphonedevsdk.vanillaforums.com'
+);
