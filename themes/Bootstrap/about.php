87600cb5c885d1aff88170d21cfbb56942dd94bc
Todd Burry <todd@vanillaforums.com>
2012-07-28 13:32:46 -0400

    Bootstrap theme 1.0b.

---
+++ themes/Bootstrap/about.php
@@ -0,0 +1,27 @@
+<?php if (!defined('APPLICATION')) exit();
+/*
+Copyright 2008, 2009 Vanilla Forums Inc.
+This file is part of Garden.
+Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
+Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
+*/
+
+/**
+ * An associative array of information about this application.
+ */
+$ThemeInfo['Bootstrap'] = array(
+   'Name' => 'Bootstrap',
+   'Description' => "A theme based on the popular Twitter Bootstrap framework.",
+   'Version' => '1.0b',
+   'Author' => "Todd Burry",
+   'AuthorEmail' => 'todd@vanillaforums.com',
+   'AuthorUrl' => 'http://vanillaforums.org/profile/todd',
+   'Options' => array(
+      'Description' => 'This theme has an alternative colour scheme.',
+      'Styles' => array(
+         'Dark' => '%s_dark'
+   ),
+)
+);
