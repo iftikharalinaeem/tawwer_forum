4a84be299067c4c97786c414f843dbc14dcf1a12
Todd Burry <todd@vanillaforums.com>
2012-08-23 11:36:15 -0400

    Block a few path in the sitemaps' robots.txt.

--- plugins/Sitemaps/views/robots.php
+++ plugins/Sitemaps/views/robots.php
@@ -4,4 +4,7 @@ echo 'Sitemap: '.Url('/sitemapindex.xml', TRUE)."\n";
 
 User-agent: *
 Disallow: /entry/
+Disallow: /messages/
+Disallow: /profile/comments/
+Disallow: /profile/discussions/
 Disallow: /search/
