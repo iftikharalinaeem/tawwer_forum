ba31d6923e4771c61e508cf5e999f3072a66deb1
Mark O'Sullivan <mark@vanillaforums.com>
2012-08-25 09:55:07 -0400

    Rolling back reactions best of page to original version, maintaining new version at bestof2.

--- plugins/Reactions/views/bestof.php
+++ plugins/Reactions/views/bestof.php
@@ -1,10 +1,46 @@
-<?php if (!defined('APPLICATION')) exit(); 
+<?php if (!defined('APPLICATION')) exit(); ?>
+<style>
+   .CountItemWrap {
+      width: <?php echo round(100 / (2 + count($this->Data('ReactionTypes', array())))).'%'; ?>
+   }
+</style>
+
+<?php
 include_once 'reaction_functions.php';
-echo Wrap($this->Data('Title'), 'h1 class="H"'); 
-?>
-<div class="BestOfData">
-   <ul class="DataList BestOfList">
-      <?php include_once('datalist.php'); ?>
-   </ul>
-   <?php echo PagerModule::Write(); ?>
+
+function ReactionFilterButton($Name, $Code, $CurrentReactionType) {
+   $LCode = strtolower($Code);
+   $Url = Url("/bestof/$LCode");
+   $ImgSrc = "http://badges.vni.la/reactions/50/$LCode.png";
+   $CssClass = '';
+   if ($CurrentReactionType == $LCode)
+      $CssClass .= ' Selected';
+   
+   $Result = <<<EOT
+<div class="CountItemWrap">
+<div class="CountItem$CssClass">
+   <a href="$Url">
+      <span class="CountTotal"><img src="$ImgSrc" /></span>
+      <span class="CountLabel">$Name</span>
+   </a>
 </div>
+</div>
+EOT;
+   
+   return $Result;
+}
+
+echo Wrap($this->Data('Title'), 'h1 class="H"');
+
+echo '<div class="DataCounts">';
+   $CurrentReactionType = $this->Data('CurrentReaction');
+   echo ReactionFilterButton(T('Everything'), 'Everything', $CurrentReactionType);
+   $ReactionTypeData = $this->Data('ReactionTypes');
+   foreach ($ReactionTypeData as $Key => $ReactionType) {
+      echo ReactionFilterButton(T(GetValue('Name', $ReactionType, '')), GetValue('UrlCode', $ReactionType, ''), $CurrentReactionType);
+   }
+echo '</div>
+   
+<div class="BestOfData">';
+include_once('datalist.php');
+echo '</div>';
