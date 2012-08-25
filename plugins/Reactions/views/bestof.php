6273c42bb83bf33ad4b7957a4896b845d9407a27
Mark O'Sullivan <mark@vanillaforums.com>
2012-08-25 08:58:00 -0400

    Moved new bestof to bestof2 method. Updated bestof2 css to be targeted to new method only.

--- plugins/Reactions/views/bestof.php
+++ plugins/Reactions/views/bestof.php
@@ -1,50 +1,10 @@
-<?php if (!defined('APPLICATION')) exit(); ?>
-<script type="text/javascript">
-$(function () {
-   var selector = '.BestOfList > .Item', $container = $('.BestOfList');
-   var $items = $('.BestOfList .Item:not(.Wide)');
-   if ($items.length == 0) $items = $('.BestOfList .Item');
-   var width = $items.outerWidth() + 20;
-   $('.Pager').hide();
-
-   $(selector).css({opacity: 0});
-   $container.imagesLoaded(function() {
-      $(selector).animate({ opacity: 1 });
-      $container.masonry({
-         itemSelector: selector,
-         columnWidth: width
-      });
-   });
-   
-   $container.infinitescroll({
-      navSelector  : '.Pager',
-      nextSelector : '.Pager .Next',
-      itemSelector : selector,
-      loading: {
-         finished: function() {
-            // $('.LoadingMore').hide();
-         },
-         selector: '.LoadingMore',
-          finishedMsg: '&nbsp;',
-          msgText: '&nbsp;',
-          img: 'http://cdn.vanillaforums.com/images/progress.gif'
-        },
-      pixelsFromNavToBottom: 800
-   }, function(newElements) {
-        var $newElems = $(newElements).css({ opacity: 0 });
-        $newElems.imagesLoaded(function(){
-          $newElems.animate({ opacity: 1 });
-          $container.masonry( 'appended', $newElems, true ); 
-        });
-      }
-   );   
-});
-</script>
-<?php echo Wrap($this->Data('Title'), 'h1 class="H"'); ?>
+<?php if (!defined('APPLICATION')) exit(); 
+include_once 'reaction_functions.php';
+echo Wrap($this->Data('Title'), 'h1 class="H"'); 
+?>
 <div class="BestOfData">
-   <div class="DataList BestOfList">
-      <?php include_once('bestoflist.php'); ?>
-   </div>
-   <?php echo PagerModule::Write(array('MoreCode' => 'Load More')); ?>
-   <div class="LoadingMore"></div>
+   <ul class="DataList BestOfList">
+      <?php include_once('datalist.php'); ?>
+   </ul>
+   <?php echo PagerModule::Write(); ?>
 </div>
