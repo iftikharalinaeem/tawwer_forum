53e3c77423a33b97c2d5f8b19f0d85d3381fa53d
Todd Burry <todd@vanillaforums.com>
2012-09-10 02:54:59 -0400

    Reactions support for social sharing.

--- plugins/Reactions/views/reaction_functions.php
+++ plugins/Reactions/views/reaction_functions.php
@@ -192,6 +192,23 @@ function WriteReactions($Row) {
       SetValue('Attributes', $Row, $Attributes);
    }
    
+   static $Types = NULL;
+   if ($Types === NULL)
+      $Types = ReactionModel::GetReactionTypes(array('Class' => array('Good', 'Bad'), 'Active' => 1));
+   Gdn::Controller()->EventArguments['ReactionTypes'] = $Types;
+   
+   if ($ID = GetValue('CommentID', $Row)) {
+      $RecordType = 'comment';
+   } elseif ($ID = GetValue('ActivityID', $Row)) {
+      $RecordType = 'activity';
+   } else {
+      $RecordType = 'discussion';
+      $ID = GetValue('DiscussionID', $Row);
+   }
+   Gdn::Controller()->EventArguments['RecordType'] = $RecordType;
+   Gdn::Controller()->EventArguments['RecordID'] = $ID;
+   
+   
    if (C('Plugins.Reactions.ShowUserReactions', TRUE))
       WriteRecordReactions($Row);
    
@@ -218,12 +235,13 @@ function WriteReactions($Row) {
             }
             echo '</ul>';
          echo '</span>';
+         
+         Gdn::Controller()->FireEvent('AfterFlag');
+         
          echo Bullet();
       }
 
-      static $Types = NULL;
-      if ($Types === NULL)
-         $Types = ReactionModel::GetReactionTypes(array('Class' => array('Good', 'Bad'), 'Active' => 1));
+      
 
       // Write the reactions.
       echo '<span class="ReactMenu">';
@@ -234,7 +252,7 @@ function WriteReactions($Row) {
          echo '</span>';
       echo '</span>';
 
-      Gdn::Controller()->EventArguments['ReactionTypes'] = $Types;
+      
       Gdn::Controller()->FireEvent('AfterReactions');
    
    echo '</div>';
