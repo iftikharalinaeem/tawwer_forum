f49e6d8c17a9b98828f284b1de8102ece828cced
Tim 'Thunderpuncher' Gunter <tim@vanillaforums.com>
2012-10-11 15:50:46 -0400

    change package.

--- quickapi/library/class.controller.php
+++ quickapi/library/class.controller.php
@@ -6,7 +6,7 @@
  * @author Tim Gunter <tim@vanillaforums.com>
  * @copyright 2003 Vanilla Forums, Inc
  * @license http://www.opensource.org/licenses/gpl-2.0.php GPLv2
- * @package Garden
+ * @package quickapi
  * @since 1.0
  */
 
