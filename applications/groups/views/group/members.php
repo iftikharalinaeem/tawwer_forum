ff39c32cd41ec0d6041deab1fd2f047a7a63abf6
lincolnwebs <lincolnwebs@gmail.com>
2013-02-03 17:25:44 -0500

    Groups - more home stuff, WIP

--- applications/groups/views/group/members.php
+++ applications/groups/views/group/members.php
@@ -1,6 +1,9 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
 
+<div class="Group-Leaders">
+   <h2><?php echo T('GroupLeaders', 'Leaders'); ?></h2>
+   <?php WriteMemberCards($this->Data('Leaders')); ?>
+</div>
 
-<?php WriteMemberCards($this->Data('Leaders'));
-
-WriteMemberCards($this->Data('Members'));
+<h2><?php echo T('GroupMembers', 'Members'); ?></h2>
+<?php WriteMemberCards($this->Data('Members')); ?>
