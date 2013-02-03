7cb375e7fefff3df6ba9b9a10034b6b205d8ea23
Todd Burry <todd@vanillaforums.com>
2013-02-03 14:12:48 -0500

    Groups application: - Added GroupModel->CheckPermission(). - Added GroupPermission() for views. - Modified GroupUrl() to allow for methods.

--- applications/groups/models/class.groupmodel.php
+++ applications/groups/models/class.groupmodel.php
@@ -10,6 +10,108 @@ class GroupModel extends Gdn_Model {
       parent::__construct('Group');
    }
    
+   /**
+    * Check permission on a group.
+    * 
+    * @param string $Permission The permission to check. Valid values are:
+    *  - Member: User is a member of the group.
+    *  - Leader: User is a leader of the group.
+    *  - Edit: The user may edit the group.
+    *  - View: The user may view the group's contents.
+    *  - Moderate: The user may moderate the group.
+    * @param int $GroupID
+    * @return boolean
+    */
+   public function CheckPermission($Permission, $GroupID) {
+      static $Permissions = array();
+      
+      $UserID = Gdn::Session()->UserID;
+      
+      if (!isset($Permissions[$UserID])) {
+         // Get the data for the group.
+         if (is_array($GroupID)) {
+            $Group = $GroupID;
+            $GroupID = $Group['GroupID'];
+         } else
+            $Group = $this->GetID($GroupID);
+         $UserGroup = Gdn::SQL()->GetWhere('UserGroup', array('GroupID' => $GroupID, 'UserID' => Gdn::Session()->UserID))->FirstRow(DATASET_TYPE_ARRAY);
+         $GroupApplicant = Gdn::SQL()->GetWhere('GroupApplicant', array('GroupID' => $GroupID, 'UserID' => Gdn::Session()->UserID))->FirstRow(DATASET_TYPE_ARRAY);
+         
+         
+         // Set the default permissions.
+         $Perms = array(
+            'Member' => FALSE,
+            'Leader' => FALSE,
+            'Join' => Gdn::Session()->IsValid(),
+            'Leave' => FALSE,
+            'Edit' => FALSE,
+            'Delete' => FALSE,
+            'Moderate' => FALSE,
+            'View' => $Group['Visibility'] == 'Public');
+         
+         // The group creator is always a member and leader.
+         if ($UserID == $Group['InsertUserID']) {
+            $Perms['Delete'] = TRUE;
+            
+            if (!$UserGroup)
+               $UserGroup = array('Role' => 'Leader');
+         }
+            
+         if ($UserGroup) {
+            $Perms['Join'] = FALSE;
+            $Perms['Join.Reason'] = T('You are already a member of this group.');
+            
+            $Perms['Member'] = TRUE;
+            $Perms['Leader'] = ($UserGroup['Role'] == 'Leader');
+            $Perms['Edit'] = $Perms['Leader'];
+            $Perms['Moderate'] = $Perms['Leader'];
+            
+            if ($UserID != $Group['InsertUserID']) {
+               $Perms['Leave'] = TRUE;
+            } else {
+               $Perms['Leave.Reason'] = T("You can't leave the group you started.");
+            }
+         }
+         
+         if ($GroupApplicant) {
+            $Perms['Join'] = FALSE; // Already applied or banned.
+         }
+         
+         // Moderators can view and edit all groups.
+         if ($UserID == Gdn::Session()->UserID && Gdn::Session()->CheckPermission('Garden.Moderation.Manage')) {
+            $Perms['Edit'] = TRUE;
+            $Perms['Delete'] = TRUE;
+            $Perms['View'] = TRUE;
+            $Perms['Moderate'] = TRUE;
+         }
+         
+         $Permissions[$UserID] = $Perms;
+      }
+      
+      $Perms = $Permissions[$UserID];
+      
+      if (!$Permission)
+         return $Perms;
+      
+      if (!isset($Perms[$Permission])) {
+         if (strpos($Permission, '.Reason') === FALSE) {
+            trigger_error("Invalid group permission $Permission.");
+            return FALSE;
+         } else {
+            $Permission = StringEndsWith($Permission, '.Reason', TRUE, TRUE);
+            if (in_array($Permission, array('Member', 'Leader'))) {
+               $Message = T(sprintf("You aren't a %s of this group.", strtolower($Permission)));
+            } else {
+               $Message = sprintf(T("You aren't allowed to %s this group."), T(strtolower($Permission)));
+            }
+            
+            return $Message;
+         }
+      } else {
+         return $Perms[$Permission];
+      }
+   }
+   
    public function GetByUser($UserID) {
       $UserGroups = $this->SQL->GetWhere('UserGroup', array('UserID' => $UserID))->ResultArray();
       $IDs = ConsolidateArrayValuesByKey($UserGroups, 'GroupID');
@@ -19,9 +121,15 @@ class GroupModel extends Gdn_Model {
    }
    
    public function GetID($ID, $DatasetType = DATASET_TYPE_ARRAY) {
+      static $Cache = array();
+      
       $ID = self::ParseID($ID);
+      if (isset($Cache[$ID]))
+         return $Cache[$ID];
       
       $Row = parent::GetID($ID, $DatasetType);
+      $Cache[$ID] = $Row;
+      
       return $Row;
    }
    
@@ -43,7 +151,4 @@ class GroupModel extends Gdn_Model {
       ));
       return $IsMember > 0;
    }
-   
-   
-   
 }
--- applications/groups/settings/bootstrap.php
+++ applications/groups/settings/bootstrap.php
@@ -4,6 +4,21 @@ function GroupSlug($Group) {
    return $Group['GroupID'].'-'.Gdn_Format::Url($Group['Name']);
 }
 
-function GroupUrl($Group) {
-   return '/group/'.GroupSlug($Group);
+function GroupUrl($Group, $Method = NULL) {
+   if ($Method) {
+      return "/group/$Method/".GroupSlug($Group);
+   } else {
+      return '/group/'.GroupSlug($Group);
+   }
+}
+
+function GroupPermission($Permission = NULL, $GroupID = NULL) {
+   if ($GroupID === NULL) {
+      $GroupID = Gdn::Controller()->Data('Group.GroupID');
+   }
+   
+   if (isset(Gdn::Controller()->GroupModel))
+      return Gdn::Controller()->GroupModel->CheckPermission($Permission, $GroupID);
+   $GroupModel = new GroupModel();
+   return $GroupModel->CheckPermission($Permission, $GroupID);
 }
