380e4d8a6835eb2a969c7439bec4d02c6b5634a4
Todd Burry <todd@vanillaforums.com>
2013-02-03 14:13:36 -0500

    Groups application: - Added live query to the GroupController and GroupsController.

--- applications/groups/controllers/class.groupscontroller.php
+++ applications/groups/controllers/class.groupscontroller.php
@@ -23,6 +23,7 @@ class GroupsController extends Gdn_Controller {
       // Set up head
       $this->Head = new HeadModule($this);
       $this->AddJsFile('jquery.js');
+      $this->AddJsFile('jquery.livequery.js');
       $this->AddJsFile('jquery-ui-1.8.17.custom.min.js');
       $this->AddJsFile('jquery.form.js');
       $this->AddJsFile('jquery.popup.js');
