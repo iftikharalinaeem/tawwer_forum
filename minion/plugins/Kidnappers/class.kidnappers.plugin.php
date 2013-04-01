7634d69d8c425255a26e3551fb26f38574f1dbd6
Tim Gunter <tim@vanillaforums.com>
2013-04-01 14:52:07 -0400

    Kidnappers: Raise Informant chance to 50%

--- minion/plugins/Kidnappers/class.kidnappers.plugin.php
+++ minion/plugins/Kidnappers/class.kidnappers.plugin.php
@@ -973,7 +973,7 @@ EOT;
          // Run informant conversion
          $FreeSomeone = false;
          $FreeSomeoneChance = mt_rand(0,100);
-         if ($FreeSomeoneChance > 60)
+         if ($FreeSomeoneChance > 50)
             $FreeSomeone = true;
          
          if ($FreeSomeone) {
