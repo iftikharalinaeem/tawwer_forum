2feb5331f826b7406b3ad9de364c470534291b16
Todd Burry <todd@vanillaforums.com>
2013-04-12 09:57:40 -0400

    Replies: Added missing views.

---
+++ plugins/Replies/views/editreply.php
@@ -0,0 +1,5 @@
+<?php if (!defined('APPLICATION')) exit();
+include_once $this->FetchViewLocation('reply_functions', '', 'plugins/Replies');
+
+$Reply = $this->Data('Reply');
+WriteReplyEdit($Reply);
