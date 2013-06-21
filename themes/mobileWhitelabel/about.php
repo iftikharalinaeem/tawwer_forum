8bfa3bf7bdfb44ad1fa8fdfcbf5c6d9e7d9f15eb
lincolnwebs <lincolnwebs@gmail.com>
2013-06-21 15:13:31 -0400

    mobile theme white label

---
+++ themes/mobileWhitelabel/about.php
@@ -0,0 +1,12 @@
+<?php if (!defined('APPLICATION')) exit();
+/**
+ * An associative array of information about this application.
+ */
+$ThemeInfo['mobilewhitelabel'] = array(
+   'Name' => 'Mobile White Label',
+   'Description' => "White labelled, simple fork of mobile theme.",
+   'Version' => '1.0.0',
+   'Author' => "Lincoln Russell",
+   'AuthorEmail' => 'lincoln@vanillaforums.com',
+   'Site' => 'none'
+);
