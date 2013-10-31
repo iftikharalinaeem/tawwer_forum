3bc19c0c8d6609877385d1e3f1a3d69329ffd9ae
Dane MacMillan <work@danemacmillan.com>
2013-10-31 11:42:12 -0400

    bookmarkcheckbox: removed views, as there is no email component."

--- plugins/bookmarkcheckbox/views/unsubscribe.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined('APPLICATION')) exit(); ?>
-<div class="SplashInfo">
-   <h1>Unsubscribe.</h1>
-   <p><?php echo T('You have successfully unsubscribed from the discussion.'); ?></p>
-</div>
