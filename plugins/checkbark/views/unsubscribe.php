163d1d0eee09aa966b6154ed24713a797a81defe
Dane MacMillan <work@danemacmillan.com>
2013-10-31 11:28:36 -0400

    checkbark: first commit of plugin, which adds a bookmark checkbox below the comment form. It is managed using core preferences.

---
+++ plugins/checkbark/views/unsubscribe.php
@@ -0,0 +1,5 @@
+<?php if (!defined('APPLICATION')) exit(); ?>
+<div class="SplashInfo">
+   <h1>Unsubscribe.</h1>
+   <p><?php echo T('You have successfully unsubscribed from the discussion.'); ?></p>
+</div>
