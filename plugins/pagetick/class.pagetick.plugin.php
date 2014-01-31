db377a939bdfae8dbe20f5ba7d6d322286bace46
Todd Burry <todd@vanillaforums.com>
2014-01-31 18:18:57 -0500

    Added an img endpoint to track pageviews.

--- plugins/pagetick/class.pagetick.plugin.php
+++ plugins/pagetick/class.pagetick.plugin.php
@@ -24,6 +24,7 @@ class PageTickPlugin extends Gdn_Plugin {
          ->Column('TickID', 'datetime', FALSE, 'primary')
          ->Column('CountViews', 'int', '0')
          ->Column('CountMobileViews', 'int', '0')
+         ->Column('CountImgViews', 'int', '0')
          ->Set();
    }
 
@@ -43,8 +44,38 @@ class PageTickPlugin extends Gdn_Plugin {
           ':CountMobileViews1' => $mobile_views));
    }
 
+   public function tickColumn($column = 'CountViews') {
+      $date = new DateTime('today midnight UTC');
+      $sql = "insert GDN_Tick (TickID, $column)
+         values (:TickID, :CountViews)
+         on duplicate key update $column = $column + :CountViews1";
+
+      Gdn::Database()->Query($sql, array(
+          ':TickID' => $date->format(DateTime::W3C),
+          ':CountViews' => 1,
+          ':CountViews1' => 1));
+   }
+
 
    /// Event Handlers ///
+
+   public function Base_AfterRenderAsset_Handler($Sender, $Args) {
+      if ($Args['AssetName'] != 'Head')
+         return;
+
+      $script = <<<EOT
+<script type="text/javascript">
+  (function() {
+    var a = document.createElement('script'); a.type = 'text/javascript'; a.async = true;
+    a.src = '/plugins/pagetick/js/imgtick.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(a, s);
+  })();
+
+</script>
+EOT;
+
+      echo $script;
+   }
 
    /**
     * @param Gdn_Controller $sender
@@ -64,5 +95,18 @@ class PageTickPlugin extends Gdn_Plugin {
       $this->tick(IsMobile());
       $sender->setData('ok', true);
       $sender->render();
+   }
+
+   public function RootController_Pagetickimg_Create($sender) {
+      $this->tickColumn('CountImgViews');
+
+      if (!headers_sent()) {
+         header("Content-type:  image/gif");
+         header("Expires: Wed, 11 Nov 1998 11:11:11 GMT");
+         header("Cache-Control: no-cache");
+         header("Cache-Control: must-revalidate");
+      }
+      require_once __DIR__.'/Pxgif.php';
+      echo Pxgif::httpStr(200, true);
    }
 }
