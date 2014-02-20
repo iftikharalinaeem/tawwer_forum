e87eea514549b6b87c4582306488e40da0756bcb
Dane MacMillan <work@danemacmillan.com>
2014-02-20 18:54:18 -0500

    editor: chrome fixes3.

--- plugins/editor/js/editor.js
+++ plugins/editor/js/editor.js
@@ -1115,8 +1115,8 @@
             if (handleIframe) {
                $(editor).focus();
                $(this).css({
-                  'border': '2px dashed rgba(0,0,0,0.25)',
-                  'border-radius': '2px'
+                  'border': '2px dashed rgba(0,0,0,0.25) !important',
+                  'border-radius': '2px !important'
                });
             }
          }).on('drop dragend dragleave', function(e) {
