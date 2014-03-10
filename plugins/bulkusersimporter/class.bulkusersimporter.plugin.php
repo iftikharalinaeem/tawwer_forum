2814f375fa78a2bcb7bba8cb45530784290c5d9c
Dane MacMillan <work@danemacmillan.com>
2014-03-10 16:53:01 -0400

    bulkusersimporter: added instructions to settings page to note possible utf-8 errors with non-latin usernames.

--- plugins/bulkusersimporter/class.bulkusersimporter.plugin.php
+++ plugins/bulkusersimporter/class.bulkusersimporter.plugin.php
@@ -3,7 +3,7 @@
 $PluginInfo['bulkusersimporter'] = array(
    'Name' => 'Bulk Users Importer',
    'Description' => 'Bulk users import with standardized CSV files.',
-   'Version' => '1.0.5',
+   'Version' => '1.0.6',
    'Author' => "Dane MacMillan",
    'AuthorEmail' => 'dane@vanillaforums.com',
    'AuthorUrl' => 'http://vanillaforums.org/profile/dane',
