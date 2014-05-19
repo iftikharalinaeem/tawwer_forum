5cb1e02610e769206fddb5e81f0216e74bc7831f
Todd Burry <todd@vanillaforums.com>
2014-05-19 11:35:13 -0400

    Add multisite tracking and hub sso.

--- plugins/sitehub/class.sitehub.plugin.php
+++ plugins/sitehub/class.sitehub.plugin.php
@@ -2,15 +2,18 @@
 
 $PluginInfo['sitehub'] = array(
     'Name'        => "Multisite Hub",
+    'Description' => 'The Multi-site Hub plugin provides functionality to manage a multi-site cluster.',
     'Version'     => '1.0.0',
     'Author'      => "Todd Burry",
     'AuthorEmail' => 'todd@vanillaforums.com',
     'AuthorUrl'   => 'http://vanillaforums.com',
-    'License'     => 'Proprietary'
+    'License'     => 'Proprietary',
+    'RequiredPlugins' => array('SimpleApi' => '1.0')
+
 );
 
 /**
- * Multisite Hub Plugin
+ * The Multisite Hub plugin provides functionality to manage a multi-site cluster.
  *
  * @author    Todd Burry <todd@vanillaforums.com>
  * @copyright 2014 (c) Todd Burry
@@ -18,15 +21,70 @@ $PluginInfo['sitehub'] = array(
  * @package   MultisiteHub
  * @since     1.0.0
  */
-class MultisiteHubPlugin extends Gdn_Plugin {
-    /**
-     * This will run when you "Enable" the plugin
-     *
-     * @since  1.0.0
-     * @access public
-     * @return bool
-     */
+class SiteHubPlugin extends Gdn_Plugin {
+    /// Constants ///
+
+    const HUB_COOKIE = 'vf_hub_ENDTX';
+    const NODE_COOKIE = 'vf_node_ENDTX';
+
+    /// Methods ///
+
     public function setup() {
-        return true;
+        $this->structure();
+    }
+
+    public function structure() {
+        gdn::structure()
+            ->table('MultiSite')
+            ->primaryKey('MultiSiteID')
+            ->column('Name', 'varchar(255)', false)
+            ->column('Url', 'varchar(255)', false, 'unique.url')
+            ->column('Status', ['pending', 'building', 'active'], 'pending')
+            ->column('DateInserted', 'datetime')
+            ->column('InsertUserID', 'int')
+            ->column('DateUpdated', 'datetime', true)
+            ->column('UpdateUserID', 'int', true)
+            ->set();
+    }
+
+    /// Event Handlers ///
+
+    /**
+     * @param Gdn_PluginManager $sender
+     */
+    public function gdn_pluginManager_afterStart_handler($sender) {
+        SaveToConfig([
+            'Garden.Cookie.Name' => self::HUB_COOKIE,
+            'Garden.Cookie.Path' => '/',
+        ], '', false);
+    }
+
+    /**
+     * Gets information about the currently signed in user suitable for hub SSO calls.
+     *
+     * @param ProfileController $sender
+     * @throws Gdn_UserException Throws an exception when the user isn't signed in.
+     */
+    public function profileController_hubSSO_create($sender) {
+        if (!Gdn::Session()->IsValid()) {
+            throw NotFoundException('User');
+        }
+
+        // Get the currently signed in user.
+        $user = Gdn::UserModel()->GetID(Gdn::Session()->UserID, DATASET_TYPE_ARRAY);
+        if (!$user) {
+            throw NotFoundException('User');
+        }
+        $ssoUser = arrayTranslate($user, array('UserID', 'Name', 'Email', 'Banned', 'Photo', 'PhotoUrl'));
+        $ssoUser['Photo'] = $ssoUser['PhotoUrl'];
+        $ssoUser['Roles'] = ConsolidateArrayValuesByKey(Gdn::UserModel()->GetRoles($user['UserID'])->ResultArray(), 'Name');
+
+        $sender->EventArguments['User'] =& $ssoUser;
+        $sender->EventArguments['Session'] =& $user;
+        $sender->FireEvent('hubSSO');
+
+        $sender->Data = array('User' => $ssoUser);
+        SaveToConfig('Api.Clean', false, false);
+        $sender->Render('Blank', 'Utility', 'Dashboard');
     }
 }
--- plugins/sitenode/class.sitenode.plugin.php
+++ plugins/sitenode/class.sitenode.plugin.php
@@ -20,7 +20,13 @@ $PluginInfo['sitenode'] = array(
 class SiteNodePlugin extends Gdn_Plugin {
     /// CONSTANTS ///
 
+    const HUB_COOKIE = 'vf_hub_ENDTX';
+    const NODE_COOKIE = 'vf_node_ENDTX';
+    const PROVIDER_KEY = 'hub';
+
     /// PROPERTIES ///
+
+    /// METHODS ///
 
     /**
      * This will run when you "Enable" the plugin
@@ -33,9 +39,79 @@ class SiteNodePlugin extends Gdn_Plugin {
     }
 
     public function structure() {
+        // Save the hub sso provider type.
+        Gdn::SQL()->Replace('UserAuthenticationProvider',
+            array('AuthenticationSchemeAlias' => self::PROVIDER_KEY, 'URL' => '...', 'AssociationSecret' => '...', 'AssociationHashMethod' => '...'),
+            array('AuthenticationKey' => self::PROVIDER_KEY), TRUE);
     }
 
-    /// METHODS ///
+    public function hubApi($path, $method = 'GET', $params = []) {
+        Trace("hub api: $method $path");
+
+        $url = rtrim(Gdn::Request()->Domain(), '/');
+        $headers = [];
+
+        // Cludge for osx that doesn't allow host files.
+        if (Gdn::Request()->Host() === 'localhost' || StringEndsWith($url, '.lc')) {
+            $url = 'http://127.0.0.1';
+            $headers['Host'] = Gdn::Request()->Host();
+        }
+
+        $url .= '/hub/'.ltrim($path, '/');
+
+        $request = new ProxyRequest();
+        $response = $request->Request([
+            'URL' => $url,
+            'Cookies' => true,
+            'Timeout' => 100
+        ], $params, null, $headers);
+
+        if ($request->ContentType === 'application/json') {
+            $response = json_decode($response, true);
+        }
+
+        if ($request->ResponseStatus != 200) {
+            Trace($response, "Error {$request->ResponseStatus}");
+            throw new Gdn_UserException(val('Exception', $response, 'There was an error performing your request.'), $request->ResponseStatus);
+        }
+
+        Trace($response, "hub api response");
+        return $response;
+    }
 
     /// EVENT HANDLERS ///
+
+    /**
+     * SSO someone into the hub if they aren't already signed in.
+     * @param Gdn_Dispatcher $sender
+     */
+    public function gdn_dispatcher_appStartup_handler($sender) {
+        if (!Gdn::Session()->IsValid() && val(self::HUB_COOKIE, $_COOKIE)) {
+            Trace('Trying hub sso');
+            try {
+                $user = val('User', $this->hubApi('/profile/hubsso.json'));
+                $user_id = Gdn::UserModel()->Connect($user['UserID'], self::PROVIDER_KEY, $user);
+                Trace($user_id, 'user ID');
+                if ($user_id) {
+                    Gdn::Session()->Start($user_id, true);
+                }
+            } catch(Exception $ex) {
+                Trace($ex, TRACE_ERROR);
+            }
+        }
+    }
+
+    /**
+     *
+     * @param Gdn_PluginManager $sender
+     */
+    public function gdn_pluginManager_afterStart_handler($sender) {
+        $path = '/'.trim(Gdn::Request()->WebRoot(), '/');
+
+        SaveToConfig([
+            'Garden.Cookie.Name' => self::NODE_COOKIE,
+            'Garden.Cookie.Path' => $path,
+            'Garden.Registration.AutoConnect' => true,
+        ], '', false);
+    }
 }
