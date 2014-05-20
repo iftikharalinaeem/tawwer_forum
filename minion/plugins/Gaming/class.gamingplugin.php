1786cd766b08536a1efca1bf152441eddca028b5
Tim Gunter <tim@vanillaforums.com>
2014-05-20 14:48:44 -0400

    Minion Gaming WIP

--- minion/plugins/Gaming/class.gamingplugin.php
+++ minion/plugins/Gaming/class.gamingplugin.php
@@ -2,15 +2,15 @@
 
 /**
  * Gaming Plugin
- * 
+ *
  * Acts a game loader / invoker, and supplies game tools to Minion games.
- * 
+ *
  * Tools:
  *  Inventory
- * 
- * Changes: 
+ *
+ * Changes:
  *  1.0     Release
- * 
+ *
  * @author Tim Gunter <tim@vanillaforums.com>
  * @copyright 2003 Vanilla Forums, Inc
  * @license Proprietary
@@ -34,110 +34,110 @@ $PluginInfo['Gaming'] = array(
 );
 
 class GamingPlugin extends Gdn_Plugin {
-   
+
    protected $games = array();
    protected $aliases = array();
 
    /**
     * Parse a token from the current state
-    * 
+    *
     * @param MinionPlugin $sender
     */
    public function MinionPlugin_Token_Handler($sender) {
       $State = &$sender->EventArguments['State'];
 
       if (!$State['Method'] && in_array($State['CompareToken'], array('play', 'playing'))) {
-         $sender->Consume($State, 'Method', 'play');
-         
-         $sender->Consume($State, 'Gather', array(
+         $sender->consume($State, 'Method', 'play');
+
+         $sender->consume($State, 'Gather', array(
             'Node'   => 'Phrase',
             'Delta'  => ''
          ));
       }
    }
-   
+
    /**
     * Parse custom minion commands
-    * 
+    *
     * @param MinionPlugin $sender
     */
    public function MinionPlugin_Command_Handler($sender) {
       $Actions = &$sender->EventArguments['Actions'];
       $State = &$sender->EventArguments['State'];
-      
+
       switch ($State['Method']) {
          case 'play':
-            
+
             // Games must be started in the OP
             if (array_key_exists('Comment', $State['Sources']))
                return;
-            
+
             // Games must have a name
             if (!array_key_exists('Phrase', $State['Targets']))
                return;
 
             $Actions[] = array('play', null, $State);
             break;
-            
+
       }
-      
+
    }
-   
+
    /**
     * Perform custom minion actions
-    * 
+    *
     * @param MinionPlugin $sender
     */
    public function MinionPlugin_Action_Handler($sender) {
-      $Action = $sender->EventArguments['Action'];
-      $State = &$sender->EventArguments['State'];
-      
-      switch ($Action) {
-         
+      $action = $sender->EventArguments['Action'];
+      $state = &$sender->EventArguments['State'];
+
+      switch ($action) {
+
          // Play a game, or shut one down
          case 'play':
-            
+
             // Games must be started in the OP
-            if (array_key_exists('Comment', $State['Sources']))
+            if (array_key_exists('Comment', $state['Sources']))
                return;
-            
-            $gameName = strtolower(GetValueR('Targets.Phrase', $State));
+
+            $gameName = strtolower(valr('Targets.Phrase', $state));
             if (!array_key_exists($gameName, $this->aliases)) {
                break;
             }
             $game = $this->aliases[$gameName];
             $pluginName = $game['plugin'];
-            $plugin = Gdn::PluginManager()->GetPluginInstance($pluginName, Gdn_PluginManager::ACCESS_PLUGINNAME);
-            
-            switch ($State['Toggle']) {
-               
+            $plugin = Gdn::pluginManager()->getPluginInstance($pluginName, Gdn_PluginManager::ACCESS_PLUGINNAME);
+
+            switch ($state['Toggle']) {
+
                case 'off':
-                  $plugin->stopGame($State['Sources']['Discussion'], $State['Sources']['User']);
+                  $plugin->stopGame($state['Sources']['Discussion'], $state['Sources']['User']);
                   break;
-               
+
                case 'on':
                default:
-                  $plugin->startGame($State['Sources']['Discussion'], $State['Sources']['User']);
+                  $plugin->startGame($state['Sources']['Discussion'], $state['Sources']['User']);
                   break;
-               
+
             }
-            
+
             break;
       }
    }
-   
+
    /**
     * Collect games
-    * 
+    *
     * @param MinionPlugin $sender
     */
    public function MinionPlugin_Start_Handler($sender) {
-      $this->FireEvent('Register');
+      $this->fireEvent('Register');
    }
-   
+
    /**
     * Register a game with Minion
-    * 
+    *
     * @param string $game
     * @param array $aliases
     * @param string $pluginName
@@ -150,5 +150,5 @@ class GamingPlugin extends Gdn_Plugin {
       );
       $this->aliases = array_merge($this->aliases, array_fill_keys($aliases, $game));
    }
-   
+
 }
