0321ea4feb7df525cad25ab5999db9f7aae911b5
Todd Burry <todd@vanillaforums.com>
2014-05-24 15:49:46 -0400

    Add the infstub plugin to offer some infrastructure mock functionality for localhost development.

---
+++ plugins/infstub/class.infrastructure.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2014 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
+class Infrastructure {
+    /// Properties ///
+
+    protected static $clusterConfig;
+
+    /// Methods ///
+
+    /**
+     * Get a value from the cluster config.
+     *
+     * @param string $key The dot-separated key to get.
+     * @param mixed $default The default value if {@link $key} is not found.
+     * @return mixed Returns the value at {@link $key} or {$link $default} if there is no value set.
+     */
+    public static function clusterConfig($key, $default = null) {
+        if (self::$clusterConfig === null) {
+            $c = @json_decode(file_get_contents(PATH_CONF.'/config-cluster.json'), true);
+            if (!is_array($c)) {
+                self::$clusterConfig = [];
+            } else {
+                self::$clusterConfig = $c;
+            }
+        }
+
+        $result = valr($key, self::$clusterConfig, $default);
+        return $result;
+    }
+}
---
+++ plugins/infstub/class.infstub.plugin.php
@@ -0,0 +1,31 @@
+<?php if (!defined('APPLICATION')) exit;
+
+$PluginInfo['infstub'] = array(
+    'Name'        => "Infrastructure Stub",
+    'Description' => "Provides some basic mock infrastructure functionality to aid in localhost development.",
+    'Version'     => '1.0.0-alpha',
+    'Author'      => "Todd Burry",
+    'AuthorEmail' => 'todd@vanillaforums.com',
+    'License'     => 'Proprietary'
+);
+
+/**
+ * Infrastructure Stub Plugin
+ *
+ * @author    Todd Burry <todd@vanillaforums.com>
+ * @copyright 2014 (c) Todd Burry
+ * @license   Proprietary
+ * @since     1.0.0
+ */
+class InfrastructureStubPlugin extends Gdn_Plugin {
+    /**
+     * This will run when you "Enable" the plugin
+     *
+     * @since  1.0.0
+     * @access public
+     * @return bool
+     */
+    public function setup() {
+        return true;
+    }
+}
