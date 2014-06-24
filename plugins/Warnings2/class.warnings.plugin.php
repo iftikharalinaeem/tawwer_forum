3edb061c2a9bc7a97d275b1aa3de9d13b89a0627
Tim Gunter <tim@vanillaforums.com>
2014-06-24 14:03:52 -0400

    Warnings2: Backwards conditional

--- plugins/Warnings2/class.warnings.plugin.php
+++ plugins/Warnings2/class.warnings.plugin.php
@@ -128,7 +128,7 @@ class Warnings2Plugin extends Gdn_Plugin {
 
         $Row->Attributes = Gdn_Format::Unserialize($Row->Attributes);
         if (isset($Row->Attributes['WarningID']) && $Row->Attributes['WarningID']) {
-            if (isset($Row->Attributes['Reversed']) && $Row->Attributes['Reversed']) {
+            if (!isset($Row->Attributes['Reversed']) || !$Row->Attributes['Reversed']) {
                 echo '<div class="DismissMessage Warning">' .
                 sprintf(T('%s was warned for this post.', '%s was <a href="%s">warned</a> for this post.'), htmlspecialchars(val('InsertName', $Row)), UserUrl($Row, 'Insert', 'notes')),
                 '</div>';
