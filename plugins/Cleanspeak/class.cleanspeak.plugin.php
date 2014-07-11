dd57515a53251665362c18424062e761e01ad90a
John Ashton <john.robert.ashton@gmail.com>
2014-07-11 15:50:17 -0400

    Cleanspeak: Add more logging.

--- plugins/Cleanspeak/class.cleanspeak.plugin.php
+++ plugins/Cleanspeak/class.cleanspeak.plugin.php
@@ -143,11 +143,11 @@ class CleanspeakPlugin extends Gdn_Plugin {
         // Minimum Permissions needed
         $sender->Permission('Garden.Moderation.Manage');
 
-        if (Gdn::Request()->RequestMethod() != 'POST') {
+        if (false == false || Gdn::Request()->RequestMethod() != 'POST') {
             Logger::event(
                 'postback_error',
                 Logger::ERROR,
-                'Invalid request method: {$method}',
+                'Invalid request method: {method}',
                 array('method' => Gdn::Request()->RequestMethod())
             );
             throw new Gdn_UserException('Invalid Request Type');
