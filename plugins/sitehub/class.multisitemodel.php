abb371e4800c2fb9ea641dd6e9a077d60d542069
John Ashton <john.robert.ashton@gmail.com>
2014-07-11 17:24:05 -0400

    Mulrisites: Add trace.

--- plugins/sitehub/class.multisitemodel.php
+++ plugins/sitehub/class.multisitemodel.php
@@ -377,6 +377,9 @@ class MultisiteModel extends Gdn_Model {
 
         $url = rtrim(http_build_url($baseUrl, $urlParts), '/').'/'.ltrim($path, '/');
 
+        Trace("baseUrl: $baseUrl");
+        Trace("node api url: $url");
+
         if ($access_token = Infrastructure::clusterConfig('cluster.loader.apikey', '')) {
             $headers['Authorization'] = "token $access_token";
         }
