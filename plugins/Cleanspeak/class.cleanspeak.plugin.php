bf7c6eb27360ca2464cec7021cc3ad1794f7ba80
John Ashton <john.robert.ashton@gmail.com>
2014-07-11 15:39:27 -0400

    Cleanspeak: Add temp debug logs.

--- plugins/Cleanspeak/class.cleanspeak.plugin.php
+++ plugins/Cleanspeak/class.cleanspeak.plugin.php
@@ -143,9 +143,11 @@ class CleanspeakPlugin extends Gdn_Plugin {
         // Minimum Permissions needed
         $sender->Permission('Garden.Moderation.Manage');
 
+        Logger::event('debug', Logger::ERROR, 'API Request Method:' . Gdn::Request()->RequestMethod());
+
         $post = Gdn::Request()->Post();
         if (!$post) {
-            Logger::event('post_error', Logger::ERROR, 'Error in POST', $post);
+            Logger::event('postback_error', Logger::ERROR, 'Error in POST', $post);
             throw new Gdn_UserException('Invalid Request Type');
         }
 
