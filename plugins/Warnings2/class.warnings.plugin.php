e896336d070f9767fbc4c3d3407e8a3b2bb17e24
Dane MacMillan <work@danemacmillan.com>
2014-07-15 10:44:10 -0400

    Warnings2: incremented version number.

--- plugins/Warnings2/class.warnings.plugin.php
+++ plugins/Warnings2/class.warnings.plugin.php
@@ -9,7 +9,7 @@
 $PluginInfo['Warnings2'] = array(
     'Name' => 'Warnings & Notes',
     'Description' => "Allows moderators to warn users and add private notes to profiles to help police the community.",
-    'Version' => '2.1.3',
+    'Version' => '2.1.4',
     'RequiredApplications' => array('Vanilla' => '2.1a'),
     'Author' => 'Todd Burry',
     'AuthorEmail' => 'todd@vanillaforums.com',
