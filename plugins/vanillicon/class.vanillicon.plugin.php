0f47307b5262fe1fa1e6a2ffd273accb8e786e69
Todd Burry <todd@vanillaforums.com>
2014-10-06 18:20:15 -0400

    Add vanillicon v2 support to the vanillicon plugin.

--- plugins/vanillicon/class.vanillicon.plugin.php
+++ plugins/vanillicon/class.vanillicon.plugin.php
@@ -9,38 +9,92 @@
 $PluginInfo['vanillicon'] = array(
    'Name' => 'Vanillicon',
    'Description' => "Provides fun default user icons from vanillicon.com.",
-   'Version' => '1.0.3',
+   'Version' => '2.0.0-beta',
    'RequiredApplications' => array('Vanilla' => '2.0.18'),
    'Author' => 'Todd Burry',
    'AuthorEmail' => 'todd@vanillaforums.com',
    'AuthorUrl' => 'http://www.vanillaforums.org/profile/todd',
    'MobileFriendly' => TRUE,
+   'SettingsUrl' => '/settings/vanillicon',
+   'SettingsPermission' => 'Garden.Settings.Manage'
 );
 
 class VanilliconPlugin extends Gdn_Plugin {
+   /// Methods ///
+
+   public function setup() {
+      $this->structure();
+   }
+
+   public function structure() {
+      TouchConfig('Plugins.Vanillicon.Type', 'v1');
+   }
+
+   /// Properties ///
+
    public function ProfileController_AfterAddSideMenu_Handler($Sender, $Args) {
       if (!$Sender->User->Photo) {
-         $Email = GetValue('Email', $Sender->User);
-         $Hash = md5($Email);
-         $Sender->User->Photo = '//w'.substr($Hash, 0, 1).'.vanillicon.com/'.$Hash.'_200.png';
+         $Sender->User->Photo = UserPhotoDefaultUrl($Sender->User, array('Size' => 200));
       }
+   }
+
+   /**
+    * @param Gdn_Controller $sender
+    */
+   public function SettingsController_Vanillicon_Create($sender) {
+      $sender->Permission('Garden.Settings.Manage');
+      $cf = new ConfigurationModule($sender);
+
+      $items = array(
+         'v1' => 'Vanillicon 1',
+         'v2' => 'Vanillicon 2 (beta)'
+      );
+
+      $cf->Initialize(array(
+         'Plugins.Vanillicon.Type' => array(
+            'LabelCode' => 'Vanillicon Set',
+            'Control' => 'radiolist',
+            'Description' => 'Which vanillicon set do you want to use?',
+            'Items' => $items,
+            'Options' => array('list' => true, 'listclass' => 'icon-list', 'display' => 'after'),
+            'Default' => 'v1'
+         )
+      ));
+
+      $sender->AddSideMenu();
+      $sender->SetData('Title', sprintf(T('%s Settings'), 'Vanillicon'));
+      $cf->RenderAll();
    }
 }
 
 if (!function_exists('UserPhotoDefaultUrl')) {
    function UserPhotoDefaultUrl($User, $Options = array()) {
-      static $IconSize = NULL;
-      if ($IconSize === NULL) {
-         $ThumbSize = C('Garden.Thumbnail.Size');
-         $IconSize = $ThumbSize <= 50 ? 50 : 100;
+      static $iconSize = NULL, $type = null;
+      if ($iconSize === NULL) {
+         $thumbSize = C('Garden.Thumbnail.Size');
+         $iconSize = $thumbSize <= 50 ? 50 : 100;
+      }
+      if ($type === null) {
+         $type = C('Plugins.Vanillicon.Type');
+      }
+      $size = val('Size', $Options, $iconSize);
+
+      $email = GetValue('Email', $User);
+      if (!$email) {
+         $email = GetValue('UserID', $User, 100);
+      }
+      $hash = md5($email);
+      $px = substr($hash, 0, 1);
+
+      switch ($type) {
+         case 'v2':
+            $photourl = "//w$px.vanillicon.com/v2/{$hash}.svg";
+            break;
+         default:
+            $photourl = "//w$px.vanillicon.com/{$hash}_{$size}.png";
+            break;
       }
 
-      $Email = GetValue('Email', $User);
-      if (!$Email) {
-         $Email = GetValue('UserID', $User, 100);
-      }
-      $Hash = md5($Email);
-      $PhotoUrl = '//w'.substr($Hash, 0, 1).".vanillicon.com/{$Hash}_{$IconSize}.png";
-      return $PhotoUrl;
+      return $photourl;
    }
 }
