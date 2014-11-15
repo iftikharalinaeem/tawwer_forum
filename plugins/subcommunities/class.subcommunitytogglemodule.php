7a58fbfca87705b6811c0090781e46e82c3e0b83
Todd Burry <todd@vanillaforums.com>
2014-11-14 20:48:49 -0500

    Set the application folder on the SubCommunityToggleModule.

--- plugins/subcommunities/class.subcommunitytogglemodule.php
+++ plugins/subcommunities/class.subcommunitytogglemodule.php
@@ -3,6 +3,11 @@
 class SubcommunityToggleModule extends Gdn_Module {
     public $LabelField = 'Name';
 
+    public function __construct($Sender = '', $ApplicationFolder = FALSE) {
+        $this->_Sender = $Sender;
+        $this->_ApplicationFolder = 'plugins/subcommunities';
+    }
+
     public function ToString() {
         $this->SetData('Sites', SubcommunityModel::all());
         $this->SetData('Current', SubcommunityModel::getCurrent());
