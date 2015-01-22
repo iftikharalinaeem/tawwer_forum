32fe1e01de68fda82fe187e2ad053d20430eb756
Todd Burry <todd@vanillaforums.com>
2015-01-22 12:59:56 -0500

    Add base support for groups searching in sphinx and advanced search.

--- plugins/AdvancedSearch/js/advanced-search.js
+++ plugins/AdvancedSearch/js/advanced-search.js
@@ -21,6 +21,7 @@ $(document).on('click', '.AdvancedSearch .Handle', function(e) {
         
         if (gdn.definition('searchAutocomplete', true) != '0') {
            $('#Form_search', $container).autocomplete(method);
+           $('.js-search-groups').autocomplete(method);
         }
     }
     
@@ -100,6 +101,7 @@ jQuery(document).ready(function($) {
     /// Search box autocomplete.
     if ($.fn.searchAutocomplete && gdn.definition('searchAutocomplete', true) != '0') {
         $('.AdvancedSearch #Form_search,.SiteSearch #Form_Search,.js-search').searchAutocomplete();
+        $('.js-search-groups').searchAutocomplete();
         
         $('.QuickSearch #Form_search').each(function() {
             var $this = $(this);
