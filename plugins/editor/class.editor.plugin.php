22c0e070e935aced067ac08ddc7bf7dbcd5f67fc
lincolnwebs <lincolnwebs@gmail.com>
2015-02-16 23:02:35 -0500

    Fix permission check to remove file while editing post

--- plugins/editor/class.editor.plugin.php
+++ plugins/editor/class.editor.plugin.php
@@ -927,18 +927,13 @@ class EditorPlugin extends Gdn_Plugin {
 
       // Save data to database using model with media table
       $Model = new Gdn_Model('Media');
-
       $Media = (array) $Model->GetID($MediaID);
 
-      if ($Media
-      && !empty($Media['InsertUserID'])
-      && Gdn::Session()->UserID == $Media['InsertUserID']
-      // These two are only available when a comment/discussion has already
-      // been saved. If removing them from a live session (ie, deciding not
-      // to use them), then they will not be filled, so remove checks.
-      //&& $Media['ForeignID'] == $ForeignID
-      //&& $Media['ForeignTable'] == $ForeignType
-      ) {
+      $IsOwner = (Gdn::Session()->UserID == $Media['InsertUserID']);
+      // @todo Per-category edit permission would be better, but this global is far simpler to check here.
+      // However, this currently matches the permission check in views/attachments.php so keep that in sync.
+      $CanDelete = ($IsOwner || Gdn::Session()->CheckPermission('Garden.Moderation.Manage'));
+      if ($Media && !empty($Media['InsertUserID']) && $CanDelete) {
          try {
             if ($Model->Delete($MediaID)) {
                // unlink the images.
@@ -978,7 +973,7 @@ class EditorPlugin extends Gdn_Plugin {
       return FALSE;
    }
 
-      /**
+   /**
     * Attach files to a comment during save.
     *
     * @access public
