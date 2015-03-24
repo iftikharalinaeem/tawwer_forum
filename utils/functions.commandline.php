8e25fbaeec88a5f07bb1fe865c4b38cd906c9902
Todd Burry <todd@vanillaforums.com>
2015-03-23 21:24:01 -0400

    Fix the content type parsing.

--- utils/functions.commandline.php
+++ utils/functions.commandline.php
@@ -10,13 +10,13 @@ function Curl($Url) {
    curl_setopt($C, CURLOPT_SSL_VERIFYPEER, FALSE);
 
    $Result = curl_exec($C);
-   
+
    if (!$Result) {
       throw new Exception(curl_error($C), curl_errno($C));
    }
    $Code = curl_getinfo($C, CURLINFO_HTTP_CODE);
    $ContentType = curl_getinfo($C, CURLINFO_CONTENT_TYPE);
-   if ($ContentType == 'application/json') {
+   if (strpos($ContentType, 'application/json') !== false) {
       $Result = json_decode($Result, TRUE);
       if ($Code != 200) {
          throw new Exception($Result['Exception'], $Result['Code']);
@@ -24,7 +24,7 @@ function Curl($Url) {
    } elseif ($Code != 200) {
       throw new Exception($Result, $Code);
    }
-   
+
    return $Result;
 }
 
