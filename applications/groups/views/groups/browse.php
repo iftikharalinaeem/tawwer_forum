e82a617e0cadc6f45fe86fca3c97b3831fe59cec
beckyvb <beckyvanbussel@gmail.com>
2015-07-28 14:08:54 -0400

    Browse groups: Add sender argument.

--- applications/groups/views/groups/browse.php
+++ applications/groups/views/groups/browse.php
@@ -8,7 +8,7 @@
 $groups = $this->Data('Groups');
 $groupModel = new GroupModel();
 $groupModel->JoinRecentPosts($groups);
-$list = new GroupListModule($groups, 'groups', $this->Title(), t("No groups to display yet."), false);
+$list = new GroupListModule($this, $groups, 'groups', $this->Title(), t("No groups to display yet."), '', false);
 echo $list;
 ?>
 
