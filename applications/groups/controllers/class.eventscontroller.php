c46afc24e67332e06b943da16cfaf616863daef8
beckyvb <beckyvanbussel@gmail.com>
2015-07-28 14:09:21 -0400

    Events: Change date range.

--- applications/groups/controllers/class.eventscontroller.php
+++ applications/groups/controllers/class.eventscontroller.php
@@ -102,12 +102,12 @@ class EventsController extends Gdn_Controller {
       $this->AddBreadcrumb($this->Title());
 
       // Upcoming events
-      $UpcomingRange = C('Groups.Events.UpcomingRange', '+30 days');
+      $UpcomingRange = C('Groups.Events.UpcomingRange', '+365 days');
       $Events = $EventModel->GetUpcoming($UpcomingRange, $EventCriteria, FALSE);
       $this->SetData('UpcomingEvents', $Events);
 
       // Recent events
-      $RecentRange = C('Groups.Events.RecentRange', '-10 days');
+      $RecentRange = C('Groups.Events.RecentRange', '-365 days');
       $Events = $EventModel->GetUpcoming($RecentRange, $EventCriteria, TRUE);
       $this->SetData('RecentEvents', $Events);
 
