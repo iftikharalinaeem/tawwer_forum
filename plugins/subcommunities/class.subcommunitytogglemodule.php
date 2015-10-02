3f03ff5ae2bf0f9d5920e69605f60aa98d82efb4
beckyvb <beckyvanbussel@gmail.com>
2015-10-02 12:39:47 -0400

    Subcommunity Toggle Module: Move setView invocation from constructor to toString.

--- plugins/subcommunities/class.subcommunitytogglemodule.php
+++ plugins/subcommunities/class.subcommunitytogglemodule.php
@@ -7,18 +7,15 @@ class SubcommunityToggleModule extends Gdn_Module {
     public function __construct($Sender = '', $ApplicationFolder = FALSE) {
         $this->_Sender = $Sender;
         $this->_ApplicationFolder = 'plugins/subcommunities';
-        $style = strtolower($this->Style);
-        if (!in_array($style, array('select'))) {
-            $view = '';
-        } else {
-            $view = "subcommunitytoggle_$style";
-        }
-        $this->setView($view);
     }
 
     public function ToString() {
         if (!$this->Visible) {
             return '';
+        }
+        $style = strtolower($this->Style);
+        if (in_array($style, array('select'))) {
+            $this->setView("subcommunitytoggle_$style");
         }
         $this->SetData('Subcommunities', SubcommunityModel::getAvailable());
         $this->SetData('Current', SubcommunityModel::getCurrent());
