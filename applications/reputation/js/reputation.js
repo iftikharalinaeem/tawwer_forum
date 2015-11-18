1bb512ab18238f0b127c6ffa8dfc4f4e70382853
beckyvb <beckyvanbussel@gmail.com>
2015-11-18 12:53:22 -0500

    Remove livequery from the reputation application.

--- applications/reputation/js/reputation.js
+++ applications/reputation/js/reputation.js
@@ -1,16 +1,25 @@
+var multicomplete = {
+    start: function() {
+        console.log('step 1');
+        var input = $('.MultiComplete');
+        if (input.length) {
+            console.log('step 2');
+            $('.MultiComplete').autocomplete(
+                gdn.url('/dashboard/user/autocomplete/'),
+                {
+                    minChars: 1,
+                    multiple: true,
+                    scrollHeight: 220,
+                    selectFirst: true
+                }
+            ).autogrow();
+        }
+    }
+}
+
 jQuery(document).ready(function($) {
-   
-   // Enable multicomplete on selected inputs
-   $('.MultiComplete').livequery(function() {
-      $(this).autocomplete(
-         gdn.url('/dashboard/user/autocomplete/'),
-         {
-            minChars: 1,
-            multiple: true,
-            scrollHeight: 220,
-            selectFirst: true
-         }
-      ).autogrow();
-   });
-   
+    multicomplete.start();
+    $('.js-give-badge').popup({
+        afterLoad: multicomplete.start
+    });
 });
