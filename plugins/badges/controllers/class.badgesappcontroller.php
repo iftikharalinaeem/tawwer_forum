0edf99227a63c87a2f6ca0fbdb659f79f1325542
lincolnwebs <lincolnwebs@gmail.com>
2015-11-18 23:55:41 -0500

    Remove livequery from badges

--- plugins/badges/controllers/class.badgesappcontroller.php
+++ plugins/badges/controllers/class.badgesappcontroller.php
@@ -42,7 +42,6 @@ class BadgesAppController extends Gdn_Controller {
 
             // Vanilla goodness
             $this->AddJsFile('jquery.js');
-            $this->AddJsFile('jquery.livequery.js');
             $this->AddJsFile('jquery.form.js');
             $this->AddJsFile('jquery.popup.js');
             $this->AddJsFile('jquery.gardenhandleajaxform.js');
