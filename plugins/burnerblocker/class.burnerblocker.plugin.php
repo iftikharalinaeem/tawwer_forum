6a5329e34f24dacd16a689da2edecab1e5536213
lincolnwebs <lincolnwebs@gmail.com>
2015-12-03 11:37:02 -0500

    Burner: add json lists and freemailers

--- plugins/burnerblocker/class.burnerblocker.plugin.php
+++ plugins/burnerblocker/class.burnerblocker.plugin.php
@@ -5,7 +5,7 @@
 
 $PluginInfo['burnerblocker'] = array(
     'Name' => 'Burner Blocker',
-    'Description' => 'Blocks all known "burner" email domains from registering. A master list is maintained by Vanilla.',
+    'Description' => 'Blocks all known "burner" email domains from registering.',
     'Version' => '1.0',
     'Author' => "Lincoln Russell",
     'AuthorEmail' => 'lincoln@vanillaforums.com',
@@ -15,36 +15,74 @@ $PluginInfo['burnerblocker'] = array(
 
 /**
  * Class BurnerBlockerPlugin
+ *
+ * Periodically the lists in /payloads should be upgraded from these sources:
+ *
+ * @see https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/index.json
+ * @see https://raw.githubusercontent.com/lavab/disposable/master/domains.json
+ *
+ * @todo Add settings page for BlockFreeEmails country configuration.
  */
 class BurnerBlockerPlugin extends Gdn_Plugin {
 
-    /** @var array Add all known burner domains here. DO NOT SHARE. */
-    protected $bannedDomains = array(
+    /** @var array Add burner domains not in our payloads. */
+    protected $burnerDomains = [
+        'slipry.net',
+    ];
+
+    /** @var array Chinese free email domains. */
+    protected $cnDomains = [
         '139.com',
         'daum.net',
-        'discardmail.com',
-        'fakeinbox.com',
-        'grr.la',
-        'guerrillamail.biz',
-        'guerrillamail.com',
-        'guerrillamail.de',
-        'guerrillamail.net',
-        'guerrillamail.org',
-        'guerrillamailblock.com',
         'hanmail.net',
-        'incognitomail.org',
-        'mailinator.com',
-        'mailnesia.com',
-        'mail.ru',
-        'mt2015.com',
-        'nowmymail.com',
-        'sharklasers.com',
-        'slipry.net',
-        'spam4.me',
-        'trashmail.com',
-        'trashmail.ws',
-        'yopmail.com',
-    );
+    ];
+
+    /** @var array Russian free email domains. */
+    protected $ruDomains = [
+        'yandex.ru',
+    ];
+
+    /** @var array US free email domains. */
+    protected $usDomains = [
+        'gmail.com',
+        'hotmail.com',
+        'outlook.com',
+        'yahoo.com',
+    ];
+
+    /**
+     * Get & validate our JSON payloads.
+     *
+     * Pro-tip: Never get clever and loop this array_merge().
+     *
+     * @return array Domains to block.
+     */
+    public function getBurnerDomains() {
+        $ivolo = json_decode(file_get_contents(__DIR__.'/payloads/ivolo.json'));
+        if (!is_array($ivolo)) {
+            $ivolo = [];
+        }
+
+        $lavab = json_decode(file_get_contents(__DIR__.'/payloads/lavab.json'));
+        if (!is_array($lavab)) {
+            $lavab = [];
+        }
+
+        return array_merge($ivolo, $lavab, $this->burnerDomains);
+    }
+
+    /**
+     * Evaluate which countries' freemail domains to block.
+     *
+     * @return array Domains to block.
+     */
+    public function getFreemailDomains() {
+        $cn = (c('Registration.BlockFreeEmails.CN')) ? $this->cnDomains : [];
+        $ru = (c('Registration.BlockFreeEmails.RU')) ? $this->ruDomains : [];
+        $us = (c('Registration.BlockFreeEmails.US')) ? $this->usDomains : [];
+
+        return array_merge($cn, $ru, $us);
+    }
 
     /**
      * Block registration of users with a known burner email domain.
@@ -57,8 +95,9 @@ class BurnerBlockerPlugin extends Gdn_Plugin {
         $email = val('Email', $args['RegisteringUser']);
         $domain = substr($email, strpos($email, '@')+1);
 
-        // Check the user's email domain against our private list.
-        if (in_array($domain, $this->bannedDomains)) {
+        // Check the user's email domain against our private lists.
+        $domainList = array_merge($this->getBurnerDomains(), $this->getFreemailDomains());
+        if (in_array($domain, $domainList)) {
             // Block the registration.
             $args['Valid'] = false;
             // Provide matching error as if a ban rule was invoked.
