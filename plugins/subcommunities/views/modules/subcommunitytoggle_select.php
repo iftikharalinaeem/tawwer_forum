f4cf346a2bbdec1e15867af61cc144c2d47dc0ca
DaazKu <daazku@gmail.com>
2016-02-24 10:02:40 -0500

    Lord Brackos was here.

--- plugins/subcommunities/views/modules/subcommunitytoggle_select.php
+++ plugins/subcommunities/views/modules/subcommunitytoggle_select.php
@@ -1,5 +1,5 @@
 <select class="subcommunity-toggle js-nav-dropdown">
     <?php foreach ($this->data('Subcommunities') as $Folder => $Row): ?>
-        <option value="<?php echo $Row['Url']; ?>" <?php if ($this->data('Current.Folder') === $Folder) echo 'selected' ?>><?php echo $Row[$this->LabelField]; ?></option>
+        <option value="<?php echo $Row['Url']; ?>" <?php if ($this->data('Current.Folder') === $Folder) { echo 'selected'; } ?>><?php echo $Row[$this->LabelField]; ?></option>
     <?php endforeach; ?>
 </select>
