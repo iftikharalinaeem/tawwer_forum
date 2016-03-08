02c59b30c348b74391ff9ceb315bf9d1482f3566
beckyvb <beckyvanbussel@gmail.com>
2016-03-08 11:42:34 -0500

    Show reactions by default.

--- plugins/Reactions/class.reactions.plugin.php
+++ plugins/Reactions/class.reactions.plugin.php
@@ -19,7 +19,7 @@
 $PluginInfo['Reactions'] = array(
     'Name' => 'Reactions',
     'Description' => "Adds reaction options to discussions & comments.",
-    'Version' => '1.4.0',
+    'Version' => '1.4.1',
     'RequiredApplications' => array('Vanilla' => '2.1'),
     'RegisterPermissions' => array(
         'Reactions.Positive.Add' => 'Garden.SignIn.Allow',
