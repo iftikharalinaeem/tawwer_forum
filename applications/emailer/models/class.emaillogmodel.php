6da05006db214f2e9271f14d2e274fd3beb160f0
Todd Burry <todd@vanillaforums.com>
2016-03-10 11:06:08 -0500

    Make the email log prune after 90 days

--- applications/emailer/models/class.emaillogmodel.php
+++ applications/emailer/models/class.emaillogmodel.php
@@ -8,19 +8,61 @@
 class EmailLogModel extends Gdn_Model {
 
     /**
+     * @var string The amount of time to delete logs after.
+     */
+    protected $pruneAfter;
+
+    /**
      * EmailLogModel constructor.
      */
     public function __construct() {
         parent::__construct('EmailLog');
+        $this->setPruneAfter('-90 days');
     }
 
     /**
-     * {@inheritdoc}
+     * Get the delete after time.
+     *
+     * @return string Returns a string compatible with {@link strtotime()}.
      */
-    private function serializeFields(&$fields) {
-        if (isset($fields['Post']) && is_array($fields['Post'])) {
-            $fields['Post'] = json_encode($fields['Post'], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    public function getPruneAfter() {
+        return $this->pruneAfter;
+    }
+
+    /**
+     * Get the exact timestamp to prune.
+     *
+     * @return int Returns a timestamp or zero if no pruning should take place.
+     */
+    public function getPruneAfterTimestamp() {
+        if (!$this->pruneAfter) {
+            return 0;
+        } else {
+            return strtotime($this->pruneAfter);
         }
+    }
+
+    /**
+     * Set the deleteAfter.
+     *
+     * @param string $pruneAfter A string compatible with {@link strtotime()}. Be sure to specify a negative string.
+     * @return DbLogger Returns `$this` for fluent calls.
+     */
+    public function setPruneAfter($pruneAfter) {
+        if ($pruneAfter) {
+            // Make sure the string is negative.
+            $now = time();
+            $testTime = strtotime($pruneAfter, $now);
+            if ($testTime === false) {
+                throw new InvalidArgumentException('Invalid timespan value for "prune after".', 400);
+            }
+            if ($testTime >= $now) {
+                throw new InvalidArgumentException('You must specify a timespan in the past for "prune after".', 400);
+            }
+        }
+
+        $this->pruneAfter = $pruneAfter;
+        return $this;
     }
 
     /**
@@ -29,7 +71,27 @@ class EmailLogModel extends Gdn_Model {
     public function insert($Fields) {
         $this->serializeFields($Fields);
 
-        return parent::insert($Fields);
+        $r = parent::insert($Fields);
+
+        // Delete some old logs.
+        if ($timestamp = $this->getPruneAfterTimestamp()) {
+            $px = Gdn::sql()->Database->DatabasePrefix;
+            $sql = "delete from {$px}EmailLog where DateInserted <= :date limit 10";
+            $rd = Gdn::database()->query($sql, ['date' => Gdn_Format::toDateTime($timestamp)]);
+        }
+
+        return $r;
+    }
+
+    /**
+     * Serialize array fields before inserting into the database.
+     *
+     * @param array &$fields The fields to serialize.
+     */
+    private function serializeFields(&$fields) {
+        if (isset($fields['Post']) && is_array($fields['Post'])) {
+            $fields['Post'] = json_encode($fields['Post'], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        }
     }
 
     /**
