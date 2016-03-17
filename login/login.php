51ea4c8b7ed31585612323d2bc4a9f8645ebd8d9
DaazKu <daazku@gmail.com>
2016-03-17 14:22:23 -0400

    Add the possibility to redirect the user after login/registration

--- login/login.php
+++ login/login.php
@@ -2,40 +2,52 @@
 include_once('config.php');
 include_once('functions.php');
 
+$actionSuccess = false;
+
+ob_start();
+
 if (isset($_REQUEST['Name'])) {
-   // Try login
-   $User = GetUser($_REQUEST['Name']);
-   if ($User && $User['Hash'] === TerribleHash($_REQUEST['Password'], $User['Salt'])) {
-      echo '<p>Successful login! Cookie lasts 1 hour.</p>';
-      // 3-hour cookie
-      setcookie('TERRIBLENAME', $_REQUEST['Name'], time()+60*60);
-   } elseif (!$User) {
-      echo '<p>User not found.</p>';
-   } else {
-      echo '<p>Password doesn\'t match. Expected: '.$User['Password'].'</p>';
-   }
+    // Try login
+    $User = GetUser($_REQUEST['Name']);
+    if ($User && $User['Hash'] === TerribleHash($_REQUEST['Password'], $User['Salt'])) {
+        echo '<p>Successful login! Cookie lasts 1 hour.</p>';
+        // 3-hour cookie
+        setcookie('TERRIBLENAME', $_REQUEST['Name'], time()+60*60);
+        $actionSuccess = true;
+    } elseif (!$User) {
+        echo '<p>User not found.</p>';
+    } else {
+        echo '<p>Password doesn\'t match. Expected: '.$User['Password'].'</p>';
+    }
 }
 
 if (isset($_REQUEST['signout'])) {
-   // Goodbye!
-   echo '<p>Logged out.</p>';
-   setcookie('TERRIBLENAME', NULL, time()-1);
+    // Goodbye!
+    echo '<p>Logged out.</p>';
+    setcookie('TERRIBLENAME', NULL, time()-1);
+    $actionSuccess = true;
 }
+
+if (isset($_REQUEST['back']) && $actionSuccess) {
+    header('Location: '.$_REQUEST['back']);
+    exit;
+}
+ob_end_flush();
 
 PageHeader();
 
 if ($Name = GetLogin()) {
-   echo "<p>Welcome back, $Name! (<a href=\"login.php?signout=1\">signout</a>)</p>";
+    echo "<p>Welcome back, $Name! (<a href=\"login.php?signout=1\">signout</a>)</p>";
 }
 
 ?>
-   <h3>Login</h3>
-   <form action="login.php">
-      <ul>
-      <li><label for="Name">Name</label><input name="Name" /></li>
-      <li><label for="Password">Password</label><input name="Password" /></li>
-      </ul>
-      <input type="submit" />
-   </form><?php
+    <h3>Login</h3>
+    <form action="login.php<?php echo isset($_REQUEST['back']) ? '?back='.rawurlencode($_REQUEST['back']) : null ?>" method="post">
+        <ul>
+        <li><label for="Name">Name</label><input name="Name" /></li>
+        <li><label for="Password">Password</label><input name="Password" /></li>
+        </ul>
+        <input type="submit" />
+    </form><?php
 
 PageFooter();
