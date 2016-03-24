79ba706562fb4a185aa1ac0f27da47d0ee6bbaa9
initvector <ryan.p@vanillaforums.com>
2016-03-24 18:46:13 -0400

    Update CSS to SCSS

--- plugins/vanillaanalytics/views/settings/analytics.php
+++ plugins/vanillaanalytics/views/settings/analytics.php
@@ -1,10 +1,12 @@
 <?php if (!defined('APPLICATION')) exit(); ?>
-<h1><?php echo $this->data('Title'); ?></h1>
-<?php
-echo Gdn_Theme::module('AnalyticsToolbarModule');
-?>
-<div id="analytics_panels">
-    <?php foreach ($this->data('AnalyticsDashboard')->getPanels() as $panel): ?>
-    <ol class="analytics-panel Sortable" id="analytics_panel_<?php echo htmlspecialchars($panel->panelID); ?>"></ol>
-    <?php endforeach; ?>
+<div class="analytics-dashboard-content">
+    <h1><?php echo $this->data('Title'); ?></h1>
+    <?php
+    echo Gdn_Theme::module('AnalyticsToolbarModule');
+    ?>
+    <div id="analytics_panels">
+        <?php foreach ($this->data('AnalyticsDashboard')->getPanels() as $panel): ?>
+        <ol class="analytics-panel analytics-panel-<?php echo htmlspecialchars($panel->panelID); ?> Sortable" id="analytics_panel_<?php echo htmlspecialchars($panel->panelID); ?>"></ol>
+        <?php endforeach; ?>
+    </div>
 </div>
