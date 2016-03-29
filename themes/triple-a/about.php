b45be7b9bf9f4b3396a0c003411d463aa1bc4c74
beckyvb <beckyvanbussel@gmail.com>
2016-03-29 12:51:07 -0400

    Bring Triple-A in line with Bootstrap 3.

--- themes/triple-a/about.php
+++ themes/triple-a/about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['triple-a'] = [
   'Name'        => "Triple-A",
   'Description' => "A dark theme for Vanilla Forums with sharp accent colors and a promoted content slider. Geared towards gaming communities and the likes. Built with Bootstrap.",
-  'Version'     => '1.0.4',
+  'Version'     => '1.0.5',
   'Url'         => 'http://vanillaforums.com',
   'Author'      => "Funktionél",
   'AuthorEmail' => 'hi@funktionel.co',
