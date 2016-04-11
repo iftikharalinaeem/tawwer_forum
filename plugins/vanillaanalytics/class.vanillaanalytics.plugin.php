f1057aca88c8775cc9f546d1ef8d6f654eebdc82
lincolnwebs <lincolnwebs@gmail.com>
2016-04-11 10:48:43 -0400

    Cache break

--- plugins/vanillaanalytics/class.vanillaanalytics.plugin.php
+++ plugins/vanillaanalytics/class.vanillaanalytics.plugin.php
@@ -10,7 +10,7 @@
 $PluginInfo['vanillaanalytics'] = array(
     'Name' => 'Vanilla Analytics',
     'Description' => 'Track important trends on your forum and chart them in a customizable dashboard.',
-    'Version' => '1.0.0',
+    'Version' => '1.0.1',
     'RequiredApplications' => array('Vanilla' => '2.2.105'),
     'Author' => 'Ryan Perry',
     'AuthorEmail' => 'ryan.p@vanillaforums.com',
