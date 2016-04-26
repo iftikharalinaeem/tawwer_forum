821e8af1ba57a2a46555416f26da4cbbb07110ed
initvector <ryan.p@vanillaforums.com>
2016-04-26 19:29:04 -0400

    Query upcoming events solely on start time

--- applications/groups/controllers/class.groupcontroller.php
+++ applications/groups/controllers/class.groupcontroller.php
@@ -98,16 +98,11 @@ class GroupController extends Gdn_Controller {
       // Get Events
       $MaxEvents = C('Groups.Events.MaxList', 5);
       $EventModel = new EventModel();
-      $Events = $EventModel->SQL
-         ->where('GroupID', $GroupID)
-         ->beginWhereGroup()
-         ->where('DateEnds >=', gmdate('Y-m-d H:i:s'))
-         ->orWhere('DateStarts >=', gmdate('Y-m-d H:i:s'))
-         ->endWhereGroup()
-         ->orderBy('DateStarts', 'asc')
-         ->limit($MaxEvents)
-         ->getWhere('Event')
-         ->resultArray();
+      $Events = $EventModel->GetWhere([
+         'GroupID'      => $GroupID,
+         'DateStarts >=' => gmdate('Y-m-d H:i:s')
+         ],
+         'DateStarts', 'asc', $MaxEvents)->ResultArray();
       $this->SetData('Events', $Events);
 
       // Get applicants.
