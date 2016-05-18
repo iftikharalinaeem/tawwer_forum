db8fc1cc81748bb428ce2ae46352c2c8d0f264e3
DaazKu <daazku@gmail.com>
2016-05-18 15:37:23 -0400

    Code formatting of AdvancedStats plugin

--- plugins/AdvancedStats/views/stats.php
+++ plugins/AdvancedStats/views/stats.php
@@ -1,19 +1,21 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php
 $InstallationID = Gdn::InstallationID();
-if (!$InstallationID)
-	$InstallationID = '3950-C089DA20-C9D34048';
+if (!$InstallationID) {
+    $InstallationID = '3950-C089DA20-C9D34048';
+}
 ?>
 <div id="vanilla-comments" style="padding: 0;"></div>
 <script type="text/javascript">
-var vanilla_forum_url = '<?php echo C('Garden.Analytics.Remote','//analytics.vanillaforums.com')?>';
-var vanilla_path = '/stats/remote/<?php echo $InstallationID; ?>';
-var vanilla_lazy_load = false;
-(function() {
-   var vanilla = document.createElement('script');
-   vanilla.type = 'text/javascript';
-   var timestamp = new Date().getTime();
-   vanilla.src = vanilla_forum_url + '/js/embed.js';
-   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(vanilla);
-})();
+    var vanilla_forum_url = '<?php echo C('Garden.Analytics.Remote','//analytics.vanillaforums.com')?>';
+    var vanilla_path = '/stats/remote/<?php echo $InstallationID; ?>';
+    var vanilla_lazy_load = false;
+    (function () {
+        var vanilla = document.createElement('script');
+        vanilla.type = 'text/javascript';
+        var timestamp = new Date().getTime();
+        vanilla.src = vanilla_forum_url + '/js/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(vanilla);
+    })();
 </script>
-<noscript>Please enable JavaScript to view the <a href="http://vanillaforums.com/?ref_noscript">statistics powered by Vanilla.</a></noscript>
+<noscript>Please enable JavaScript to view the <a href="http://vanillaforums.com/?ref_noscript">statistics powered by
+        Vanilla.</a></noscript>
