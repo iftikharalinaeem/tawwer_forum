de2dfce2c082340e82156b957d9d7063ce23c49c
Alexandre Chouinard <Daazku@gmail.com>
2016-08-15 11:17:11 -0400

    Remove beta version dependency from Warnings2 plugin.

--- plugins/Warnings2/class.warnings2.plugin.php
+++ plugins/Warnings2/class.warnings2.plugin.php
@@ -10,7 +10,7 @@ $PluginInfo['Warnings2'] = array(
     'Name' => 'Warnings & Notes',
     'Description' => 'Allows moderators to warn users and add private notes to profiles to help police the community.',
     'Version' => '2.5',
-    'RequiredApplications' => ['Vanilla' => '2.1a'],
+    'RequiredApplications' => ['Vanilla' => '2.1'],
     'MobileFriendly' => true,
     'SettingsUrl' => '/settings/warnings',
     'SettingsPermission' => 'Garden.Settings.Manage',
