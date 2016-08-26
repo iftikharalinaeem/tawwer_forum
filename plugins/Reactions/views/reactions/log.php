83b30ea8ef7bd5777a157e105735041789ac5d7e
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2016-08-26 17:19:01 -0400

    Added class to react button "log" and to <td>s in react button table.

--- plugins/Reactions/views/reactions/log.php
+++ plugins/Reactions/views/reactions/log.php
@@ -6,15 +6,15 @@
       foreach ($this->Data('UserTags') as $Row): 
       ?>
       <tr class="Item">
-         <td><?php echo Gdn_Format::Date($Row['DateInserted']); ?></td>
-         <td><?php echo UserAnchor($Row); ?></td>
-         <td>
+         <td class="ReactionsLog-DateCol"><?php echo Gdn_Format::Date($Row['DateInserted']); ?></td>
+         <td class="ReactionsLog-UserCol"><?php echo UserAnchor($Row); ?></td>
+         <td class="ReactionsLog-ReactionCol">
             <?php
             $ReactionType = ReactionModel::FromTagID($Row['TagID']);
             echo htmlspecialchars($ReactionType['Name']);
             ?>
          </td>
-         <td>
+         <td class="ReactionsLog-OptionsCol">
             <div class="Options">
             <?php
             echo Anchor('&times;', "/reactions/undo/{$this->Data['RecordType']}/{$this->Data['RecordID']}/{$ReactionType['UrlCode']}?userid={$Row['UserID']}&tagid={$Row['TagID']}", 'TextColor Hijack',
