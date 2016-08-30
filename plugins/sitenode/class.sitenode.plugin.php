aa6069a7bab0f0defb5e434aec4aeda72487be5d
initvector <ryan.p@vanillaforums.com>
2016-08-30 16:17:55 -0400

    Force reporting categories to override hub

--- plugins/sitenode/class.sitenode.plugin.php
+++ plugins/sitenode/class.sitenode.plugin.php
@@ -79,6 +79,20 @@ class SiteNodePlugin extends Gdn_Plugin {
             ->Table('UserAuthenticationProvider')
             ->Column('SyncWithHub', 'tinyint(1)', '1')
             ->Set();
+
+        // Reporting categories should be managed locally. Set them to override hub, if they aren't already.
+        $hubReporting = Gdn::sql()->getWhere([
+            ['Type' => 'Reporting', 'OverrideHub' => 0],
+            'Category'
+        ])->count();
+
+        if ($hubReporting > 0) {
+            Gdn::sql()->update(
+                'Category',
+                ['OverrideHub' => 1],
+                ['Type' => 'Reporting', 'OverrideHub' => 0]
+            );
+        }
     }
 
     /**
