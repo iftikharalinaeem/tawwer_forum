7900deef4e0d29e2017669e038f9ad639db283f6
DaazKu <daazku@gmail.com>
2016-09-07 13:46:42 -0400

    Fix missing php tags due to bad merge.

--- plugins/RoleTracker/views/settings.php
+++ plugins/RoleTracker/views/settings.php
@@ -3,12 +3,15 @@
 /* @var SettingsController $this */
 echo $this->Form->open(['class' => 'RoleTracker Settings', 'enctype' => 'multipart/form-data']);
 echo $this->Form->errors();
+?>
 <div class="padded">
     <p><?php echo t('Tracked roles get extra CSS classes.', 'Discussions and comments made by users with a <b>tracked role</b> will be tagged with that role, and will receive a CSS class to allow visual customization.'); ?></p>
     <p><?php echo t('Tracked roles make content more visible.', 'This is useful for letting regular members know that a staff member has posted in a discussion, for example. <b>This functionality is not retro-active.</b>'); ?></p>
     <p><?php echo t('Choose which roles to track below.'); ?></p>
 </div>
-foreach($this->data('Roles') as $roleID => $role) { ?>
+<?php
+foreach($this->data('Roles') as $roleID => $role) {
+?>
     <div class="form-group row">
         <div class="label-wrap-wide">
             <?php echo $this->Form->label($this->Form->formData()[$roleID.'_Name'], $roleID.'_Name'); ?>
