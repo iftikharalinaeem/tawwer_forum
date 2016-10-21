aae88b41f053414b0ad4798aee83fa4d6a60276f
beckyvb <beckyvanbussel@gmail.com>
2016-10-21 14:37:13 -0400

    Use search rendering functions to render search
    Use search rendering functions to render search.

--- plugins/sitehub/views/multisites/index.php
+++ plugins/sitehub/views/multisites/index.php
@@ -9,15 +9,8 @@
 <div class="toolbar">
     <div class="toolbar-main">
         <?php
-        echo $this->form->open(['action' => url('/multisites')]);
-        echo $this->form->errors();
-        echo '<div class="search-wrap input-wrap">';
-        echo '<div class="search-icon-wrap search-icon-search-wrap">'.dashboardSymbol('search').'</div>';
-        echo $this->form->textBox('search', ['placeholder' => t('Search sites', 'Search for sites by the name of the site or its url'), 'class' => 'form-control']);
-        echo ' ', $this->form->button(t('Go'), ['name' => 'go', 'class' => 'search-submit']);
-        echo '<a class="search-icon-wrap search-icon-clear-wrap" href="'.url('/multisites').'">'.dashboardSymbol('close').'</a>';
-        echo '</div>';
-        echo $this->form->close();
+        $placeholder = t('Search sites', 'Search for sites by the name of the site or its url');
+        echo $this->form->searchForm('search', '/multisites', ['placeholder' => $placeholder]);
         ?>
     </div>
     <?php PagerModule::write(array('Sender' => $this, 'View' => 'pager-dashboard')); ?>
