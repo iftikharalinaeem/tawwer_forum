92ab221780fb62350ae8fbe94a77f8b7dd5b87e1
beckyvb <beckyvanbussel@gmail.com>
2016-10-21 14:30:18 -0400

    Add header menu tab markup to Custom Theme
    Use header menu tab markup in Custom Theme rather than anchors. Anchors shouldn't be used to control content on a page.

--- plugins/CustomTheme/js/customtheme.js
+++ plugins/CustomTheme/js/customtheme.js
@@ -1,9 +1,9 @@
 // This file contains javascript that is specific to the dashboard/profile controller.
 jQuery(document).ready(function($) {
    // Load tab content on tab-click
-   $('.js-custom-theme-menu a').click(function() {
-      $('.js-custom-theme-menu a').removeClass('active');
-      $('.js-custom-theme-menu a').attr('aria-selected', 'false');
+   $('.js-custom-theme-menu .header-menu-item').click(function() {
+      $('.js-custom-theme-menu .header-menu-item').removeClass('active');
+      $('.js-custom-theme-menu .header-menu-item').attr('aria-selected', 'false');
       $(this).addClass('active');
       $(this).attr('aria-selected', 'true');
       if ($(this).hasClass('js-custom-html')) {
