55c33f51f787dd07e6830651b1ee0faa019dd8a6
DaazKu <daazku@gmail.com>
2016-10-25 16:48:03 -0400

    Revert "Remove extra space after quote"
    This reverts commit 6e0930b4285599d711bca06f82cd015b277c2b68.

--- plugins/Warnings2/class.warnings2.plugin.php
+++ plugins/Warnings2/class.warnings2.plugin.php
@@ -310,11 +310,12 @@ class Warnings2Plugin extends Gdn_Plugin {
         }
 
         if ($quote) {
-            $issuer = Gdn::userModel()->getID($warning['InsertUserID'], DATASET_TYPE_ARRAY);
+            $content = sprintf(T('Re: %s'), "{$location}<br/>{$context}");
 
-            $content = sprintf(T('Re: %s'), "{$location}<br>{$context}");
+            $issuer = Gdn::userModel()->getID($warning['InsertUserID'], DATASET_TYPE_ARRAY);
+            $content .= "<br/>";
             $content .= wrap(T('Moderator'), 'strong').' '.userAnchor($issuer);
-            $content .= "<br>";
+            $content .= "<br/>";
             $content .= wrap(T('Points'), 'strong').' '.$warning['Points'];
 
             echo wrap($content, 'div', array(
@@ -359,7 +360,7 @@ class Warnings2Plugin extends Gdn_Plugin {
                 $emailTemplate = $email->getEmailTemplate();
                 $message = $emailTemplate->getMessage();
 
-                $message .= '<br>'.t('Post that triggered the warning:').formatQuote($record, false);
+                $message .= '<br>'.t('Post that triggered the warning:').'<br>'.formatQuote($record, false);
                 $emailTemplate->setMessage($message);
             }
         }
