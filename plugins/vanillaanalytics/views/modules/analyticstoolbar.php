e6e58ce750a346ef5aaf2b8791ec3065f4721d20
DaazKu <daazku@gmail.com>
2016-11-23 17:51:43 -0500

    Fix casening of parameters

--- plugins/vanillaanalytics/views/modules/analyticstoolbar.php
+++ plugins/vanillaanalytics/views/modules/analyticstoolbar.php
@@ -5,8 +5,13 @@
     echo '<div class="filter-content">'.$form->dropDown('cat01', $this->data('cat01'), ['IncludeNull' => 'All Categories']).'</div>';
     echo '<div class="btn-group">';
     foreach($this->data('Intervals') as $interval) {
-        echo wrap(val('Text', $interval), 'div', ['class' => 'js-analytics-interval btn btn-secondary', 'data-seconds' => val('data-seconds', $interval), 'data-interval' => strtolower(val('Text', $interval))]);
-//        echo '<div class="filter-interval">'.$form->dropDown('intervals', $this->data('Intervals'), ['value' => 'daily']).'</div>';
+        echo wrap(val('text', $interval), 'div',
+            [
+                'class' => 'js-analytics-interval btn btn-secondary',
+                'data-seconds' => val('data-seconds', $interval),
+                'data-interval' => strtolower(val('text', $interval))
+            ]
+        );
     }
     echo '</div>';
     echo '<div class="filter-date">'.$form->textBox('dates', ['class' => 'js-date-range form-control']).'</div>';
