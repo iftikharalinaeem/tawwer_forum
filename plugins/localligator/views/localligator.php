eb1a88a2408618be60acf73908f686f6d6510c3a
beckyvb <beckyvanbussel@gmail.com>
2016-11-24 15:19:47 -0500

    Use checkbox hack to render checkboxes.

--- plugins/localligator/views/localligator.php
+++ plugins/localligator/views/localligator.php
@@ -47,16 +47,30 @@ if (empty($strings)) : ?>
         <table class="table-data js-tj">
             <thead>
             <tr>
-                <th class="column-checkbox" data-tj-ignore="true"><input class="js-check-all" type="checkbox"></th>
+                <th class="column-checkbox" data-tj-ignore="true">
+                    <div class="checkbox-painted-wrapper checkbox">
+                        <input class="js-check-all" type="checkbox" id="CheckAll">
+                        <label for="CheckAll"><?php echo t('Check All'); ?></label>
+                    </div>
+                </th>
                 <th class="column-lg" data-tj-main="true"><?php echo t('Translation Code'); ?></th>
                 <th class="column-xl"><?php echo t('Default Translation'); ?></th>
             </tr>
             </thead>
             <tbody>
             <?php foreach ($strings as $string) :
-                $attr = ['value' => val('value', $string), 'class' => val('class', $string)]; ?>
+                $attr = [
+                    'value' => val('value', $string),
+                    'class' => val('class', $string),
+                    'display' => 'after'
+                ]; ?>
                 <tr>
-                    <td><?php echo $form->checkBox('StringsToAdd[]', '', $attr); ?></td>
+                    <td>
+                        <?php
+                        $class = 'checkbox-painted-wrapper';
+                        echo wrap($form->checkBox('StringsToAdd[]', val('title', $string), $attr), 'div', ['class' => $class]);
+                        ?>
+                    </td>
                     <td><?php echo val('title', $string); ?></td>
                     <td><?php echo val('description', $string) ?></td>
                 </tr>
