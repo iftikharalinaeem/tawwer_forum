f3a34c1c042e44c9cf7add535b97b9c884538344
Alexandre Chouinard <Daazku@gmail.com>
2016-11-28 15:24:44 -0500

    Use the debug() function instead of checking the config

--- plugins/vanillaanalytics/models/class.keenioclient.php
+++ plugins/vanillaanalytics/models/class.keenioclient.php
@@ -203,7 +203,7 @@ class KeenIOClient extends Garden\Http\HttpClient {
 
             return json_decode($result);
         } catch (Exception $e) {
-            if (c('Debug')) {
+            if (debug()) {
                 print_r('DEBUG:'. $e->getMessage());
             }
             Logger::event('vanilla_analytics', Logger::ERROR, $e->getMessage());
