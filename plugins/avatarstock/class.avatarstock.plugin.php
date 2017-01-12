b3765a3726dab8c8c36339a90168b69d4a266eec
beckyvb <beckyvanbussel@gmail.com>
2017-01-12 15:57:16 -0500

    Remove config fallback.

--- plugins/avatarstock/class.avatarstock.plugin.php
+++ plugins/avatarstock/class.avatarstock.plugin.php
@@ -332,7 +332,7 @@ class AvatarStockPlugin extends Gdn_Plugin {
             );
 
             // Create n thumbnail (cropped)
-            $crop_dimensions = c('Garden.Thumbnail.Size', 120);
+            $crop_dimensions = c('Garden.Thumbnail.Size');
             $crop_parsed = $upload_image->SaveImageAs(
                 $tmp_file,
                 $path_thumb_n,
@@ -492,7 +492,7 @@ class AvatarStockPlugin extends Gdn_Plugin {
                 $UserPhoto = sprintf($Props['SaveFormat'], "userpics/$Subdir/$Basename");
 
                 // Save the uploaded image in thumbnail size
-                $ThumbSize = c('Garden.Thumbnail.Size', 120);
+                $ThumbSize = c('Garden.Thumbnail.Size');
                 $UploadImage->SaveImageAs(
                     $TmpImage,
                     "userpics/$Subdir/n$Basename",
