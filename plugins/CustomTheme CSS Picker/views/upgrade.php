b7b109454ec91fbb28309ac53692979154f5a6bd
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-03-15 10:12:06 -0400

    Removed unused plugin

--- plugins/CustomTheme CSS Picker/views/upgrade.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('APPLICATION')) exit(); ?>
-<h1>Upgrade</h1>
-<div class="Legal">
-   <p>
-      You must upgrade to a paid plan in order to apply theme customizations.
-      Click your "Account" link on the top-right of the screen to upgrade now.
-   </p>
-</div>
