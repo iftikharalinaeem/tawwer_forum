3150d2661b0b2f0d0b029dc7b54549a6b41c1786
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-03-27 11:39:39 -0400

    Added “disabled” page

---
+++ plugins/CustomTheme/views/disabled.php
@@ -0,0 +1,9 @@
+<?php if (!defined('APPLICATION')) exit; ?>
+
+<h1><?php echo t('Disabled'); ?></h1>
+
+<div class="padded">
+   <p>
+      <?php echo t('This feature is disabled. Contact your CSM for more info.'); ?>
+   </p>
+</div>
