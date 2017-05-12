ccb4eb6d420fff75f46fa19ac79778a0fa293530
DaazKu <daazku@gmail.com>
2017-05-12 11:29:25 -0400

    Convert addons info array to addon.json

--- plugins/vanillaanalytics/class.vanillaanalytics.plugin.php
+++ plugins/vanillaanalytics/class.vanillaanalytics.plugin.php
@@ -7,16 +7,6 @@
  * @package vanillaanalytics
  */
 
-$PluginInfo['vanillaanalytics'] = [
-    'Name' => 'Vanilla Analytics',
-    'Description' => 'Track important trends on your forum and chart them in a customizable dashboard.',
-    'Version' => '1.0.4',
-    'RequiredApplications' => array('Vanilla' => '2.2.103'),
-    'Author' => 'Ryan Perry',
-    'AuthorEmail' => 'ryan.p@vanillaforums.com',
-    'AuthorUrl' => 'http://vanillaforums.org/profile/initvector'
-];
-
 /**
  * Facilitate the tracking of events from Vanilla to one (or more) analytics services.
  */
