bcda5d357a6c00dbea1101e5f75f93f858b82938
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-07-18 14:53:34 -0400

    Reworked JS to automatically add a new line when there are no more empty inputs and allow enter to submit form

--- plugins/Polls/js/polls.js
+++ plugins/Polls/js/polls.js
@@ -1,25 +1,40 @@
 jQuery(document).ready(function($) {
+    var emptySpace = /^\s*$/g;
+    var $template = $('.NewPollForm:first').find('.PollOption:first').clone();
+    $template.find('.InputBox').val(''); // Reset value just in case it's not empty
+
+    function cleanUpEmptyInputs($form) {
+        $form.find('.InputBox').each(function(){
+            if($(this).val().match(emptySpace)) {
+                $(this).closest('.PollOption').remove();
+            }
+        });
+    }
+
+    function addEmptyField($pollOptions, $template) {
+        $pollOptions.append($template.clone()).find('.InputBox').focus();
+    }
+
+    $('.AddPollOption').on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        addEmptyField($(this).closest('.NewPollForm').find('.PollOptions'), $template);
+    });
 
     $('.NewPollForm').each(function(){
         var $form = $(this);
-        var $template = $form.find('.PollOption:first').clone(false);
-        $template.find('.InputBox').val(''); // Reset value just in case it's not empty
         var $pollOptions = $form.find('.PollOptions');
+        cleanUpEmptyInputs($form);
+        addEmptyField($pollOptions, $template);
 
-        $form.find('.AddPollOption').on('click', function(e){
-            e.stopPropagation();
-            e.preventDefault();
-            $pollOptions.append($template.clone());
-            $pollOptions.find('.PollOption:last .InputBox').focus();
+        $form.on('keypress', '.InputBox', function(e){
+            if ($(this).val() != "" && $pollOptions.find('.PollOption:last-child .InputBox').val() != "") {
+                $pollOptions.append($template.clone());
+            }
         });
 
-        // Block new lines if current line is empty
-        $form.on('keypress', '.InputBox', function(e){
-            var isEnterKey = (e.keyCode || e.which) == 13;
-            if (isEnterKey && $(this).val() == "") {
-                e.stopPropagation();
-                e.preventDefault();
-            }
+        $form.on('submit', function(){
+            cleanUpEmptyInputs($form);
         });
     });
 
@@ -40,11 +55,14 @@ jQuery(document).ready(function($) {
             gdn.informError(xhr);
          },
          success: function(json) {
+             console.log("json: ", json);
             $('.PollForm').replaceWith(json.PollHtml);
             gdn.inform(json);
             return false;
          },
          complete: function(XMLHttpRequest, textStatus) {
+             console.log("XMLHttpRequest: ", XMLHttpRequest);
+             console.log("textStatus: ", textStatus);
             $('.InProgress').removeClass('InProgress'); // just in case
          }
       });
