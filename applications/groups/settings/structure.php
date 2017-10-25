0123a5b9bea8c9025292a854f2b4e927e63e8822
DaazKu <daazku@gmail.com>
2017-10-25 12:17:33 -0400

    Allow Group's structure to be run without needing a valid session

--- applications/groups/settings/structure.php
+++ applications/groups/settings/structure.php
@@ -17,7 +17,8 @@ $St = Gdn::structure();
 
 Gdn::permissionModel()->define([
     'Groups.Group.Add' => 'Garden.Profiles.Edit',
-    'Groups.Moderation.Manage' => 'Garden.Moderation.Manage']);
+    'Groups.Moderation.Manage' => 'Garden.Moderation.Manage',
+]);
 
 // Define the groups table.
 $St->table('Group');
@@ -99,7 +100,10 @@ if ($St->tableExists('Category')) {
                 'DisplayAs' => 'Discussions',
                 'AllowDiscussions' => 1,
                 'AllowGroups' => 1,
-                'Sort' => 1000
+                'Sort' => 1000,
+                // The model requires these 2 fields and there is nothing that guarantee that a user will be logged in.
+                'DateInserted' => date(MYSQL_DATE_FORMAT),
+                'InsertUserID' => Gdn::session()->isValid() ? Gdn::session()->UserID : 0,
             ];
             // Backwards compat for a new column.
             if ($St->columnExists('CanDelete')) {
