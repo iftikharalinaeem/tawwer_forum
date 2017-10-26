dcb9a7d0e405fd184a7229bfb0ae56fbcc325def
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-10-26 11:15:30 -0400

    Added a proper class on poll options text inputs Refactored javascript for Polls to delegate their click events, so we don't need to worry about document ready or contentLoad

--- plugins/Polls/js/polls.js
+++ plugins/Polls/js/polls.js
@@ -1,64 +1,59 @@
-$(document).on("contentLoad", function() {
-    var emptySpace = /^\s*$/g;
+function getPollsInputTemplate() {
     var $template = $('.NewPollForm:first .PollOption:last-child').clone();
-    $template.find('.InputBox').val(''); // Reset value just in case it's not empty
+    $template.find('.InputBox').val('');
+    return $template;
+}
 
+$(document).on('click', '.AddPollOption', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).closest('.NewPollForm').find('.PollOptions').append(getPollsInputTemplate()).find('.InputBox').focus();
+});
 
-    $('.AddPollOption').on('click', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $(this).closest('.NewPollForm').find('.PollOptions').append($template.clone()).find('.InputBox').focus();
+$(document).on('input', '.PollOptionInput', function(e){
+    var $pollOptions = $(this).closest('.PollOptions');
+    if ($(this).val() != "" && $pollOptions.find('.PollOption:last-child .InputBox').val() != "") {
+        $pollOptions.append(getPollsInputTemplate());
+    }
+});
 
-    });
+$(document).on('click', '.PollForm form :submit', function() {
+    var btn = this,
+        frm = $(this).parents('form');
 
-    $('.NewPollForm').each(function(){
-        var $form = $(this);
-        var $pollOptions = $form.find('.PollOptions');
+    var postValues = $(frm).serialize()+'&DeliveryType=VIEW&DeliveryMethod=JSON',
+        action = $(frm).attr('action');
 
-        $form.on('input', '.InputBox', function(e){
-            if ($(this).val() != "" && $pollOptions.find('.PollOption:last-child .InputBox').val() != "") {
-                $pollOptions.append($template.clone());
-            }
-        });
-    });
-
-   $(document).on('click', '.PollForm form :submit', function() {
-      var btn = this,
-         frm = $(this).parents('form');
-
-      var postValues = $(frm).serialize()+'&DeliveryType=VIEW&DeliveryMethod=JSON',
-         action = $(frm).attr('action');
-
-      $(btn).addClass('InProgress'); // Replace the button with a spinner
-      $.ajax({
-         type: "POST",
-         url: action,
-         data: postValues,
-         dataType: 'json',
-         error: function(xhr) {
+    $(btn).addClass('InProgress'); // Replace the button with a spinner
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: postValues,
+        dataType: 'json',
+        error: function(xhr) {
             gdn.informError(xhr);
-         },
-         success: function(json) {
+        },
+        success: function(json) {
             $('.PollForm').replaceWith(json.PollHtml);
             gdn.inform(json);
             return false;
-         },
-         complete: function(XMLHttpRequest, textStatus) {
+        },
+        complete: function(XMLHttpRequest, textStatus) {
             $('.InProgress').removeClass('InProgress'); // just in case
-         }
-      });
-      return false;
-   });
-
-    $(".js-poll-result-btn").click(function() {
-
-        if ($(".js-poll-results").css('display') == 'none') {
-            $(".js-poll-form").hide();
-            $(".js-poll-results").show();
-        } else {
-            $(".js-poll-form").show();
-            $(".js-poll-results").hide();
         }
-        return false;
     });
+    return false;
+});
+
+$(document).on('click', '.js-poll-result-btn', function() {
+    var $poll = $(this).closest('.Item');
+
+    if (!$poll.find(".js-poll-results").is(':visible')) {
+        $poll.find(".js-poll-form").hide();
+        $poll.find(".js-poll-results").show();
+    } else {
+        $poll.find(".js-poll-form").show();
+        $poll.find(".js-poll-results").hide();
+    }
+    return false;
 });
