5b93859a68d25a1b76d670395f2a7332e958a298
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-10-30 16:01:04 -0400

    Fixed spacing issues

--- plugins/Warnings2/views/settings.php
+++ plugins/Warnings2/views/settings.php
@@ -3,13 +3,11 @@
 <h1><?php echo t($this->data('Title')); ?></h1>
 
 <div class="padded">
-    <p class="Info">
-        <?php echo t('Warning types description',
-                        'Users are banned when they reach a total of 5 (non expired) points.<br>'
-                        .'Editing a warning type will not affect any previous warning.'
-                    )
-        ?>
-    </p>
+    <?php echo t('Warning types description',
+                    'Users are banned when they reach a total of 5 (non expired) points.<br>'
+                    .'Editing a warning type will not affect any previous warning.'
+                )
+    ?>
 </div>
 
 <?php
@@ -28,39 +26,40 @@
                     'months' => 'months',
                 ];
 
+                echo '<div class="padded">';
                 echo anchor(t('Add warning type'), '/warningtypes/add', 'Popup SmallButton');
+                echo '</div>';
             ?>
         </div>
+        <table id="WarningsTable" class="table-data js-tj">
+            <thead>
+                <tr>
+                    <th class="column-sm"><?php echo t('Name')?></th>
+                    <th class="column-lg"><?php echo t('Description')?></th>
+                    <th class="column-sm"><?php echo t('Points')?></th>
+                    <th class="column-sm"><?php echo t('Expiration')?></th>
+                    <th class="column-md"><?php echo t('Options')?></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+                $warnings = $this->data('Warnings');
+                foreach($this->data('Warnings') as $warningID => $warning) {
+                    echo '<tr>';
+                    echo    '<td>'.htmlentities($warning['Name']).'</td>';
+                    echo    '<td>'.htmlentities($warning['Description']).'</td>';
+                    echo    '<td>'.$warning['Points'].'</td>';
+                    echo    '<td>'.$warning['ExpireNumber'].' '.$warning['ExpireType'].'</td>';
+                    echo    '<td>'
+                                .anchor(t('Edit'), '/warningtypes/edit/'.$warningID, 'Popup SmallButton')
+                                . ' &middot; '
+                                .anchor(t('Delete'), '/warningtypes/delete/'.$warningID, 'Popup SmallButton')
+                            .'</td>';
+                    echo "</tr>\n";
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
-
-    <table id="WarningsTable" class="table-data js-tj">
-        <thead>
-            <tr>
-                <th class="column-sm"><?php echo t('Name')?></th>
-                <th class="column-lg"><?php echo t('Description')?></th>
-                <th class="column-sm"><?php echo t('Points')?></th>
-                <th class="column-sm"><?php echo t('Expiration')?></th>
-                <th class="column-md"><?php echo t('Options')?></th>
-            </tr>
-        </thead>
-
-        <tbody>
-            <?php
-            $warnings = $this->data('Warnings');
-            foreach($this->data('Warnings') as $warningID => $warning) {
-                echo '<tr>';
-                echo    '<td>'.htmlentities($warning['Name']).'</td>';
-                echo    '<td>'.htmlentities($warning['Description']).'</td>';
-                echo    '<td>'.$warning['Points'].'</td>';
-                echo    '<td>'.$warning['ExpireNumber'].' '.$warning['ExpireType'].'</td>';
-                echo    '<td>'
-                            .anchor(t('Edit'), '/warningtypes/edit/'.$warningID, 'Popup SmallButton')
-                            . ' &middot; '
-                            .anchor(t('Delete'), '/warningtypes/delete/'.$warningID, 'Popup SmallButton')
-                        .'</td>';
-                echo "</tr>\n";
-            }
-            ?>
-        </tbody>
-    </table>
 <?php echo $this->Form->close();
