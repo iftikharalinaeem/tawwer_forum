1905a09b70fe932cfe4db50685eed8f81d366bcb
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-10-30 15:45:52 -0400

    Fixed table styling for settings page

--- plugins/Warnings2/views/settings.php
+++ plugins/Warnings2/views/settings.php
@@ -2,42 +2,48 @@
 
 <h1><?php echo t($this->data('Title')); ?></h1>
 
-<div class="Info">
-    <?php echo t('Warning types description',
-                    'Users are banned when they reach a total of 5 (non expired) points.<br>'
-                    .'Editing a warning type will not affect any previous warning.'
-                )
-    ?>
+<div class="padded">
+    <p class="Info">
+        <?php echo t('Warning types description',
+                        'Users are banned when they reach a total of 5 (non expired) points.<br>'
+                        .'Editing a warning type will not affect any previous warning.'
+                    )
+        ?>
+    </p>
 </div>
 
 <?php
     /* @var SettingsController $this */
     echo $this->Form->open(['class' => 'Warnings Settings', 'enctype' => 'multipart/form-data']);
 ?>
-    <div class="Wrap">
-        <?php
-            echo $this->Form->errors();
+    <div class="padded">
+        <div class="Wrap">
+            <?php
+                echo $this->Form->errors();
 
-            $durationPeriods = [
-                'hours' => 'hours',
-                'days' => 'days',
-                'weeks' => 'weeks',
-                'months' => 'months',
-            ];
+                $durationPeriods = [
+                    'hours' => 'hours',
+                    'days' => 'days',
+                    'weeks' => 'weeks',
+                    'months' => 'months',
+                ];
 
-            echo anchor(t('Add warning type'), '/warningtypes/add', 'Popup SmallButton');
-        ?>
+                echo anchor(t('Add warning type'), '/warningtypes/add', 'Popup SmallButton');
+            ?>
+        </div>
     </div>
-    <table cellspacing="0" id="WarningsTable">
+
+    <table id="WarningsTable" class="table-data js-tj">
         <thead>
             <tr>
-                <th><?php echo t('Name')?></th>
-                <th><?php echo t('Description')?></th>
-                <th><?php echo t('Points')?></th>
-                <th><?php echo t('Expiration')?></th>
-                <th><?php echo t('Options')?></th>
+                <th class="column-sm"><?php echo t('Name')?></th>
+                <th class="column-lg"><?php echo t('Description')?></th>
+                <th class="column-sm"><?php echo t('Points')?></th>
+                <th class="column-sm"><?php echo t('Expiration')?></th>
+                <th class="column-md"><?php echo t('Options')?></th>
             </tr>
         </thead>
+
         <tbody>
             <?php
             $warnings = $this->data('Warnings');
@@ -49,6 +55,7 @@
                 echo    '<td>'.$warning['ExpireNumber'].' '.$warning['ExpireType'].'</td>';
                 echo    '<td>'
                             .anchor(t('Edit'), '/warningtypes/edit/'.$warningID, 'Popup SmallButton')
+                            . ' &middot; '
                             .anchor(t('Delete'), '/warningtypes/delete/'.$warningID, 'Popup SmallButton')
                         .'</td>';
                 echo "</tr>\n";
