939c9bacba4baba6f262c97eb6a48ef343beb76c
DaazKu <daazku@gmail.com>
2017-11-03 14:19:19 -0400

    Add EventsApiController and its unit tests

---
+++ applications/groups/controllers/api/EventsApiController.php
@@ -0,0 +1,587 @@
+<?php
+/**
+ * @author Alexandre (DaazKu) Chouinard <alexandre.c@vanillaforums.com>
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
+ */
+
+use Garden\Schema\Schema;
+use Garden\Web\Exception\ClientException;
+use Garden\Web\Exception\NotFoundException;
+use Garden\Web\Exception\ServerException;
+use Vanilla\Utility\CapitalCaseScheme;
+use Vanilla\Utility\CamelCaseScheme;
+
+/**
+ * API Controller for the `/events` resource.
+ */
+class EventsApiController extends AbstractApiController {
+
+    /** @var CapitalCaseScheme */
+    private $capitalCaseScheme;
+
+    /** @var CamelCaseScheme */
+    private $camelCaseScheme;
+
+    /** @var EventModel */
+    private $eventModel;
+
+    /** @var GroupModel */
+    private $groupModel;
+
+    /** @var UserModel */
+    private $userModel;
+
+    /**
+     * ConversationsApiController constructor.
+     *
+     * @param EventModel $eventModel
+     * @param GroupModel $groupModel
+     * @param UserModel $userModel
+     */
+    public function __construct(
+        EventModel $eventModel,
+        GroupModel $groupModel,
+        UserModel $userModel
+    ) {
+        $this->camelCaseScheme = new CamelCaseScheme();
+        $this->capitalCaseScheme = new CapitalCaseScheme();
+        $this->eventModel = $eventModel;
+        $this->groupModel = $groupModel;
+        $this->userModel = $userModel;
+    }
+
+    /**
+     * Delete an event.
+     *
+     * @param int $id The ID of the event.
+     * @throws ClientException
+     */
+    public function delete($id) {
+        $this->permission('Garden.SignIn.Allow');
+
+        $this->idParamEventSchema()->setDescription('Delete a group.');
+        $this->schema([], 'out');
+
+        $row = $this->eventByID($id);
+
+        if ($row['InsertUserID'] !== $this->getSession()->UserID && !$this->eventModel->checkPermission('Delete', $id)) {
+            throw new ClientException('You do not have the rights to delete this event.');
+        }
+
+        // EventModel->deleteID() won't do here.
+        $this->eventModel->delete(['EventID' => $id]);
+    }
+
+    /**
+     * Get an event by its ID.
+     *
+     * @param int $id The event ID.
+     * @throws NotFoundException If the event could not be found.
+     * @return array
+     */
+    public function eventByID($id) {
+        $row = $this->eventModel->getID($id, DATASET_TYPE_ARRAY);
+        if (!$row) {
+            throw new NotFoundException('Event');
+        }
+
+        return $row;
+    }
+
+    /**
+     * Get a schema instance comprised of all available event participant fields.
+     *
+     * @return Schema Returns a schema object.
+     */
+    public function fullEventParticipantSchema() {
+        /** @var Schema $schema */
+        static $schema;
+
+        if ($schema === null) {
+            // Name this schema so that it can be read by swagger.
+            $schema = $this->schema([
+                'eventID:i' => 'The ID of the event.',
+                'userID:i' => 'The user ID of the participant.',
+                'user?' => $this->getUserFragmentSchema(),
+                'dateInserted:dt' => 'When the event was created.',
+                'attending:s|n' => [
+                    'enum' => ['yes', 'no', 'maybe'],
+                    'description' => 'Is the participant attending the event.',
+                ]
+            ], 'Event');
+        }
+
+        return $schema;
+    }
+
+    /**
+     * Get a schema instance comprised of all available event fields.
+     *
+     * @return Schema Returns a schema object.
+     */
+    public function fullEventSchema() {
+        /** @var Schema $schema */
+        static $schema;
+
+        if ($schema === null) {
+            // Name this schema so that it can be read by swagger.
+            $schema = $this->schema([
+                'eventID:i' => 'The ID of the event.',
+                'groupID:i' => 'The group the event is in.',
+                'name:s' => 'The name of the event.',
+                'body:s' => 'The description of the event.',
+                'format:s' => 'The input format of the event.',
+                'location:s|null' => [
+                    'maxLength' => 255,
+                    'description' => 'The location of the event.'
+                ],
+                'dateStarts:dt' => 'When the event starts.',
+                'dateEnds:dt|n' => 'When the event ends.',
+                'dateInserted:dt' => 'When the event was created.',
+                'insertUserID:i' => 'The user that created the event.',
+                'insertUser?' => $this->getUserFragmentSchema(),
+                'dateUpdated:dt|n' => 'When the event was updated.',
+                'updateUserID:i|n' => 'The user that updated the event.',
+                'updateUser?' => $this->getUserFragmentSchema(),
+                'url:s' => 'The full URL to the event.',
+            ], 'Event');
+        }
+
+        return $schema;
+    }
+
+    /**
+     * Get an event.
+     *
+     * @param int $id The ID of the event.
+     * @throws Exception
+     * @return array
+     */
+    public function get($id) {
+        $this->permission();
+
+        $this->idParamEventSchema()->setDescription('Get an event.');
+        $out = $this->schema($this->fullEventSchema(), 'out');
+
+        $row = $this->eventByID($id);
+
+        if (!$this->eventModel->checkPermission('View', $id)) {
+            throw new ClientException('You do not have the rights to view this event.');
+        }
+
+        $this->userModel->expandUsers($row, ['InsertUserID', 'UpdateUserID']);
+
+        $row = $this->normalizeEventOutput($row);
+        return $out->validate($row);
+    }
+
+    /**
+     * List event's participants.
+     *
+     * @param int $id The ID of the event.
+     * @param array $query
+     * @throws Exception
+     * @return array
+     */
+    public function get_participants($id, array $query) {
+        $this->permission();
+
+        $this->idParamEventSchema();
+        $in = $this->schema([
+            'attending:s?' => [
+                'default' => 'all',
+                'enum' => ['yes', 'no', 'maybe', 'answered', 'unanswered', 'all'],
+                'description' => 'Filter participant by attending status.',
+            ],
+            'page:i?' => [
+                'description' => 'Page number.',
+                'default' => 1,
+                'minimum' => 1,
+            ],
+            'limit:i?' => [
+                'description' => 'The number of items per page.',
+                'default' => $this->eventModel->getDefaultLimit(),
+                'minimum' => 1,
+                'maximum' => 100,
+            ],
+            'expand:b?' => 'Expand associated records.',
+        ], 'in')->setDescription('Get event\'s participants.');
+        $out = $this->schema([':a' => $this->fullEventParticipantSchema()], 'out');
+
+        $query = $in->validate($query);
+
+        $participantData = $this->normalizeEventParticipantInput($query);
+
+        // Paging
+        list($offset, $limit) = offsetLimit("p{$query['page']}", $query['limit']);
+
+        // Filters
+        $where = [];
+        if (array_key_exists('Attending', $participantData)) {
+            if ($participantData['Attending'] === 'Answered') {
+                $where['Attending !='] = 'Invited';
+            } else if ($participantData['Attending'] === 'Unanswered') {
+                $where['Attending'] = 'Invited';
+            }
+        }
+
+        // Data
+        $rows = [];
+        if ($where !== null) {
+            $rows = $this->eventModel->getInvitedUsers($id, $where, '', 'asc', $limit, $offset);
+        }
+
+        if (!empty($query['expand'])) {
+            $this->userModel->expandUsers($rows, ['UserID']);
+        }
+        foreach ($rows as &$row) {
+            $row = $this->normalizeEventParticipantOutput($row);
+        }
+
+        $result = $out->validate($rows);
+        return $result;
+    }
+
+    /**
+     * Get an event for editing.
+     *
+     * @throws NotFoundException if the event could not be found.
+     * @throws ClientException
+     * @param int $id The ID of the event.
+     * @return array
+     */
+    public function get_edit($id) {
+        $this->permission('Garden.SignIn.Allow');
+
+        $in = $this->idParamEventSchema()->setDescription('Get an event for editing.');
+        $out = $this->schema(
+            Schema::parse(['eventID', 'groupID', 'name', 'body', 'format', 'location', 'dateStarts', 'dateEnds'])
+                ->add($this->fullEventSchema()),
+            'out'
+        );
+
+        $row = $this->eventByID($id);
+
+        if (!$this->eventModel->checkPermission('View', $id)) {
+            throw new ClientException('You do not have the rights to view this event.');
+        }
+
+        $result = $out->validate($row);
+        return $result;
+    }
+
+    /**
+     * Get a group by its ID.
+     *
+     * @param int $id The group ID.
+     * @throws NotFoundException If the group could not be found.
+     * @return array
+     */
+    public function groupByID($id) {
+        $row = $this->groupModel->getID($id, DATASET_TYPE_ARRAY);
+        if (!$row) {
+            throw new NotFoundException('Group');
+        }
+
+        return $row;
+    }
+
+    /**
+     * Get an ID-only event record schema.
+     *
+     * @return Schema Returns a schema object.
+     */
+    public function idParamEventSchema() {
+        return $this->schema(['id:i' => 'The event ID.'], 'in');
+    }
+
+    /**
+     * List events.
+     *
+     * @param array $query
+     * @return array
+     */
+    public function index(array $query) {
+        $this->permission();
+
+        $in = $this->schema([
+            'groupID:i' => 'Filter by group ID.',
+            'sort:s?' => [
+                'enum' => [
+                    'dateInserted', '-dateInserted',
+                    'dateStarts', '-dateStarts',
+                ],
+                'description' => 'Sort the results by the specified field. The default sort order is ascending.'
+                    .'Prefixing the field with "-" will sort using a descending order.',
+            ],
+            'page:i?' => [
+                'description' => 'Page number.',
+                'default' => 1,
+                'minimum' => 1,
+            ],
+            'limit:i?' => [
+                'description' => 'The number of items per page.',
+                'default' => $this->eventModel->getDefaultLimit(),
+                'minimum' => 1,
+                'maximum' => 100,
+            ],
+            'expand:b?' => 'Expand associated records.',
+        ], 'in')->setDescription('List events.');
+        $out = $this->schema([':a' => $this->fullEventSchema()], 'out');
+
+        $query = $in->validate($query);
+
+        // Sorting
+        $sortField = '';
+        $sortOrder = 'asc';
+        if (array_key_exists('sort', $query)) {
+            $sortField = $this->capitalCaseScheme->convert(ltrim($query['sort'], '-'));
+            if (strlen($sortField) !== strlen($query['sort'])) {
+                $sortOrder = 'desc';
+            }
+        }
+
+        // Paging
+        list($offset, $limit) = offsetLimit("p{$query['page']}", $query['limit']);
+
+        // Filters
+        $where = [];
+        if (array_key_exists('groupID', $query)) {
+            $where['GroupID'] = $query['groupID'];
+        }
+
+        // Data
+        $rows = [];
+        if ($where !== null) {
+            $rows = $this->eventModel->getWhere($where, $sortField, $sortOrder, $limit, $offset)->resultArray();
+        }
+
+        if (!empty($query['expand'])) {
+            $this->userModel->expandUsers($rows, ['InsertUserID', 'UpdateUserID']);
+        }
+        foreach ($rows as &$row) {
+            $row = $this->normalizeEventOutput($row);
+        }
+
+        $result = $out->validate($rows);
+        return $result;
+    }
+
+    /**
+     * Normalize an event Schema record to match the database definition.
+     *
+     * @param $schemaRecord Event Schema record.
+     * @return array Return a database record.
+     */
+    public function normalizeEventInput($schemaRecord) {
+        $dbRecord = $this->capitalCaseScheme->convertArrayKeys($schemaRecord);
+        return $dbRecord;
+    }
+
+    /**
+     * Normalize an event participant Schema record to match the database definition.
+     *
+     * @param $schemaRecord Event participant Schema record.
+     * @return array Return a database record.
+     */
+    public function normalizeEventParticipantInput($schemaRecord) {
+        if (array_key_exists('attending', $schemaRecord)) {
+            if ($schemaRecord['attending'] === null) {
+                $schemaRecord['attending'] = 'Invited';
+            }
+            $schemaRecord['attending'] = $this->capitalCaseScheme->convert($schemaRecord['attending']);
+        }
+
+        $dbRecord = $this->capitalCaseScheme->convertArrayKeys($schemaRecord);
+        return $dbRecord;
+    }
+
+    /**
+     * Normalize a event database record to match the schema definition.
+     *
+     * @param $dbRecord Event database record.
+     * @return array Return a schema record.
+     */
+    public function normalizeEventOutput($dbRecord) {
+        $dbRecord['Body'] = Gdn_Format::to($dbRecord['Body'], $dbRecord['Format']);
+
+        $schemaRecord = $this->camelCaseScheme->convertArrayKeys($dbRecord);
+        $schemaRecord['url'] = eventUrl($dbRecord);
+
+        return $schemaRecord;
+    }
+
+    /**
+     * Normalize a event participant database record to match the schema definition.
+     *
+     * @param $dbRecord Event participant database record.
+     * @return array Return a schema record.
+     */
+    public function normalizeEventParticipantOutput($dbRecord) {
+        $dbRecord['Attending'] = $this->camelCaseScheme->convert($dbRecord['Attending']);
+        $dbRecord['Attending'] = $dbRecord['Attending'] === 'invited' ? null : $dbRecord['Attending'];
+
+        $schemaRecord = $this->camelCaseScheme->convertArrayKeys($dbRecord);
+        return $schemaRecord;
+    }
+
+    /**
+     * Update an event.
+     *
+     * @param int $id The ID of the event.
+     * @param array $body The request body.
+     * @throws NotFoundException If unable to find the event.
+     * @throws ClientException
+     * @return array
+     */
+    public function patch($id, array $body) {
+        $this->permission('Garden.SignIn.Allow');
+
+        $this->idParamEventSchema();
+        $in = $this->postEventSchema()->setDescription('Update an event.');
+        $out = $this->schema($this->fullEventSchema(), 'out');
+
+        $body = $in->validate($body, true);
+
+        $row = $this->eventByID($id);
+
+        $eventData = $this->normalizeEventInput($body);
+        $eventData['EventID'] = $id;
+
+        if ($row['InsertUserID'] !== $this->getSession()->UserID && !$this->eventModel->checkPermission('Edit', $id)) {
+            throw new ClientException('You do not have the rights to edit this event.');
+        }
+
+        if (!empty($eventData['GroupID'])) {
+            $this->groupByID($eventData['GroupID']);
+        }
+
+        $this->eventModel->save($eventData);
+        $this->validateModel($this->eventModel);
+
+        $result = $this->eventByID($id);
+        $this->userModel->expandUsers($result, ['InsertUserID', 'UpdateUserID']);
+
+        $result = $this->normalizeEventOutput($result);
+        return $out->validate($result);
+    }
+
+    /**
+     * Create an event.
+     *
+     * @throws ServerException
+     * @throws NotFoundException If the group or event was not found.
+     * @throws ClientException
+     * @param array $body
+     * @return mixed
+     */
+    public function post(array $body) {
+        $this->permission('Garden.SignIn.Allow');
+
+        $in = $this->postEventSchema()->setDescription('Create an event.');
+        $out = $this->schema($this->fullEventSchema(), 'out');
+
+        if (!$this->eventModel->checkPermission('Create', null)) {
+            throw new ClientException('You do not have the rights to create an event.');
+        }
+
+        $body = $in->validate($body);
+
+        $this->groupByID($body['groupID']);
+
+        $eventData = $this->normalizeEventInput($body);
+
+        $id = $this->eventModel->save($eventData);
+        $this->validateModel($this->eventModel);
+
+        if (!$id) {
+            throw new ServerException('Unable to create event.', 500);
+        }
+
+        $row = $this->eventByID($id);
+        $this->userModel->expandUsers($row, ['InsertUserID']);
+
+        $result = $this->normalizeEventOutput($row);
+        return $out->validate($result);
+    }
+
+    /**
+     * RSVP to an event.
+     *
+     * @throws ClientException
+     * @throws ServerException
+     * @param int $id The event ID.
+     * @param array $body
+     * @return array
+     */
+    public function post_participants($id, array $body) {
+        $this->permission('Garden.SignIn.Allow');
+
+        $this->idParamEventSchema();
+        $in = $this->schema([
+            'userID:s?' => 'The ID of the user that RSVP.',
+            'attending:s|n' => [
+                'enum' => ['yes', 'no', 'maybe'],
+                'description' => 'Is the participant attending the event.',
+            ]
+        ])->setDescription('RSVP to an event.');
+        $out = $this->schema($this->fullEventParticipantSchema(), 'out');
+
+        $this->eventByID($id);
+
+        if (!$this->eventModel->checkPermission('View', $id)) {
+            throw new ClientException('You do not have the rights to view that event.');
+        }
+
+        $body = $in->validate($body);
+
+        $userID = !empty($body['userID']) ? $body['userID'] : $this->getSession()->UserID;
+        if ($this->getSession()->UserID !== $userID && !$this->eventModel->checkPermission('Organizer', $id, $this->getSession()->UserID)) {
+            throw new ClientException('You do not have the rights to add a participant to that event.');
+        }
+
+        $participantData = $this->normalizeEventParticipantInput($body);
+
+        $this->eventModel->attend($userID, $id, $participantData['Attending']);
+
+        $error = true;
+        $users = $this->eventModel->getInvitedUsers($id, ['UserID' => $userID]);
+        $participant = null;
+        if (count($users) === 1) {
+            $participant = array_pop($users);
+        }
+        if ($participant !== null) {
+            $error = ($participant['Attending'] !== $participantData['Attending']);
+        }
+        if ($error) {
+            throw new ServerException('Unable to insert the participant\'s RSVP.');
+        }
+
+        $this->userModel->expandUsers($participant, ['UserID']);
+
+        $result = $this->normalizeEventParticipantOutput($participant);
+        return $out->validate($result);
+    }
+
+
+    /**
+     * Get an event schema with minimal add/edit fields.
+     *
+     * @return Schema Returns a schema object.
+     */
+    public function postEventSchema() {
+        static $postEventSchema;
+
+        if ($postEventSchema === null) {
+            $postEventSchema = $this->schema(
+                Schema::parse(
+                    ['groupID', 'name', 'body', 'format', 'location', 'dateStarts', 'dateEnds?']
+                )->add($this->fullEventSchema()),
+                'EventPost'
+            );
+        }
+
+        return $this->schema($postEventSchema, 'in');
+    }
+}
---
+++ applications/groups/tests/APIv2/EventsTest.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * @author Alexandre (DaazKu) Chouinard <alexandre.c@vanillaforums.com>
+ * @copyright 2009-2017 Vanilla Forums Inc.
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
+ */
+
+namespace VanillaTests\APIv2;
+
+/**
+ * Test the /api/v2/events endpoints.
+ */
+class EventsTest extends AbstractResourceTest {
+
+    /** @var array */
+    protected static $groups;
+
+    /** @var int */
+    protected static $testCount = 0;
+
+    /** {@inheritdoc} */
+    protected $baseUrl = '/events';
+
+    /** {@inheritdoc} */
+    protected $editFields = ['groupID', 'name', 'body', 'format', 'location', 'dateStarts', 'dateEnds'];
+
+    /** {@inheritdoc} */
+    protected $patchFields = ['groupID', 'name', 'body', 'format', 'location', 'dateStarts', 'dateEnds'];
+
+    /** {@inheritdoc} */
+    protected $pk = 'eventID';
+
+    /** {@inheritdoc} */
+    protected $singular = 'event';
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function setupBeforeClass() {
+        self::$groups = [];
+        self::$addons = ['vanilla', 'groups'];
+        parent::setupBeforeClass();
+
+        /** @var \Gdn_Session $session */
+        $session = self::container()->get(\Gdn_Session::class);
+        $session->start(self::$siteInfo['adminUserID'], false, false);
+
+        /** @var \GroupsApiController $groupsAPIController */
+        $groupsAPIController = static::container()->get('GroupsApiController');
+
+        for ($i = 0; $i < 2; $i++) {
+            $groupTxt = uniqid(__CLASS__." ");
+            self::$groups[] = $groupsAPIController->post([
+                'name' => $groupTxt,
+                'description' => $groupTxt,
+                'format' => 'markdown',
+                'privacy' => 'public',
+            ]);
+        }
+
+        // Disable email sending.
+        /** @var \Gdn_Configuration $config */
+        $config = static::container()->get('Config');
+        $config->set('Garden.Email.Disabled', true, true, false);
+
+        $session->end();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp() {
+        parent::setUp();
+        static::$testCount++;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function indexUrl() {
+        $indexUrl = $this->baseUrl;
+        $indexUrl .= '?'.http_build_query(['groupID' => self::$groups[0]['groupID']]);
+        return $indexUrl;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function modifyRow(array $row) {
+        $row = parent::modifyRow($row);
+
+        // Assign the event to the other group.
+        if (isset($row['GroupID'])) {
+            $row['GroupID'] = self::$groups[1]['groupID'];
+        }
+        return $row;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function record() {
+        $count = self::$testCount;
+        $name = "Test Event $count";
+        $record = [
+            'groupID' => self::$groups[0]['groupID'],
+            'name' => $name,
+            'body' => "$name description",
+            'format' => 'markdown',
+            'location' => 'Somewhere',
+            'dateStarts' => date(\DateTime::RFC3339),
+            'dateEnds' => date(\DateTime::RFC3339, now() + 36000),
+        ];
+
+        return $record;
+    }
+}
