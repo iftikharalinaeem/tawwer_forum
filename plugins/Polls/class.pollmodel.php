839de18dd83fc7c9ea3bf06fb31415ab6bfb4cd2
DaazKu <daazku@gmail.com>
2018-01-30 16:33:24 -0500

    Expose addInsertFields in poll model

--- plugins/Polls/class.pollmodel.php
+++ plugins/Polls/class.pollmodel.php
@@ -158,4 +158,14 @@ class PollModel extends Gdn_Model {
         }
         return false;
     }
+
+    /**
+     * Expose Gdn_Model::addInsertFields()
+     * We need to expose this function so that we can pass pre validation in save().
+     *
+     * @param array $fields
+     */
+    public function addInsertFields(&$fields) {
+        parent::addInsertFields($fields);
+    }
 }
