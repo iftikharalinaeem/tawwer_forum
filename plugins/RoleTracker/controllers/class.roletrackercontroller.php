a1bd6f00fca7f8f30317cff5686cff8b2345b7fe
DaazKu <daazku@gmail.com>
2018-02-15 17:19:22 -0500

    Add the ability to remove a traker tag from a discussion

--- plugins/RoleTracker/controllers/class.roletrackercontroller.php
+++ plugins/RoleTracker/controllers/class.roletrackercontroller.php
@@ -86,4 +86,47 @@ class RoleTrackerController extends Gdn_Controller {
 
         $this->render('blank', 'utility', 'dashboard');
     }
+
+    /**
+     * Remove a tracking tag from a discussion.
+     *
+     * @param $discussionID
+     * @param $tagID
+     * @throws Exception
+     * @throws \Vanilla\Exception\PermissionException
+     */
+    public function untrack($discussionID, $tagID) {
+        // Make sure we are posting back.
+        if (!Gdn::request()->isAuthenticatedPostBack()) {
+            throw permissionException('Javascript');
+        }
+
+        $discussionModel = new DiscussionModel();
+        $discussion = $discussionModel->getID($discussionID, DATASET_TYPE_ARRAY);
+        if (!$discussion || !Gdn::session()->checkPermission('Vanilla.Discussions.Edit', true, 'Category', val('PermissionCategoryID', $discussion))) {
+            throw new \Vanilla\Exception\PermissionException('Vanilla.Discussions.Edit');
+        }
+
+        $trackedRoles = RoleTrackerModel::instance()->getTrackedRoles();
+        if (!$trackedRoles) {
+            throw new Exception('No roles are tracked.');
+        }
+        $trackedRolesByTag = Gdn_DataSet::index($trackedRoles, 'TrackerTagID');
+
+        $tagModel = new TagModel();
+        $discussionTags = $tagModel->getDiscussionTags($discussionID, TagModel::IX_TAGID);
+        if (!$discussionTags || !isset($discussionTags[$tagID])) {
+            throw new Exception('The discussion is not tracked.');
+        }
+
+        $tagDiscussionModel = new Gdn_Model('TagDiscussion');
+        $tagDiscussionModel->delete([
+            'DiscussionID' => $discussionID,
+            'TagID' => $tagID,
+        ]);
+
+        $this->informMessage(sprintft('%s was successfully un tracked.', htmlentities($discussionTags[$tagID]['FullName'])));
+
+        $this->render('blank', 'utility', 'dashboard');
+    }
 }
