8ded0ea92d5b00ee1132c9c11260441c206e2119
DaazKu <daazku@gmail.com>
2018-03-19 10:51:12 -0400

    Improve css selector performance

--- applications/groups/js/event.js
+++ applications/groups/js/event.js
@@ -250,7 +250,7 @@ jQuery(document).ready(function($) {
       });
 
       // On event page
-      $(':radio.EventAttending').on('change', function(e) {
+      $('.EventAttending:radio').on('change', function(e) {
          var eventId = $('.EventInfo').data('eventid');
          var eventAttending = $(e.target);
          var result = false;
