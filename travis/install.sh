946dd79d965814a3c0e3b2f4fa4d862f7fc58ea3
Todd Burry <todd@vanillaforums.com>
2018-04-04 14:16:10 -0400

    Fix plugin symlinks

--- travis/install.sh
+++ travis/install.sh
@@ -7,9 +7,12 @@ git clone --depth=50 --branch=master https://github.com/vanilla/vanilla vanilla
 cd "$TRAVIS_BUILD_DIR/vanilla/applications"
 ln -s ../../applications/* ./
 cd "$TRAVIS_BUILD_DIR/vanilla/plugins"
-ln -s ../plugins/* ./
+ln -s ../../plugins/* ./
 
 cd "$TRAVIS_BUILD_DIR/vanilla"
+ls -lah ./applications
+ls -lah ./plugins
+
 composer self-update
 composer install --optimize-autoloader
 composer require phpunit/phpunit ~5
