b4b97c51da83b7e35188f81dade670218b73521e
Tim Gunter <gunter.tim@gmail.com>
2018-04-07 12:43:08 -0400

    Missing some fixes from previous version.

--- plugins/subcommunities/class.subcommunitymodel.php
+++ plugins/subcommunities/class.subcommunitymodel.php
@@ -164,7 +164,9 @@ class SubcommunityModel extends Gdn_Model {
     public static function getDefaultSite() {
         $defaultSite = Gdn::cache()->get(self::CACHE_KEY_DEFAULTSITE);
         if (!is_array($defaultSite)) {
-            $row = Gdn::sql()->getWhere('Subcommunity', ['IsDefault' => 1], '', '', 1)->resultArray();
+            $sql = clone Gdn::sql();
+            $sql->reset();
+            $row = $sql->getWhere('Subcommunity', ['IsDefault' => 1], '', '', 1)->resultArray();
             if (is_array($row) && count($row) === 1) {
                 $defaultSite = current($row);
                 self::calculateRow($defaultSite);
