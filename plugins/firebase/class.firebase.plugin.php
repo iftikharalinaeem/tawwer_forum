9d94da8bf0bb817804043659321bdd349326944c
Patrick Kelly <patrick.k@vanillaforums.com>
2018-06-28 16:59:21 -0400

    Refine Firebase plugin, add documentation, remove unused files.

--- plugins/firebase/class.firebase.plugin.php
+++ plugins/firebase/class.firebase.plugin.php
@@ -1,15 +1,87 @@
 <?php
+/**
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license proprietary
+ */
 
-class fireBasePlugin extends Gdn_OAuth2 {
+
+/**
+ * Class FireBasePlugin
+ *
+ * This plugin injects the Firebase SDK into the head of pages to detect via Javascript if a visitor is logged into a firebase provider
+ * and if so, and if the user is not already logged in, connects the user to the forum.
+ */
+class FireBasePlugin extends Gdn_OAuth2 {
+
+    /* var array of possible third-party providers that can be handled by Firebase. */
+    public $availableAuthProviders = [
+        'GoogleAuthProvider',
+        'FacebookAuthProvider',
+        'TwitterAuthProvider',
+        'GitHubAuthProvider',
+        'EmailAuthProvider',
+        ];
+
+    /**
+     * fireBasePlugin constructor.
+     */
     public function __construct() {
+        $this->setProviderKey('firebase');
+        $this->settingsView = 'plugins/settings/firebase';
     }
 
-    public function base_render_before($sender, $args) {
+    /**
+     * Inject the Javascript and CSS files into the head to listen for and handle logged in users.
+     *
+     * @param Gdn_Controller $sender
+     * @param Gdn_Controller $args
+     * @throws Exception
+     */
+    public function base_afterRenderAsset_handler($sender, $args) {
+        if (Gdn::controller()->deliveryMethod() != DELIVERY_METHOD_XHTML) {
+            return;
+        }
+        // Include the javascript only in the head.
+        if (val('AssetName', $args) !== 'Head') {
+            return;
+        }
+
+        // Do not include in the entry pages since we are already connecting.
+        if ($sender->ControllerName === 'entrycontroller') {
+            return;
+        }
+        // Don't bother if the user is already logged in.
+        if (Gdn::session()->UserID) {
+            return;
+        }
+
+        // Don't bother if the provider is not configured.
+        if (!$this->isConfigured()) {
+            return;
+        }
+
+        $provider = $this->provider();
+        // Don't bother if there are not Firebase Auth Providers configured.
+        $configuredAuthProviders = $this->configuredProviders($provider);
+        if (!$configuredAuthProviders) {
+            return;
+        }
+
+        $authProvidersConfigured = '';
+        foreach ($configuredAuthProviders as $authProvider) {
+            $authProvidersConfigured .= 'firebase.auth.'.$authProvider.'.PROVIDER_ID,
+            ';
+        }
+
+        $sender->setData('APIKey', val('AssociationKey', $provider));
+        $sender->setData('AuthDomain', val('AuthorizeUrl', $provider));
+        $sender->setData('FirebaseAuthProviders', $authProvidersConfigured);
+
         include $sender->fetchViewLocation('firebase-ui', '', 'plugins/firebase');
    }
 
     /**
-     * Inject a container into page for the buttons.
+     * Inject a container into page for the Firebase SDK to print out buttons, etc.
      *
      * @param $sender
      * @param $args
@@ -20,13 +92,172 @@ class fireBasePlugin extends Gdn_OAuth2 {
             ';
    }
 
-   // Elaborate this if you want to have a dedicated sign in page
-   public function vanillaController_firebasesignin_create($sender, $args) {
-       $sender->render('firebase-ui', '', 'plugins/firebase');
-   }
+    /**
+     * Create a form in the Dashboard to save the Firebase API Key and Auth Domain as well is toggle on and off
+     *  which third-party providers to use.
+     *
+     * @param Gdn_Controller $sender
+     * @param Gdn_Controller $args
+     * @throws Gdn_UserException
+     */
+    public function settingsEndpoint($sender, $args) {
+        $sender->permission('Garden.Settings.Manage');
+        $model = new Gdn_AuthenticationProviderModel();
 
-   // This page will receieve the response from firebase with the user info
-    public function vanillaController_firebaseconnect_create($sender, $args) {
-        $sender->render('firebaseconnect', '', 'plugins/firebase');
+        /* @var Gdn_Form $form */
+        $form = new Gdn_Form();
+        $form->setModel($model);
+        $sender->Form = $form;
+
+        if (!$form->authenticatedPostBack()) {
+            $provider = $this->provider();
+            $form->setData($provider);
+        } else {
+
+            $form->setFormValue('AuthenticationKey', $this->getProviderKey());
+
+            $sender->Form->validateRule('AssociationKey', 'ValidateRequired', 'You must provide an API Key.');
+            $sender->Form->validateRule('AuthorizeUrl', 'ValidateRequired', 'You must provide a valid Firebase Auth Domain.');
+
+            // To satisfy the AuthenticationProviderModel, create a BaseUrl.
+            $baseUrlParts = parse_url($form->getValue('AuthorizeUrl'));
+            $baseUrl = (val('scheme', $baseUrlParts) && val('host', $baseUrlParts)) ? val('scheme', $baseUrlParts).'://'.val('host', $baseUrlParts) : null;
+            if ($baseUrl) {
+                $form->setFormValue('BaseUrl', $baseUrl);
+                $form->setFormValue('SignInUrl', $baseUrl); // kludge for default provider
+            }
+            if ($form->save()) {
+                $sender->informMessage(t('Saved'));
+            }
+        }
+
+        // Set up the form.
+        $formFields = [
+            'AssociationKey' =>  ['LabelCode' => 'API Key', 'Description' => 'API key from the console of your Firebase Web App.'],
+            'AuthorizeUrl' =>  ['LabelCode' => 'Auth Domain', 'Description' => 'Auth Domain from the console of your Firebase Web App.'],
+            'RegisterUrl' => ['LabelCode' => 'Register Url', 'Description' => 'Enter the endpoint to direct a user to register.'],
+            'SignOutUrl' => ['LabelCode' => 'Sign Out Url', 'Description' => 'Enter the endpoint to direct a user to sign out.'],
+            'TermsUrl' => ['LabelCode' => 'Terms of Service URL', 'Description' => 'URL to your Terms of Service'],
+            'GoogleAuthProvider' => ['LabelCode' => 'Google Auth Provider', 'Control' => 'toggle', 'Description' => 'Allow users to sign in with their Google identities.'],
+            'FacebookAuthProvider' => ['LabelCode' => 'Facebook Auth Provider', 'Control' => 'toggle', 'Description' => 'Allow users to sign in with their Facebook identities.'],
+            'TwitterAuthProvider' => ['LabelCode' => 'Twitter Auth Provider', 'Control' => 'toggle', 'Description' => 'Allow users to sign in with their Twitter identities.'],
+            'GitHubAuthProvider' => ['LabelCode' => 'GitHub Auth Provider', 'Control' => 'toggle', 'Description' => 'Allow users to sign in with their GitHub identities.'],
+            'EmailAuthProvider' => ['LabelCode' => 'Email/Password Authentication', 'Control' => 'toggle', 'Description' => 'Allow users to sign in with their Email and Password.'],
+            'IsDefault' =>  ['LabelCode' => 'Make this connection your default signin method.', 'Control' => 'checkbox']
+        ];
+
+        $sender->setData('_Form', $formFields);
+
+        $sender->setHighlightRoute();
+        if (!$sender->data('Title')) {
+            $sender->setData('Title', sprintf(t('%s Settings'), 'Firebase SSO'));
+        }
+
+        $view = ($this->settingsView) ? $this->settingsView : 'plugins/oauth2';
+
+        // Create and send the possible redirect URLs that will be required by the authenticating server and display them in the dashboard.
+        // Use Gdn::Request instead of convience function so that we can return http and https.
+        $redirectUrls = Gdn::request()->url('/entry/'. $this->getProviderKey(), true, true);
+        $sender->setData('redirectUrls', $redirectUrls);
+
+        $sender->render('settings', '', $view);
+    }
+
+    /**
+     * Recieve the user's data via an AJAX post returned from Firebase SDK and save it to the session.
+     *
+     * @param EntryController $sender
+     * @throws Gdn_UserException
+     * @return true;
+     */
+    public function entryEndpoint($sender) {
+        if ($error = $sender->Request->get('error')) {
+            throw new Gdn_UserException($error);
+        }
+
+        Gdn::session()->stash($this->getProviderKey()); // remove any stashed provider data.
+
+        /* @var Gdn_Form $form */
+        $form = $sender->Form; //new gdn_Form();
+
+        $rawProfile = $form->formValues();
+        $profile = $this->translateProfileResults($rawProfile['providerData'][0]);
+
+        // Once the profile data has been saved to the session, Firebase SDK will redirect the browser to the connect page.
+        Gdn::session()->stash($this->getProviderKey(), ['Profile' => $profile]);
+        return true;
+    }
+
+    /**
+     * Translate the keys of the profile data being sent from Firebase to match the keys Vanilla uses.
+     *
+     * @param array $rawProfile profile as it is returned from the provider.
+     * @return array Profile array transformed by child class or as is.
+     */
+    public function translateProfileResults($rawProfile = []) {
+        $translatedKeys = [
+            'email' => 'Email',
+            'photoURL' => 'Photo',
+            'displayName' => 'Name',
+            'name' => 'FullName',
+            'uid' => 'UniqueID',
+        ];
+        $profile = self::translateArrayMulti($rawProfile, $translatedKeys, true);
+        $profile['Provider'] = $this->providerKey;
+        return $profile;
+    }
+
+    /**
+     * Check if the plugin is configured.
+     *
+     * @return bool True if it is configured, otherwise false.
+     */
+    public function isConfigured($provider = false) {
+        if ($provider === false) {
+            $provider = $this->provider();
+        }
+
+        if (!$provider) {
+            return false;
+        }
+
+        if (!val('AssociationKey', $provider) || !val('AuthorizeUrl', $provider)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Get the third-party providers that Firebase is configured to authenticate against (e.g. Facebook, GitHub, Google, etc.).
+     *
+     * @param bool $provider
+     * @return array|bool False if no provider was passed, or an array of third-party providers.
+     */
+    public function configuredProviders($provider = false) {
+        if (!$provider) {
+            return false;
+        }
+        $authProvidersConfigured = [];
+        foreach ($this->availableAuthProviders as $authProvider) {
+            if (val($authProvider, $provider)) {
+                $authProvidersConfigured[] = $authProvider;
+            }
+        }
+        return $authProvidersConfigured;
+    }
+}
+
+
+$firebase = new FireBasePlugin();
+if (!function_exists('signInUrl') && $firebase->isConfigured() && $firebase->isDefault()) {
+    /**
+     * Don't use Vanilla's native signin buttons if you are using the Firebase SDK.
+     *
+     * @param string $target
+     * @param bool $force
+     * @return string
+     */
+    function signInUrl($target = '', $force = false) {
+        return '';
     }
 }
--- plugins/firebase/views/firebase-ui.php
+++ plugins/firebase/views/firebase-ui.php
@@ -1,47 +1,92 @@
 <script src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"></script>
+<script src="https://cdn.firebase.com/libs/firebaseui/2.5.1/firebaseui.js"></script>
+<link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/2.5.1/firebaseui.css" />
+
 <script>
     // Initialize Firebase
     var config = {
-        apiKey: "<?php echo c('firebase.ApiKey', 'AIzaSyABfLkOCaR15I9RHQ7dPvLXrV-xbsz7jtA') ?>",
-        authDomain: "<?php echo c('firebase.authDomain', 'sincere-hearth-93716.firebaseapp.com') ?>"
+        apiKey: "<?php echo $sender->data('APIKey') ?>",
+        authDomain: "<?php echo $sender->data('AuthDomain') ?>"
     };
     firebase.initializeApp(config);
-</script>
-<script src="https://cdn.firebase.com/libs/firebaseui/2.5.1/firebaseui.js"></script>
-<link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/2.5.1/firebaseui.css" />
-<script type="text/javascript">
-    // FirebaseUI config.
-    var uiConfig = {
-        signInSuccessUrl: 'https://firebasetest.com/vanilla/firebaseconnect',
-        signInOptions: [
-            // Leave the lines as is for the providers you want to offer your users.
-            <?php
-                // TODO, Make these default to FALSE
-            if (c('firebase.GoogleAuthProvider', true)) {
-                echo 'firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                ';
-            }
-            if (c('firebase.FacebookAuthProvider', true)) {
-                echo 'firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                ';
-            }
-            if (c('firebase.EmailAuthProvider', true)) {
-                echo 'firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                ';
-            }
-            ?>
-        ],
-        <?php
-        if (c('firebase.tosUrl')) {
-            // Terms of service url.
-            echo 'tosUrl: '.c('firebase.tosUrl').',
-            ';
-        }
-        ?>
-    };
 
-    // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user && !gdn.getMeta('SignedIn')) {
+            console.log('User Detected: '+user.displayName);
+            console.log('User Not Logged in:'+gdn.getMeta('SignedIn'));
+            var request = $.ajax({
+                method : "post",
+                url : "/entry/firebase",
+                data : {
+                    "displayName": user.displayName,
+                    "email": user.email,
+                    "emailVerified": user.emailVerified,
+                    "phoneNumber": user.phoneNumber,
+                    "photoURL": user.photoURL,
+                    "uid": user.uid,
+                    "accessToken": user.accessToken,
+                    "providerData": user.providerData
+                },
+                success: function(result) {
+                    console.log('Has created a profile in stash. Redirecting to '+'/entry/connect/firebase?target='+encodeURIComponent(window.location))
+                    window.location = "/entry/connect/firebase?target="+encodeURIComponent(window.location);
+                },
+                error: function(msg) {
+                    console.log('There has been an error calling firebase!');
+                    console.log(msg);
+                }
+            });
+        }
+        if(!user) {
+            // Use the Firebase
+            console.log('Has No User');
+            // FirebaseUI config.
+            var uiConfig = {
+                signInSuccessUrl: window.location,
+                signInOptions: [
+                    // Leave the lines as is for the providers you want to offer your users.
+                    <?php
+                    echo $sender->data('FirebaseAuthProviders');
+                    ?>
+                ],
+                <?php
+                if (c('firebase.tosUrl')) {
+                    // Terms of service url.
+                    echo 'tosUrl: '.c('firebase.tosUrl').',
+            ';
+                }
+                ?>
+            };
+
+            // Initialize the FirebaseUI Widget using Firebase.
+            var ui = new firebaseui.auth.AuthUI(firebase.auth());
+            // The start method will wait until the DOM is loaded.
+            ui.start('#firebaseui-auth-container', uiConfig);
+        }
+    });
+
+    $(function() {
+        $('.firebase-connect-link').on('click', function() {
+            var authType = $(this).attr('data-authtype');
+            switch (authType) {
+                case 'googleauthprovider':
+                    provider = new firebase.auth.GoogleAuthProvider()
+                    break;
+                case 'facebookauthprovider':
+                    provider = new firebase.auth.FacebookAuthProvider()
+                    break;
+                case 'githubauthprovider':
+                    provider = new firebase.auth.GithubAuthProvider()
+                    break;
+                case 'twitterauthprovider':
+                    provider = new firebase.auth.TwitterAuthProvider()
+                    break;
+                case 'emailauthprovider':
+                    provider = new firebase.auth.sendSignInLinkToEmail()
+                    break;
+            }
+
+            firebase.auth().signInWithRedirect(provider);
+        });
+    });
 </script>
