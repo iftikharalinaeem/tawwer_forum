d61c9a6588fe20440ad0883fb7290b8d78228254
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-07-03 16:59:04 -0400

    Test smarty page

---
+++ plugins/knowledge-styles/views/knowledgestyles/components/test.tpl
@@ -0,0 +1,5 @@
+<div class="testing">
+    Test: {$Test}
+</div>
+
+
--- plugins/knowledge-styles/views/knowledgestyles/index.php
+++ plugins/knowledge-styles/views/knowledgestyles/index.php
@@ -1,3 +1,9 @@
-<h1>
-    Knowledge Styles
-</h1>
+<?php if (!defined('APPLICATION')) exit();
+echo wrap($this->data('Title'), 'h1', ['class' => 'pageTitle']);
+
+$data = array(
+    'Test' => '123',
+
+//$smarty = $this->smarty->assign($data);
+
+//echo $output = $smarty->fetch('components/test.tpl');
