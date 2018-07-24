d5eef4b7f0ccf8e255e0902314f4792ad59ea8e3
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-07-24 16:32:09 -0400

    Redirect to groups home when you leave group

--- applications/groups/controllers/class.groupcontroller.php
+++ applications/groups/controllers/class.groupcontroller.php
@@ -419,7 +419,7 @@ class GroupController extends Gdn_Controller {
                 'GroupID' => $group['GroupID']
             ];
             $this->GroupModel->leave($data);
-            $this->jsonTarget('', '', 'Refresh');
+            $this->setRedirectTo('/groups');
         }
 
         $this->setData('Group', $group);
