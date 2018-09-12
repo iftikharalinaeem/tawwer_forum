fd01343b246c9a899361ca0932bc017d62350c09
Adam Charron <adam@charrondev.com>
2018-09-12 17:32:07 -0400

    Make all license declarations proprietary

--- plugins/knowledge/src/scripts/@types/api/articleRevision.ts
+++ plugins/knowledge/src/scripts/@types/api/articleRevision.ts
@@ -1,62 +1,67 @@
 /**
  * @copyright 2009-2018 Vanilla Forums Inc.
- * @license http://www.opensource.org/licenses/gpl-2.0.php GNU GPL v2
+ * @license Proprietary
  */
 
 import { IUserFragment } from "@dashboard/@types/api";
 
 export enum Format {
-  TEXT = "text",
-  TEXTEX = "textex",
-  MARKDOWN = "markdown",
-  WYSIWYG = "wysiwyg",
-  HTML = "html",
-  BBCODE = "bbcode",
-  RICH = "rich",
+    TEXT = "text",
+    TEXTEX = "textex",
+    MARKDOWN = "markdown",
+    WYSIWYG = "wysiwyg",
+    HTML = "html",
+    BBCODE = "bbcode",
+    RICH = "rich"
 }
 
 export interface IArticleRevisionFragment {
-  articleRevisionID: number;
-  articleID: number;
-  status: string;
-  name: string;
-  format: Format;
-  bodyRendered: string;
-  locale: string;
-  insertUser: IUserFragment;
-  dateInserted: string;
+    articleRevisionID: number;
+    articleID: number;
+    status: string;
+    name: string;
+    format: Format;
+    bodyRendered: string;
+    locale: string;
+    insertUser: IUserFragment;
+    dateInserted: string;
 }
 
 interface IArticleRevisionRequiredData {
-  articleID: number;
-  name: string; // The title of the article
-  body: string; // The content of the article.
-  format: Format; // The format of the article.
+    articleID: number;
+    name: string; // The title of the article
+    body: string; // The content of the article.
+    format: Format; // The format of the article.
 }
 
 interface IArticleRevisionDefaultedData {
-  locale: string; // The locale the article was written in
-  status: string; // Revision status (e.g. "published")
+    locale: string; // The locale the article was written in
+    status: string; // Revision status (e.g. "published")
 }
 
 interface IArticleRevisionServerManagedData {
-  articleRevisionID: number;
-  insertUserID: number;
-  insertUser?: IUserFragment;
-  dateInserted: string;
-  bodyRendered: string;
+    articleRevisionID: number;
+    insertUserID: number;
+    insertUser?: IUserFragment;
+    dateInserted: string;
+    bodyRendered: string;
 }
 
 // The record
-export interface IArticleRevision extends IArticleRevisionRequiredData, IArticleRevisionDefaultedData, IArticleRevisionServerManagedData {}
+export interface IArticleRevision
+    extends IArticleRevisionRequiredData,
+        IArticleRevisionDefaultedData,
+        IArticleRevisionServerManagedData {}
 
 // Request/Response interfaces
-export interface IPostArticleRevisionRequestBody extends IArticleRevisionRequiredData, Partial<IArticleRevisionDefaultedData> {}
+export interface IPostArticleRevisionRequestBody
+    extends IArticleRevisionRequiredData,
+        Partial<IArticleRevisionDefaultedData> {}
 
 export interface IPostArticleRevisionResponseBody extends IArticleRevision {}
 
 export interface IGetArticleRevisionRequestBody {
-  id: number;
+    id: number;
 }
 
 export interface IGetArticleRevisionResponseBody extends IArticleRevision {}
