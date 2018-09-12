daeda67eb45b1c7f1b99618b1ad23084f65c658f
Adam Charron <adam@charrondev.com>
2018-09-12 14:22:44 -0400

    Add travis build script

---
+++ travis/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+## Clone vanilla/vanilla so we can test against it
+
+cd $TRAVIS_BUILD_DIR
+
+# This directory might already exist (from the cache) but its not a big deal.
+mkdir "$TRAVIS_BUILD_DIR/vanilla"
+cd "$TRAVIS_BUILD_DIR/vanilla"
+
+# Because cached files are already possibly here we can't do a clone.
+printf "\nCloning the main vanilla repository..."
+git init
+git remote add origin https://github.com/vanilla/vanilla
+git pull --depth 50 origin master
+
+# Symlink in the knowledge plugin
+printf "\nSymlinking plugins from the knowledge repo..."
+cd "$TRAVIS_BUILD_DIR/vanilla/plugins"
+ln -s ../../plugins/* ./
+
+printf "\nInstalling node_modules for vanilla..."
+cd "$TRAVIS_BUILD_DIR/vanilla/"
+yarn install --pure-lockfile
+
+composer self-update
+composer install --optimize-autoloader
+yarn build
+
+cd "$TRAVIS_BUILD_DIR"
