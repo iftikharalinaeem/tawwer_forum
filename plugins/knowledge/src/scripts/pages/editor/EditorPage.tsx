a18d01efb5fd89a48790bc2f159bc5ed150b6d26
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-09-18 11:21:58 -0400

    Added legacy mode param to rich editor Added button and utility styles to KB

--- plugins/knowledge/src/scripts/pages/editor/EditorPage.tsx
+++ plugins/knowledge/src/scripts/pages/editor/EditorPage.tsx
@@ -44,6 +44,7 @@ export class EditorPage extends React.Component<IProps> {
                                             editorID={editorID}
                                             editorDescriptionID={editorDescriptionId}
                                             isPrimaryEditor={true}
+                                            legacyMode={false}
                                         />
                                     </div>
                                 </form>
