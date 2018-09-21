3995f19968272ffcef357b75fdc125453c518d35
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2018-09-21 16:36:28 -0400

    Fixed placeholder text for related articles Removed unused DropDown component

--- plugins/knowledge/src/scripts/pages/article/components/ArticleContent.tsx
+++ plugins/knowledge/src/scripts/pages/article/components/ArticleContent.tsx
@@ -8,6 +8,8 @@ import * as React from "react";
 import { IArticle } from "@knowledge/@types/api";
 import { PanelWidget } from "@knowledge/layouts/PanelLayout";
 import UserContent from "@knowledge/components/UserContent";
+import DropDown from "@library/components/dropdown/DropDown";
+import { t } from "@library/application";
 
 interface IProps {
     article: IArticle;
@@ -15,9 +17,19 @@ interface IProps {
 
 export default class ArticleContent extends React.Component<IProps> {
     public render() {
+        const test1 = `test 1`;
+        const test2 = `test 2`;
+        const test3 = `test 3`;
+
         const { article } = this.props;
         return (
             <PanelWidget>
+                <DropDown name={t("Test DropDown")} className="testDropDown">
+                    <p>{test1}</p>
+                    <p>{test2}</p>
+                    <p>{test3}</p>
+                </DropDown>
+
                 <UserContent content={article.articleRevision.bodyRendered} />
             </PanelWidget>
         );
