765cf135e9513d990fc9468fb1c038b29ddcd26e
initvector <ryan.p@vanillaforums.com>
2018-10-01 20:30:38 -0400

    Add knowledge-categories endpoint

--- plugins/knowledge/Models/KnowledgeCategoryModel.php
+++ plugins/knowledge/Models/KnowledgeCategoryModel.php
@@ -6,15 +6,65 @@
 
 namespace Vanilla\Knowledge\Models;
 
+use DateTimeImmutable;
+use Gdn_Session;
+
 /**
  * A model for managing knowledge categories.
  */
-class KnowledgeCategoryModel extends \Vanilla\Models\Model {
+class KnowledgeCategoryModel extends \Vanilla\Models\PipelineModel {
+
+    /** @var Gdn_Session */
+    private $session;
 
     /**
      * KnowledgeCategoryModel constructor.
      */
-    public function __construct() {
-        parent::__construct('knowledgeCategory');
+    public function __construct(Gdn_Session $session) {
+        parent::__construct("knowledgeCategory");
+        $this->session = $session;
+    }
+
+    /**
+     * Delete knowledge categories.
+     *
+     * @param array $where
+     * @param int $limit
+     */
+    public function delete(array $where, int $limit = 1) {
+        $this->sql()->delete(
+            $this->getTable(),
+            $where,
+            $limit
+        );
+    }
+
+    /**
+     * Add a knowledge category.
+     *
+     * @param array $set Field values to set.
+     * @return mixed ID of the inserted row.
+     * @throws \Exception If an error is encountered while performing the query.
+     */
+    public function insert(array $set) {
+        $set["insertUserID"] = $set["updateUserID"] = $this->session->UserID;
+        $set["dateInserted"] = $set["dateUpdated"] = new DateTimeImmutable("now");
+
+        $result = parent::insert($set);
+        return $result;
+    }
+
+    /**
+     * Update existing knowledge categories.
+     *
+     * @param array $set Field values to set.
+     * @param array $where Conditions to restrict the update.
+     * @throws \Exception If an error is encountered while performing the query.
+     * @return bool True.
+     */
+    public function update(array $set, array $where): bool {
+        $set["updateUserID"] = $this->session->UserID;
+        $set["dateUpdated"] = new DateTimeImmutable("now");
+        return parent::update($set, $where);
     }
 }
---
+++ plugins/knowledge/tests/APIv2/KnowledgeCategoriesTest.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
+namespace VanillaTests\APIv2;
+
+/**
+ * Test the /api/v2/knowledge-categories endpoint.
+ */
+class KnowledgeCategoriesTest extends AbstractResourceTest {
+
+    /** @var string The resource route. */
+    protected $baseUrl = "/knowledge-categories";
+
+    /** @var array Fields to be checked with get/<id>/edit */
+    protected $editFields = [
+        "name",
+        "parentID",
+        "displayType",
+        "isSection",
+        "sort",
+        "sortChildren",
+    ];
+
+    /** @var string The name of the primary key of the resource. */
+    protected $pk = "knowledgeCategoryID";
+
+    /** @var string The singular name of the resource. */
+    protected $singular = "knowledgeCategory";
+
+    /** @var bool Whether to check if paging works or not in the index. */
+    protected $testPagingOnIndex = false;
+
+    /**
+     * This method is called before the first test of this test class is run.
+     */
+    public static function setupBeforeClass() {
+        self::$addons = ["vanilla", "knowledge"];
+        parent::setupBeforeClass();
+    }
+
+    /**
+     * Modify the row for update requests.
+     *
+     * @param array $row The row to modify.
+     * @return array Returns the modified row.
+     */
+    public function modifyRow(array $row) {
+        $row["name"] = $row["name"] ?? "Knowledge Category";
+        $row["parentID"] = $row["parentID"] ?? -1;
+        $row["isSection"] = $row["isSection"] ?? false;
+        $row["sort"] = $row["sort"] ?? 1;
+        $row["sortChildren"] = $row["sortChildren"] ?? "name";
+        $row["displayType"] = $row["displayType"] ?? null;
+
+        $row["name"] = md5($row["name"]);
+        $row["parentID"]++;
+        $row["isSection"] = !$row["isSection"];
+        $row["sort"]++;
+
+        $sortChildrenDiff = array_diff(
+            ["name", "dateInserted", "dateInsertedDesc", "manual"],
+            [$row["sortChildren"]]
+        );
+        $row["sortChildren"] = array_rand(array_flip($sortChildrenDiff));
+
+        if ($row["isSection"]) {
+            $row["displayType"] = array_rand(array_flip(["help", "guide", "search"]));
+        } else {
+            $row["displayType"] = null;
+        }
+
+        return $row;
+    }
+
+    /**
+     * Grab values for inserting a new knowledge category.
+     *
+     * @return array
+     */
+    public function record() {
+        $record = [
+            "name" => "Test Knowledge Category",
+            "parentID" => -1,
+            "isSection" => false,
+            "sortChildren" => "name",
+            "displayType" => null,
+            "sort" => 0,
+        ];
+        return $record;
+    }
+}
