3626c13aae1c3019a72cd3c19bc7e0f83061d82e
Adam Charron <adam@charrondev.com>
2018-10-02 11:26:27 -0400

    Add extra doc blocks

--- plugins/knowledge/src/scripts/modules/categories/state/reducer.ts
+++ plugins/knowledge/src/scripts/modules/categories/state/reducer.ts
@@ -7,6 +7,9 @@
 import { IKbCategoriesState } from "./types";
 import { dummyData } from "./dummyData";
 
+/**
+ * Dummy category reducer that currently only contains hard-coded data.
+ */
 export default function categoriesReducer(state: IKbCategoriesState = dummyData, action: any): IKbCategoriesState {
     return state;
 }
