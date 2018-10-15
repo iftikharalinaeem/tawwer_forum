7bb52686c541c78929ed9e992270d04e2cbd9de1
initvector <ryan.p@vanillaforums.com>
2018-10-15 16:37:16 -0400

    Fix an oopsie

--- plugins/knowledge/src/scripts/routes/pageRoutes.tsx
+++ plugins/knowledge/src/scripts/routes/pageRoutes.tsx
@@ -49,7 +49,7 @@ const CategoriesPage = Loadable({
  */
 export function getPageRoutes() {
     return [
-        <Route exact path="/kb" component={HomePage} key={1} />,
+        <Route exact path="/kb" component={HomePage} key={"/kb"} />,
         <Route path={ARTICLE_ROUTE} component={ArticlePage} key={ARTICLE_ROUTE} />,
         <Route path={CATEGORIES_ROUTE} component={CategoriesPage} key={CATEGORIES_ROUTE} />,
         <Route path={ADD_EDIT_ROUTE} component={EditorPage} key={ADD_EDIT_ROUTE} />,
