309ed395acd9fc3f56f6d40bf05a80e44adbc844
Patrick Kelly <patrick.k@vanillaforums.com>
2018-10-17 14:54:09 -0400

    Format body of the post before creating a Salesforce Case.

--- plugins/Salesforce/class.salesforce.plugin.php
+++ plugins/Salesforce/class.salesforce.plugin.php
@@ -674,7 +674,7 @@ class SalesforcePlugin extends Gdn_Plugin {
             'Origin' => c('Salesforce.OriginValue', 'Vanilla'),
             'Options' => $salesforce->getCaseStatusOptions(),
             'Priorities' => $salesforce->getCasePriorityOptions(),
-            'Body' => Gdn_Format::textEx($content->Body),
+            'Body' => Gdn_Format::textEx(Gdn_Format::to($content->Body, $content->Format)),
             'Type' => $type,
             'CommentID' => val('CommentID', $content),
             'InsertUserID' => val('InsertUserID', $content),
