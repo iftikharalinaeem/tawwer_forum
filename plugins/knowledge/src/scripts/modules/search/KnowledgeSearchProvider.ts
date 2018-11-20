c45abf3cce022c15afeed3b787f3b1c7b9469467
Adam Charron <adam@charrondev.com>
2018-11-20 14:31:03 -0500

    Debounce search provider

--- plugins/knowledge/src/scripts/modules/search/KnowledgeSearchProvider.ts
+++ plugins/knowledge/src/scripts/modules/search/KnowledgeSearchProvider.ts
@@ -1,3 +1,9 @@
+/**
+ * @author Adam (charrondev) Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2018 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
 import { ISearchOptionData } from "@library/components/search/SearchOption";
 import { ISearchOptionProvider } from "@library/contexts/ApiContext";
 import { IComboBoxOption } from "@library/components/forms/select/SearchBar";
@@ -6,18 +12,13 @@ import { ISearchResult } from "@knowledge/@types/api";
 import apiv2 from "@library/apiv2";
 import qs from "qs";
 import { formatUrl } from "@library/application";
-
-/**
- * @author Adam (charrondev) Charron <adam.c@vanillaforums.com>
- * @copyright 2009-2018 Vanilla Forums Inc.
- * @license Proprietary
- */
+import pDebounce from "p-debounce";
 
 export default class KnowledgeSearchProvider implements ISearchOptionProvider {
     /**
      * Simple data loading function for the search bar/react-select.
      */
-    public async autocomplete(value: string): Promise<Array<IComboBoxOption<ISearchOptionData>>> {
+    private fetchSearch = async (value: string): Promise<Array<IComboBoxOption<ISearchOptionData>>> => {
         const queryObj = {
             name: value,
             expand: ["user", "category"],
@@ -36,7 +37,28 @@ export default class KnowledgeSearchProvider implements ISearchOptionProvider {
                 data,
             };
         });
-    }
+    };
+
+    /**
+     * A debounced version of the fetchSearch() function.
+     */
+    private debounceFetchSearch = pDebounce(this.fetchSearch, 100);
+
+    /**
+     * Get autocomplete results.
+     *
+     * This has an early bailout for an empty string because initially focusing the input can cause
+     * a change event to be fired with an empty value.
+     *
+     * @see https://github.com/JedWatson/react-select/issues/614#issuecomment-380763225
+     */
+    public autocomplete = (query: string) => {
+        if (query === "") {
+            return Promise.resolve([]);
+        }
+
+        return this.debounceFetchSearch(query);
+    };
 
     public makeSearchUrl(query: string): string {
         return formatUrl(`/kb/search?query=${encodeURIComponent(query)}`);
