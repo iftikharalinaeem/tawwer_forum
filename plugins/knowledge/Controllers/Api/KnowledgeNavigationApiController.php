316a2401960a1ec09d8dddce61df747f7745f20a
initvector <ryan.p@vanillaforums.com>
2018-12-03 18:47:02 -0500

    Add ability to move and sort items in the tree with the navigation API

--- plugins/knowledge/Controllers/Api/KnowledgeNavigationApiController.php
+++ plugins/knowledge/Controllers/Api/KnowledgeNavigationApiController.php
@@ -7,16 +7,19 @@
 namespace Vanilla\Knowledge\Controllers\Api;
 
 use AbstractApiController;
+use Exception;
 use Garden\Schema\Schema;
+use Garden\Schema\ValidationException;
+use Garden\Web\Exception\HttpException;
+use Vanilla\Exception\PermissionException;
 use Vanilla\Knowledge\Models\ArticleModel;
 use Vanilla\Knowledge\Models\KnowledgeCategoryModel;
+use Vanilla\Knowledge\Models\Navigation;
 
 /**
  * Endpoint for the virtual "knowledge navigation" resource.
  */
 class KnowledgeNavigationApiController extends AbstractApiController {
-    const RECORD_TYPE_CATEGORY = 'knowledgeCategory';
-    const RECORD_TYPE_ARTICLE = 'article';
 
     /** @var ArticleModel */
     private $articleModel;
@@ -59,6 +62,44 @@ class KnowledgeNavigationApiController extends AbstractApiController {
     }
 
     /**
+     * Given two navigation items, compare them and determine their sort order.
+     *
+     * @param array $a
+     * @param array $b
+     * @return int A value of -1, 0 or 1, depending on if $a is less than, equal to, or greater than $b.
+     */
+    private function compareItems(array $a, array $b): int {
+        $sortA = $a["sort"] ?? null;
+        $sortB = $b["sort"] ?? null;
+        if ($sortA === $sortB) {
+            // Same sort weight? We must go deeper.
+            $typeA = $a["recordType"] ?? null;
+            $typeB = $b["recordType"] ?? null;
+            if ($typeA === $typeB) {
+                // Same record type? Sort by name.
+                $nameA = $a["name"] ?? null;
+                $nameB = $b["name"] ?? null;
+                return $nameA <=> $nameB;
+            } elseif ($typeA === Navigation::RECORD_TYPE_ARTICLE) {
+                // If the types differ, and A is an article, then B must be a category. Categories rank higher.
+                return 1;
+            } else {
+                // If they're not the same type, and A isn't an article, it must be a category. A should rank higher.
+                return -1;
+            }
+        } elseif ($sortA === null) {
+            // If they're not the same, and A is null, then B must not be null. B should rank higher.
+            return 1;
+        } elseif ($sortB === null) {
+            // If they're not the same, and B is null, then A must not be null. A should rank higher.
+            return -1;
+        } else {
+            // We have two non-null, non-equal sort weights. Compare them using the combined-comparison operator.
+            return $sortA <=> $sortB;
+        }
+    }
+
+    /**
      * Get navigation fragment schema attributes array
      *
      * @return array
@@ -67,18 +108,30 @@ class KnowledgeNavigationApiController extends AbstractApiController {
         return [
             "name" => [
                 "allowNull" => true,
+                "description" => "Name of the item.",
+                "type" => "string",
+            ],
+            "url?" => [
+                "description" => "Full URL to the record.",
                 "type" => "string"
             ],
-            "url?" => ["type" => "string"],
-            "parentID?" => ["type" => "integer"],
-            "recordID" => ["type" => "integer"],
+            "parentID?" => [
+                "description" => "Unique ID of the category this record belongs to.",
+                "type" => "integer",
+            ],
+            "recordID" => [
+                "description" => "Unique ID of the record represented by the navigation item.",
+                "type" => "integer"
+            ],
             "sort" => [
                 "allowNull" => true,
-                "type" => "integer"
+                "description" => "Sort weight.",
+                "type" => "integer",
             ],
             "knowledgeCategoryID?" => ["type" => "integer"],
             "recordType" => [
-                "enum" => [self::RECORD_TYPE_CATEGORY, self::RECORD_TYPE_ARTICLE],
+                "description" => "Type of record represented by the navigation item.",
+                "enum" => [Navigation::RECORD_TYPE_CATEGORY, Navigation::RECORD_TYPE_ARTICLE],
                 "type" => "string",
             ]
         ];
@@ -133,7 +186,7 @@ class KnowledgeNavigationApiController extends AbstractApiController {
     private function getNavigation(bool $flatMode = true): array {
 
         $categories = $this->knowledgeCategoryModel->get();
-        $categories = $this->normalizeOutput($categories, self::RECORD_TYPE_CATEGORY);
+        $categories = $this->normalizeOutput($categories, Navigation::RECORD_TYPE_CATEGORY);
 
         $catIds = array_column($categories, 'knowledgeCategoryID');
         $articles = $this->articleModel->getExtended(
@@ -142,9 +195,9 @@ class KnowledgeNavigationApiController extends AbstractApiController {
                 'a.status' => ArticleModel::STATUS_PUBLISHED
             ],
             [],
-            ['recordType' => self::RECORD_TYPE_ARTICLE]
+            ['recordType' => Navigation::RECORD_TYPE_ARTICLE]
         );
-        $articles = $this->normalizeOutput($articles, self::RECORD_TYPE_ARTICLE);
+        $articles = $this->normalizeOutput($articles, Navigation::RECORD_TYPE_ARTICLE);
         if ($flatMode) {
             return array_merge($categories, $articles);
         } else {
@@ -184,8 +237,9 @@ class KnowledgeNavigationApiController extends AbstractApiController {
     private function createTree(array $list, array $parent): array {
         $tree = [];
         foreach ($parent as $k => $l) {
-            if (isset($list[$l['knowledgeCategoryID']]) && $l['recordType'] === self::RECORD_TYPE_CATEGORY) {
+            if (isset($list[$l['knowledgeCategoryID']]) && $l['recordType'] === Navigation::RECORD_TYPE_CATEGORY) {
                 $l['children'] = $this->createTree($list, $list[$l['knowledgeCategoryID']]);
+                usort($l["children"], [$this, "compareItems"]);
             }
             $tree[] = $l;
         }
@@ -229,17 +283,17 @@ class KnowledgeNavigationApiController extends AbstractApiController {
      * Massage tree data for useful API output.
      *
      * @param array $rows
-     * @param srting $recordType Record type: RECORD_TYPE_CATEGORY || RECORD_TYPE_ARTICLE
+     * @param string $recordType Record type: RECORD_TYPE_CATEGORY || RECORD_TYPE_ARTICLE
      * @return array
      * @throws \Exception If $row is not a valid knowledge category.
      */
-    private function normalizeOutput(array $rows, string $recordType = self::RECORD_TYPE_CATEGORY): array {
+    private function normalizeOutput(array $rows, string $recordType = Navigation::RECORD_TYPE_CATEGORY): array {
         foreach ($rows as &$row) {
-            if ($recordType === self::RECORD_TYPE_CATEGORY) {
+            if ($recordType === Navigation::RECORD_TYPE_CATEGORY) {
                 $row["recordID"] = $row["knowledgeCategoryID"];
-                $row["recordType"] = self::RECORD_TYPE_CATEGORY;
+                $row["recordType"] = Navigation::RECORD_TYPE_CATEGORY;
                 $row["url"] = $this->knowledgeCategoryModel->url($row);
-            } elseif ($recordType === self::RECORD_TYPE_ARTICLE && $row["recordType"] == self::RECORD_TYPE_ARTICLE) {
+            } elseif ($recordType === Navigation::RECORD_TYPE_ARTICLE && $row["recordType"] == Navigation::RECORD_TYPE_ARTICLE) {
                 $row["recordID"] = $row["articleID"];
                 $row["url"] = $this->articleModel->url($row);
             }
@@ -249,5 +303,76 @@ class KnowledgeNavigationApiController extends AbstractApiController {
         }
 
         return $rows;
+    }
+
+    /**
+     * Get a navigation-friendly record hierarchy of categories and articles in flat mode.
+     *
+     * @param array $body Request body.
+     * @return array Navigation items, arranged in a one-dimensional array.
+     * @throws Exception If no session is available.
+     * @throws HttpException If a relevant ban has been applied on the permission(s) for this session.
+     * @throws PermissionException If the user does not have adequate permission(s).
+     * @throws ValidationException Throws an exception when input does not validate against the input schema.
+     * @throws ValidationException Throws an exception when output does not validate against the output schema.
+     * @todo Add support for multiple knowledge bases.
+     */
+    public function patch_flat(array $body = []): array {
+        $this->permission("knowledge.kb.view");
+
+        $patchSchema = Schema::parse([
+            ":a" => Schema::parse([
+                "recordType",
+                "recordID",
+                "parentID",
+                "sort",
+            ])->add(Schema::parse($this->getFragmentSchema()))
+        ]);
+        $in = $this->schema($patchSchema, "in")->setDescription("Get a navigation-friendly category hierarchy flat mode.");
+        $out = $this->schema([":a" => $this->categoryNavigationFragment()], "out");
+
+        // Prep the input.
+        $body = $in->validate($body);
+
+        // Add a basic index.
+        $navigation = [];
+        foreach ($body as $item) {
+            $key = $item["recordType"]."-".$item["recordID"];
+            $navigation[$key] = $item;
+        }
+
+        // Update categories.
+        $categories = $this->knowledgeCategoryModel->get();
+        Navigation::updateAlteredRows(
+            $categories,
+            $navigation,
+            Navigation::RECORD_TYPE_CATEGORY,
+            "knowledgeCategoryID",
+            "parentID",
+            $this->knowledgeCategoryModel
+        );
+
+        // Update articles.
+        Navigation::updateAlteredRows(
+            $this->articleModel->get(),
+            $navigation,
+            Navigation::RECORD_TYPE_ARTICLE,
+            "articleID",
+            "knowledgeCategoryID",
+            $this->articleModel
+        );
+
+        foreach ($categories as $category) {
+            $parentID = $category["parentID"] ?? null;
+            if ($parentID === -1) {
+                $this->knowledgeCategoryModel->updateCounts($category["knowledgeCategoryID"]);
+                break;
+            }
+        }
+
+        // Grab the new tree.
+        $tree = $this->getNavigation();
+        $result = $out->validate($tree);
+        return $result;
     }
 }
