3851e77a762125df5685dd23c0837f84b46aa468
Adam Charron <adam@charrondev.com>
2018-12-06 18:11:48 -0500

    Update navigation to uise bottomCTA

--- plugins/knowledge/src/scripts/modules/navigation/Navigation.tsx
+++ plugins/knowledge/src/scripts/modules/navigation/Navigation.tsx
@@ -14,6 +14,7 @@ import { IActiveRecord } from "@library/components/siteNav/SiteNavNode";
 import { organize } from "@library/components/icons";
 import { t } from "@library/application";
 import { OrganizeCategoriesRoute } from "@knowledge/routes/pageRoutes";
+import Permission from "@library/users/Permission";
 
 interface IProps extends INavigationStoreState {
     actions: NavigationActions;
@@ -27,21 +28,26 @@ interface IProps extends INavigationStoreState {
 export class Navigation extends React.Component<IProps> {
     public render(): React.ReactNode {
         return (
-            <div className="navigation">
-                <SiteNav collapsible={this.props.collapsible!} activeRecord={this.props.activeRecord}>
-                    {NavigationModel.selectChildren(
-                        this.props.navigationItems,
-                        "knowledgeCategory1" /** Temporarily hardcoded until knowledge bases are wired up. */,
-                    )}
-                </SiteNav>
-                <hr className="navigation-divider" />
-                <div className="navigation-cta">
-                    {organize()}
-                    <OrganizeCategoriesRoute.Link data={{ kbID: 1 }}>
-                        {t("Organize Categories")}
-                    </OrganizeCategoriesRoute.Link>
-                </div>
-            </div>
+            <SiteNav
+                collapsible={this.props.collapsible!}
+                activeRecord={this.props.activeRecord}
+                bottomCTA={
+                    <Permission permission="kb.manage">
+                        <hr className="navigation-divider" />
+                        <div className="navigation-cta">
+                            {organize()}
+                            <OrganizeCategoriesRoute.Link data={{ kbID: 1 }}>
+                                {t("Organize Categories")}
+                            </OrganizeCategoriesRoute.Link>
+                        </div>
+                    </Permission>
+                }
+            >
+                {NavigationModel.selectChildren(
+                    this.props.navigationItems,
+                    "knowledgeCategory1" /** Temporarily hardcoded until knowledge bases are wired up. */,
+                )}
+            </SiteNav>
         );
     }
 
