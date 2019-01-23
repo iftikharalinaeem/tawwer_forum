38f961d77bf26f78d1caa69332c2dc34bdb68d6c
Adam Charron <adam@charrondev.com>
2019-01-22 19:25:09 -0500

    Update frontend routing to account for home pages

---
+++ plugins/knowledge/src/scripts/pages/KnowledgeBasePage.tsx
@@ -0,0 +1,13 @@
+/**
+ * @author Adam (charrondev) Charron <adam.c@vanillaforums.com>
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
+import React from "react";
+
+export default class KnowledgeBasePage extends React.Component {
+    public render() {
+        return <div>Hello knowledge base</div>;
+    }
+}
