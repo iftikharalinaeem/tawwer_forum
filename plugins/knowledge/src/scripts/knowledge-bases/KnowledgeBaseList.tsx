903c541c61bb9d7ac645272c5767cd7f550eb08e
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-01-30 11:31:05 -0500

    Added fallback icon for subcommunity picker

--- plugins/knowledge/src/scripts/knowledge-bases/KnowledgeBaseList.tsx
+++ plugins/knowledge/src/scripts/knowledge-bases/KnowledgeBaseList.tsx
@@ -15,6 +15,7 @@ import FullPageLoader from "@library/components/FullPageLoader";
 import WidgetContainer from "@library/components/layouts/components/WidgetContainer";
 import { WidgetContainerSize } from "@library/components/layouts/components/WidgetContainer";
 import SubcommunityList from "@library/components/subcommunities/SubcommunityList";
+import { knowledgeBaseNoIcon } from "@library/components/icons";
 
 /**
  * Component representing a list of visible knowledge bases.
@@ -45,6 +46,7 @@ class KnowledgeBaseList extends React.Component<IProps> {
                     hiddenTitle={true}
                     items={knowledgeBases}
                     emptyMessage={t("No knowledge bases found.")}
+                    fallbackIcon={knowledgeBaseNoIcon("subcommunityList-fallbackIcon")}
                 />
             </WidgetContainer>
         );
