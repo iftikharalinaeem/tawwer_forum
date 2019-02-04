39a7d7d748a1aa4125b32d075d5b160f940938af
Adam Charron <adam@charrondev.com>
2019-02-04 14:48:44 -0500

    Fix licenses

--- plugins/knowledge/Controllers/ArticlesPageController.php
+++ plugins/knowledge/Controllers/ArticlesPageController.php
@@ -2,7 +2,7 @@
 /**
  * @author Adam Charron <adam.c@vanillaforums.com>
  * @copyright 2009-2019 Vanilla Forums Inc.
- * @license GPL-2.0-only
+ * @license Proprietary
  */
 
 namespace Vanilla\Knowledge\Controllers;
