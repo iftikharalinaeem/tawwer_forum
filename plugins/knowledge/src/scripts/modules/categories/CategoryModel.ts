88fa82a3111115f24c9f2ef5b97d2fd1df9e86c7
Adam Charron <adam@charrondev.com>
2019-02-08 13:40:05 -0500

    Update categories page to dynamically fetch category data.

--- plugins/knowledge/src/scripts/modules/categories/CategoryModel.ts
+++ plugins/knowledge/src/scripts/modules/categories/CategoryModel.ts
@@ -3,184 +3,48 @@
  * @license Proprietary
  */
 
-import { LoadStatus, ILoadable, INavigationTreeItem } from "@library/@types/api";
 import ReduxReducer from "@library/state/ReduxReducer";
+import { KnowledgeReducer, KNOWLEDGE_ACTION } from "@knowledge/state/model";
+import { IKbCategoryFragment, IKbCategory } from "@knowledge/@types/api";
+import produce from "immer";
 import CategoryActions from "@knowledge/modules/categories/CategoryActions";
-import { IStoreState, KnowledgeReducer } from "@knowledge/state/model";
-import { ICrumb } from "@library/components/Breadcrumbs";
-import { KbRecordType } from "@knowledge/navigation/state/NavigationModel";
-import { IKbCategoryFragment } from "@knowledge/@types/api";
+import { reducerWithoutInitialState, reducerWithInitialState } from "typescript-fsa-reducers";
+import { stat } from "fs";
 
-export type IKbCategoriesState = ILoadable<{
+export interface IKbCategoriesState {
     categoriesByID: {
-        [id: number]: IKbCategoryFragment;
+        [id: number]: IKbCategory;
     };
-}>;
-
-type ReducerType = KnowledgeReducer<IKbCategoriesState>;
+}
 
 export default class CategoryModel implements ReduxReducer<IKbCategoriesState> {
-    public static readonly ROOT_CATEGORY: INavigationTreeItem = {
-        name: "Root Category",
-        recordID: -1,
-        recordType: KbRecordType.CATEGORY,
-        parentID: -1,
-        url: "#",
-        sort: null,
-        children: [],
-    };
-
-    /**
-     * Get a category out of the state as a category fragment.
-     *
-     * @param state - The top level redux state.
-     * @param categoryID - The ID of the category to lookup.
-     */
-    public static selectKbCategoryFragment(state: IStoreState, categoryID: number): IKbCategoryFragment | null {
-        if (state.knowledge.categories.status !== LoadStatus.SUCCESS) {
-            throw new Error("Categories not loaded.");
-        }
-
-        if (categoryID === CategoryModel.ROOT_CATEGORY.recordID) {
-            return null;
-        }
-
-        const category = state.knowledge.categories.data!.categoriesByID[categoryID];
-
-        if (category === undefined) {
-            throw new Error(`Category ${categoryID} not found.`);
-        }
-
-        return category;
-    }
-
-    /**
-     * Get a category out of the state as a MutliTypeFragment.
-     *
-     * @param state - The top level redux state.
-     * @param categoryID - The ID of the category to lookup.
-     */
-    public static selectMixedRecord(state: IStoreState, categoryID: number): INavigationTreeItem | null {
-        const record = this.selectKbCategoryFragment(state, categoryID);
-        if (record === null) {
-            return null;
-        }
-
-        const { knowledgeCategoryID, ...rest } = record;
-        return {
-            ...rest,
-            recordType: "knowledgeCategory",
-            recordID: knowledgeCategoryID,
-            sort: null,
-            children: [],
-        };
-    }
-
-    /**
-     * Select the IDs of all of the children for a given parent.
-     *
-     * @param state The full store state.
-     * @param parentID The ID of the parent to check.
-     */
-    private static selectChildrenIDsFromParent(state: IStoreState, parentID: number): number[] {
-        return Object.values(state.knowledge.categories.data!.categoriesByID)
-            .filter(category => category.parentID === parentID)
-            .map(category => category.knowledgeCategoryID);
-    }
-
-    /**
-     * Select a category tree from the store state.
-     *
-     * @param state The full store state.
-     * @param categoryID The category ID to get the tree from.
-     * @param maxDepth The maximum depth of the tree to calculate.
-     */
-    public static selectMixedRecordTree(
-        state: IStoreState,
-        categoryID: number,
-        maxDepth: number = 3,
-    ): INavigationTreeItem {
-        const category: INavigationTreeItem =
-            categoryID === -1 || categoryID === null ? this.ROOT_CATEGORY : this.selectMixedRecord(state, categoryID)!;
-
-        if (maxDepth > 1) {
-            category.children = this.selectChildrenIDsFromParent(state, categoryID).map(id =>
-                this.selectMixedRecordTree(state, id, maxDepth - 1),
-            );
-        }
-        return category;
-    }
-
-    /**
-     * Follow a category's parentID up to the root to assemble it's breadcrumbs.
-     *
-     * @param state - The top level redux state.
-     * @param categoryID - The ID of the category to lookup.
-     */
-    public static selectKbCategoryBreadcrumb(state: IStoreState, categoryID: number): ICrumb[] {
-        const crumbs: IKbCategoryFragment[] = [];
-        let complete = false;
-        let lookupID = categoryID;
-
-        // Loop through and follow up the parent IDs.
-        while (!complete) {
-            const category = CategoryModel.selectKbCategoryFragment(state, lookupID);
-            if (!category) {
-                throw new Error("Attempting to lookup breadcrumb that doesn't exist.");
-            }
-
-            crumbs.unshift(category);
-            if (category.parentID === -1) {
-                complete = true;
-            } else {
-                lookupID = category.parentID;
-            }
-        }
-
-        return crumbs;
-    }
-
     public initialState: IKbCategoriesState = {
-        status: LoadStatus.PENDING,
+        categoriesByID: {},
     };
 
-    public reducer: ReducerType = (state = this.initialState, action) => {
-        switch (action.type) {
-            case CategoryActions.GET_ALL_REQUEST:
-                return {
-                    status: LoadStatus.LOADING,
-                };
-            case CategoryActions.GET_ALL_RESPONSE:
-                const categories = {};
-                for (const category of action.payload.data) {
-                    categories[category.knowledgeCategoryID] = category;
+    public reducer = produce(
+        reducerWithInitialState(this.initialState)
+            .case(CategoryActions.getCategoryACs.done, (nextState, payload) => {
+                nextState.categoriesByID[payload.params.id] = payload.result;
+                return nextState;
+            })
+            .default((nextState, action: KNOWLEDGE_ACTION) => {
+                // Default for non-FSA actions.
+                switch (action.type) {
+                    case CategoryActions.PATCH_CATEGORY_RESPONSE: {
+                        const { knowledgeCategoryID } = action.meta;
+                        const category = nextState.categoriesByID[knowledgeCategoryID];
+                        if (category) {
+                            nextState.categoriesByID[knowledgeCategoryID] = {
+                                ...category,
+                                ...action.payload.data,
+                            };
+                        }
+                        break;
+                    }
                 }
 
-                return {
-                    status: LoadStatus.SUCCESS,
-                    data: {
-                        categoriesByID: categories,
-                    },
-                };
-            case CategoryActions.GET_ALL_ERROR:
-                return {
-                    status: LoadStatus.ERROR,
-                    error: action.payload,
-                };
-            case CategoryActions.POST_CATEGORY_RESPONSE: {
-                if (state.status === LoadStatus.SUCCESS) {
-                    const { categoriesByID } = state.data!;
-                    categoriesByID[action.payload.data.knowledgeCategoryID] = action.payload.data;
-                    return {
-                        status: LoadStatus.SUCCESS,
-                        data: {
-                            categoriesByID,
-                        },
-                    };
-                }
-            }
-        }
-
-        return state;
-    };
+                return nextState;
+            }),
+    );
 }
--- plugins/knowledge/src/scripts/state/reducer.ts
+++ plugins/knowledge/src/scripts/state/reducer.ts
@@ -7,7 +7,6 @@
 import ArticleMenuModel from "@knowledge/modules/article/ArticleMenuModel";
 import ArticleModel from "@knowledge/modules/article/ArticleModel";
 import ArticlePageModel from "@knowledge/modules/article/ArticlePageModel";
-import CategoriesPageReducer from "@knowledge/modules/categories/CategoriesPageReducer";
 import CategoryModel from "@knowledge/modules/categories/CategoryModel";
 import DraftsPageModel from "@knowledge/modules/drafts/DraftsPageModel";
 import EditorPageModel from "@knowledge/modules/editor/EditorPageModel";
@@ -19,6 +18,7 @@ import { IKbState } from "@knowledge/state/model";
 import { combineReducers } from "redux";
 import KnowledgeBaseModel from "@knowledge/knowledge-bases/KnowledgeBaseModel";
 import routeReducer from "@knowledge/routes/RouteReducer";
+import { categoryPageReducer } from "@knowledge/modules/categories/categoryPageReducer";
 
 const knowledgeReducer = combineReducers<IKbState>({
     articles: new ArticleModel().reducer,
@@ -28,7 +28,7 @@ const knowledgeReducer = combineReducers<IKbState>({
     editorPage: new EditorPageModel().reducer,
     revisionsPage: new RevisionsPageModel().reducer,
     categories: new CategoryModel().reducer,
-    categoriesPage: new CategoriesPageReducer().reducer,
+    categoriesPage: categoryPageReducer,
     locationPicker: new LocationPickerModel().reducer,
     searchPage: new SearchPageModel().reducer,
     navigation: new NavigationModel().reducer,
