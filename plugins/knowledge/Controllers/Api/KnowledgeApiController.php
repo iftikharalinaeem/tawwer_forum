ea5a6161f5ac38fad044615fcc424a99aa9ff012
Todd Burry <todd@vanillaforums.com>
2019-07-27 16:21:24 -0400

    Change the article dtypes to be in the 500’s
    The dtype attribute in sphinx is “namespaced” by its index so articles being type 5 should all have dtypes in the 500’s. This is to leave space available for the other indexes.
    
    This change has the potential to break searching until the templates are updated properly so there is a kludge to still respect dtype 5.

--- plugins/knowledge/Controllers/Api/KnowledgeApiController.php
+++ plugins/knowledge/Controllers/Api/KnowledgeApiController.php
@@ -32,8 +32,8 @@ class KnowledgeApiController extends AbstractApiController {
 
     const SPHINX_DEFAULT_LIMIT = 100;
 
-    const TYPE_ARTICLE = 5;
-    const TYPE_ARTICLE_DELETED = 6;
+    const TYPE_ARTICLE = 500;
+    const TYPE_ARTICLE_DELETED = 501;
     const TYPE_DISCUSSION = 0;
     const TYPE_QUESTION = 1;
     const TYPE_POLL = 2;
@@ -425,8 +425,13 @@ class KnowledgeApiController extends AbstractApiController {
             $ids = [];
             foreach ($searchResults['matches'] as $guid => $record) {
                 $this->results['matches'][$guid]['orderIndex'] = $record['weight'];
-                $type = self::RECORD_TYPES[$record['attrs']['dtype']];
-                $ids[$record['attrs']['dtype']][] = ($guid - $type['offset']) / $type['multiplier'];
+                $dtype = $record['attrs']['dtype'];
+                $dtype = $dtype == 5 ? 500 : $dtype; // kludge for changed dtype.
+
+                $type = self::RECORD_TYPES[$dtype] ?? null;
+                if ($type) {
+                    $ids[$dtype][] = ($guid - $type['offset']) / $type['multiplier'];
+                }
             };
             $results = [];
             foreach ($ids as $dtype => $recordIds) {
