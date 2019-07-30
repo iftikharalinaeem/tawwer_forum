a46dd8acea7b40c0614e33109e0fe46c7ba656c4
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-07-30 13:37:59 -0400

    Removed pasted content in organize categories story

--- plugins/knowledge/src/scripts/modules/article/components/KnowledgePages.story.tsx
+++ plugins/knowledge/src/scripts/modules/article/components/KnowledgePages.story.tsx
@@ -17,50 +17,8 @@ import { StoryHeading } from "@library/storybook/StoryHeading";
 
 const reactionsStory = storiesOf("KnowledgeBase/pages", module);
 
-// Add Knobs
-
-interface IArticleReactionProps extends IArticleReaction {
-    signedIn: boolean;
-    isYesSubmitting: boolean;
-    isNoSubmitting: boolean;
-}
-
 // interface IArticleReactionOption
 
 reactionsStory.add("Organize Categories", () => {
-    const noop = () => {
-        return;
-    };
-    const positiveIntOptions: EmptyNumberOptions = ({ min: 0 } as unknown) as EmptyNumberOptions;
-
-    const yesVotes = number("Yes Votes", 10, positiveIntOptions);
-    const noVotes = number("No Votes", 2, positiveIntOptions);
-
-    const signedIn = boolean("User Signed In", true);
-
-    enum possibleVote {
-        NO_VOTE = "noVote",
-        YES = "yes",
-        NO = "no",
-    }
-
-    const userVote =
-        signedIn &&
-        options(
-            "User's Vote (must be signed in)",
-            {
-                Yes: possibleVote.YES,
-                No: possibleVote.NO,
-                "Not Voted": possibleVote.NO_VOTE,
-            },
-            possibleVote.NO_VOTE,
-            {
-                display: "inline-radio",
-            },
-        );
-
-    const loadingVote =
-        signedIn && userVote !== possibleVote.NO_VOTE && boolean("Loading vote (must have voted)", false);
-
     return <></>;
 });
