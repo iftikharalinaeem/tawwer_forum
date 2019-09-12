ab42950a003b8411198715e52effc7d9b5746635
alexander.kim <kim.alexander.ca@gmail.com>
2019-09-12 13:36:04 -0400

    trigger build

--- plugins/Sphinx/tests/APIv2/SphinxSearchTest.php
+++ plugins/Sphinx/tests/APIv2/SphinxSearchTest.php
@@ -32,7 +32,7 @@ class SphinxSearchTest extends AbstractAPIv2Test {
      */
     public static function setupBeforeClass() {
         parent::setupBeforeClass();
-       exec('curl 127.0.0.1:9399', $dockerResponse);
+       exec('curl sphinx:9399', $dockerResponse);
        // die(print_r($dockerResponse));
 
         /** @var \Gdn_Session $session */
