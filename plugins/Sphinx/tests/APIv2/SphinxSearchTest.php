b6ab2c41596036ed5d9d2876fddecf932fe97fac
alexander.kim <kim.alexander.ca@gmail.com>
2019-09-13 09:27:27 -0400

    trigger build

--- plugins/Sphinx/tests/APIv2/SphinxSearchTest.php
+++ plugins/Sphinx/tests/APIv2/SphinxSearchTest.php
@@ -115,7 +115,7 @@ class SphinxSearchTest extends AbstractAPIv2Test {
     public function testRecordTypesDiscussion() {
         $params = [
             //'query' => self::$discussion['name'],
-           'query' => 'discussion',
+           //'query' => 'discussion',
             'recordTypes' => 'discussion',
         ];
         $response = $this->api()->get('/search?'.http_build_query($params));
