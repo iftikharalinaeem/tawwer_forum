4c1fdb36fcbf76f2ef79b0f7d031e56e2619c548
alexander.kim <kim.alexander.ca@gmail.com>
2019-09-13 09:36:12 -0400

    reorder tests

--- plugins/Sphinx/tests/APIv2/SphinxSearchTest.php
+++ plugins/Sphinx/tests/APIv2/SphinxSearchTest.php
@@ -110,6 +110,25 @@ class SphinxSearchTest extends AbstractAPIv2Test {
     }
 
 
+   /**
+    * Test search scoped to discussions.
+    */
+   public function testRecordTypesDiscussion() {
+      $params = [
+         'query' => self::$discussion['name'],
+         'recordTypes' => 'discussion',
+      ];
+      $response = $this->api()->get('/search?'.http_build_query($params));
+      $this->assertEquals(200, $response->getStatusCode());
+
+      $results = $response->getBody();
+
+      $this->assertTrue(count($results) === 1);
+      foreach ($results as $result) {
+         self::$searchResultSchema->validate($result);
+      }
+      $this->assertRowsEqual(['recordType' => 'discussion'], $results[0]);
+   }
 
     /**
      * Test search scoped to comments.
@@ -130,26 +149,6 @@ class SphinxSearchTest extends AbstractAPIv2Test {
         }
         $this->assertRowsEqual(['recordType' => 'comment'], $results[0]);
     }
-
-   /**
-    * Test search scoped to discussions.
-    */
-   public function testRecordTypesDiscussion() {
-      $params = [
-         'query' => self::$discussion['name'],
-         'recordTypes' => 'discussion',
-      ];
-      $response = $this->api()->get('/search?'.http_build_query($params));
-      $this->assertEquals(200, $response->getStatusCode());
-
-      $results = $response->getBody();
-
-      $this->assertTrue(count($results) === 1);
-      foreach ($results as $result) {
-         self::$searchResultSchema->validate($result);
-      }
-      $this->assertRowsEqual(['recordType' => 'discussion'], $results[0]);
-   }
 
     /**
      * Test search scoped to a discussion.
