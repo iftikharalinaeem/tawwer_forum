0e256157d5ea461369c2fb573ac947c3c1fb336a
Adam Charron <adam@charrondev.com>
2019-09-16 15:54:06 -0400

    Implement link to management UI in product form group

--- plugins/subcommunities/src/scripts/forms/ProductIntegrationFormGroup.tsx
+++ plugins/subcommunities/src/scripts/forms/ProductIntegrationFormGroup.tsx
@@ -7,10 +7,14 @@ import { DashboardFormGroup } from "@dashboard/forms/DashboardFormGroup";
 import { DashboardToggle } from "@dashboard/forms/DashboardToggle";
 import { t } from "@library/utility/appUtils";
 import { useProductsState } from "@subcommunities/products/productSelectors";
-import React from "react";
+import React, { Children, useState } from "react";
 import { DashboardLabelType } from "@dashboard/forms/DashboardFormLabel";
 import { useProductActions } from "@subcommunities/products/ProductActions";
 import { LoadStatus } from "@library/@types/api/core";
+import Translate from "@library/content/Translate";
+import Button from "@library/forms/Button";
+import { ButtonTypes } from "@library/forms/buttonStyles";
+import { ProductManager } from "@subcommunities/products/ProductManager";
 
 interface IProps {}
 
@@ -21,20 +25,39 @@ interface IProps {}
 export function ProductIntegrationFormGroup(props: IProps) {
     const { enableStatus, enabled } = useProductsState();
     const { toggleFeatureEnabled } = useProductActions();
+    const [showModal, setShowModal] = useState(false);
 
     return (
-        <DashboardFormGroup
-            labelType={DashboardLabelType.WIDE}
-            label={t("Enable Product Integration")}
-            description={t("When enabled, you can manage products, and group subcommunities by those products")}
-        >
-            <DashboardToggle
-                inProgress={enableStatus === LoadStatus.LOADING}
-                checked={enabled}
-                onChange={() => {
-                    void toggleFeatureEnabled({ enabled: !enabled });
-                }}
-            />
-        </DashboardFormGroup>
+        <>
+            {showModal && <ProductManager onClose={() => setShowModal(false)} asModal />}
+            <DashboardFormGroup
+                labelType={DashboardLabelType.WIDE}
+                label={t("Enable Product Integration")}
+                description={
+                    <Translate
+                        source="When enabled, you can <0>manage products</0>, and group subcommunities by those products."
+                        c0={text => {
+                            if (enabled) {
+                                return (
+                                    <Button baseClass={ButtonTypes.TEXT_PRIMARY} onClick={() => setShowModal(true)}>
+                                        {text}
+                                    </Button>
+                                );
+                            } else {
+                                return text;
+                            }
+                        }}
+                    />
+                }
+            >
+                <DashboardToggle
+                    inProgress={enableStatus === LoadStatus.LOADING}
+                    checked={enabled}
+                    onChange={() => {
+                        void toggleFeatureEnabled({ enabled: !enabled });
+                    }}
+                />
+            </DashboardFormGroup>
+        </>
     );
 }
