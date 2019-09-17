ebcc2ea6a1188c7b098cb77c0653d57a9b51e622
Adam Charron <adam@charrondev.com>
2019-09-17 17:25:07 -0400

    Update product selector to support different types of target values

--- plugins/subcommunities/src/scripts/entries/admin.tsx
+++ plugins/subcommunities/src/scripts/entries/admin.tsx
@@ -3,9 +3,15 @@
  * @license Proprietary
  */
 
+import React from "react";
 import { addComponent } from "@library/utility/componentRegistry";
 import { ProductIntegrationFormGroup } from "@subcommunities/forms/ProductIntegrationFormGroup";
 import { ProductSelectorFormGroup } from "@subcommunities/forms/ProductSelectorFormGroup";
 
 addComponent("product-integration-form-group", ProductIntegrationFormGroup);
-addComponent("product-selector-form-group", ProductSelectorFormGroup);
+addComponent("product-selector-form-group", props => (
+    <ProductSelectorFormGroup {...props} formFieldName="ProductID" valueType="productID" />
+));
+addComponent("site-section-group-selector-form-group", props => (
+    <ProductSelectorFormGroup {...props} formFieldName="siteSectionGroup" valueType="sectionGroup" />
+));
