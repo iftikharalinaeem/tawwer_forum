9000d3b1f5286499244fd05e88081c12a7521c38
alexander.kim <kim.alexander.ca@gmail.com>
2019-09-30 18:23:26 -0400

    change sphinx host to 127.0.0.1 for circleci

--- plugins/Polls/tests/Sphinx/SearchPollTest.php
+++ plugins/Polls/tests/Sphinx/SearchPollTest.php
@@ -48,7 +48,7 @@ class SearchPollTest extends AbstractAPIv2Test {
     public static function setupBeforeClass() {
         parent::setupBeforeClass();
 
-        saveToConfig('Plugins.Sphinx.Server', 'sphinx');
+        saveToConfig('Plugins.Sphinx.Server', '127.0.0.1');
         saveToConfig('Plugins.Sphinx.UseDeltas', true);
 
         /** @var \Gdn_Session $session */
