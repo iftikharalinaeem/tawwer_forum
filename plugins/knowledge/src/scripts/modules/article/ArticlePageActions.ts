0f81e852f277716013886f08c940696731959c03
NeenaDev <neena.dev@vanillaforums.com>
2019-10-15 13:09:57 -0400

    fixes

--- plugins/knowledge/src/scripts/modules/article/ArticlePageActions.ts
+++ plugins/knowledge/src/scripts/modules/article/ArticlePageActions.ts
@@ -58,7 +58,7 @@ export default class ArticlePageActions extends ReduxActions {
             }
 
             const localResponseData = await this.articleActions.fetchLocales({ articleID });
-            const localResponse = localResponseData.data || null;
+            const localResponse = localResponseData !== undefined ? localResponseData.data : null;
             const kbID = article.knowledgeBaseID;
             await this.navigationActions.getNavigationFlat(kbID);
             return article;
