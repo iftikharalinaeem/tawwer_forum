1ac283d3dc6b53d6812bb30aeb69777b989ef5c8
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2019-11-07 18:14:02 -0500

    Added product management modal, fixed modal props for add knowledge base

--- plugins/knowledge/src/scripts/knowledge-settings/KnowledgeBaseAddEdit.tsx
+++ plugins/knowledge/src/scripts/knowledge-settings/KnowledgeBaseAddEdit.tsx
@@ -13,7 +13,7 @@ import LinkAsButton from "@library/routing/LinkAsButton";
 import { ToolTip, ToolTipIcon } from "@library/toolTip/ToolTip";
 import { getMeta } from "@library/utility/appUtils";
 import { t } from "@vanilla/i18n";
-import React, {useMemo, useState} from "react";
+import React, { useMemo, useState } from "react";
 import Button from "@library/forms/Button";
 import ModalConfirm from "@library/modal/ModalConfirm";
 import Modal from "@library/modal/Modal";
@@ -25,14 +25,26 @@ import SmartAlign from "@library/layout/SmartAlign";
 import classNames from "classnames";
 import FrameFooter from "@library/layout/frame/FrameFooter";
 import ButtonLoader from "@library/loaders/ButtonLoader";
-import {uniqueIDFromPrefix} from "@library/utility/idUtils";
+import { uniqueIDFromPrefix } from "@library/utility/idUtils";
+import { frameFooterClasses } from "@library/layout/frame/frameFooterStyles";
+import { DashboardSelect } from "@dashboard/forms/DashboardSelect";
+import { LoadStatus } from "@library/@types/api/core";
+import { DashboardFormGroup } from "@dashboard/forms/DashboardFormGroup";
+import Translate from "@library/content/Translate";
+import SmartLink from "@library/routing/links/SmartLink";
+import { ProductManager } from "@subcommunities/products/ProductManager";
+import { DashboardFormList } from "@dashboard/forms/DashboardFormList";
 
 interface IProps {}
 
-const onCancel = () => {};
+const doNothing = () => {
+    return;
+};
 
 export function KnowledgeBaseAddEdit(props: IProps) {
-    const [open, setOpen] = useState(false);
+    const [openForm, setOpenForm] = useState(false);
+    const [openProductManagement, setOpenProductManagement] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const toggleButtonRef = React.createRef<HTMLButtonElement>();
 
@@ -40,54 +52,90 @@ export function KnowledgeBaseAddEdit(props: IProps) {
         return uniqueIDFromPrefix("addKnowledgeBase");
     }, []);
 
-    const title =
+    const classFrameFooter = frameFooterClasses();
+
+    const onCancel = () => {
+        setOpenForm(false);
+    };
+
+    const onClose = () => {
+        setOpenForm(false);
+    };
+
+    const save = () => {
+        setOpenForm(false);
+    };
 
     return (
         <>
-            <Button baseClass={ButtonTypes.ICON_COMPACT} onClick={() => setOpen(true)}>
-                {t("Add Knowledgebase")}
+            <Button
+                buttonRef={toggleButtonRef}
+                baseClass={ButtonTypes.DASHBOARD_PRIMARY}
+                onClick={() => setOpenForm(true)}
+            >
+                {t("Add Knowledge Base")}
             </Button>
-            {open && (
+            {openForm && (
                 <Modal
                     size={ModalSizes.LARGE}
-                    elementToFocus={toggleButtonRef.current as HTMLElement}
                     exitHandler={onCancel}
                     titleID={titleID}
-                    elementToFocusOnExit={this.props.elementToFocusOnExit}
+                    elementToFocusOnExit={toggleButtonRef.current as HTMLElement}
+                    scrollable={true}
                 >
                     <Frame
                         header={
-                            <FrameHeader
-                                titleID={titleID}
-                                closeFrame={onCancel}
-                                srOnlyTitle={srOnlyTitle!}
-                                title={title}
-                            />
+                            <FrameHeader titleID={titleID} closeFrame={onClose} title={t("Add/Edit Knowledge Base")} />
                         }
-                        body={<FrameBody>{t("body")}</FrameBody>}
+                        body={
+                            <FrameBody>
+                                <DashboardFormList>
+                                    <DashboardFormGroup
+                                        label="Product"
+                                        description={
+                                            <Translate
+                                                source="Associate a product with this Subcommunity. <0>Use the product management UI</0> to replace add, edit, or delete products."
+                                                c0={content => (
+                                                    <Button
+                                                        baseClass={ButtonTypes.TEXT_PRIMARY}
+                                                        onClick={event => {
+                                                            setOpenProductManagement(true);
+                                                        }}
+                                                    >
+                                                        {content}
+                                                    </Button>
+                                                )}
+                                            />
+                                        }
+                                    >
+                                        <DashboardSelect disabled={loading} options={[]} onChange={doNothing} />
+                                    </DashboardFormGroup>
+                                </DashboardFormList>
+                            </FrameBody>
+                        }
                         footer={
                             <FrameFooter justifyRight={true}>
                                 <Button
                                     className={classFrameFooter.actionButton}
                                     baseClass={ButtonTypes.TEXT}
-                                    buttonRef={this.cancelRef}
                                     onClick={onCancel}
                                 >
                                     {t("Cancel")}
                                 </Button>
                                 <Button
                                     className={classFrameFooter.actionButton}
-                                    onClick={onConfirm}
+                                    onClick={save}
                                     baseClass={ButtonTypes.TEXT_PRIMARY}
-                                    disabled={isConfirmLoading}
+                                    disabled={loading}
                                 >
-                                    {isConfirmLoading ? <ButtonLoader /> : this.props.confirmTitle}
+                                    {loading ? <ButtonLoader /> : t("Save")}
                                 </Button>
                             </FrameFooter>
                         }
                     />
                 </Modal>
             )}
+            {openProductManagement && <ProductManager asModal={true} onClose={() => setOpenProductManagement(false)} />}
         </>
     );
 }
