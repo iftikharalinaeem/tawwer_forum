6749290a1eaffb4d2c505e4ed81029250300f050
Adam Charron <adam@charrondev.com>
2019-11-11 20:52:12 -0500

    Update translation interface

--- plugins/knowledge/src/scripts/navigation/NavigationTranslator.tsx
+++ plugins/knowledge/src/scripts/navigation/NavigationTranslator.tsx
@@ -7,7 +7,7 @@ import React, { useDebugValue } from "react";
 import { useContentTranslator, ITranslationProperty, TranslationPropertyType } from "@vanilla/i18n";
 import { useSelector } from "react-redux";
 import { IKnowledgeAppStoreState } from "@knowledge/state/model";
-import { INormalizedNavigationItem } from "@knowledge/navigation/state/NavigationModel";
+import { INormalizedNavigationItem, KbRecordType } from "@knowledge/navigation/state/NavigationModel";
 import { KB_RESOURCE_NAME } from "@knowledge/constants";
 import { LoadStatus } from "@library/@types/api/core";
 
@@ -33,16 +33,15 @@ export function OrganizeCategoriesTranslator(props: IProps) {
             onDismiss={() => {}}
             isLoading={isLoading}
             isFullScreen
+            resource={KB_RESOURCE_NAME}
         />
     );
 }
 
 function navItemToTranslationProperty(item: INormalizedNavigationItem): ITranslationProperty {
     return {
-        resource: KB_RESOURCE_NAME,
         propertyName: "name",
         propertyType: TranslationPropertyType.TEXT,
-        translationPropertyKey: `${KB_RESOURCE_NAME}.${item.recordType}.${item.recordID}.name`,
         recordType: item.recordType,
         recordID: item.recordID,
         sourceText: item.name,
@@ -62,7 +61,9 @@ function useStatus(kbID: number) {
 
 function useNavigationCategoriesForKB(kbID: number) {
     const navItemsByID = useSelector((state: IKnowledgeAppStoreState) => state.knowledge.navigation.navigationItems);
-    const itemsForKb = Object.values(navItemsByID).filter(item => item && item.knowledgeBaseID === kbID);
+    const itemsForKb = Object.values(navItemsByID).filter(
+        item => item && item.knowledgeBaseID === kbID && item.recordType === KbRecordType.CATEGORY,
+    );
     useDebugValue(kbID);
     return itemsForKb;
 }
