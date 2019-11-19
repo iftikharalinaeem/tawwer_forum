9ebce1c1d77206d49a89b6ce0ffd381c4b1eda0f
Patrick Kelly <nervoustwit@users.noreply.github.com>
2019-11-19 11:27:42 -0500

    Update plugins/jwtsso/JWTSSOPlugin.php
    Nudge over code.
    
    Co-Authored-By: Ryan <initvector@users.noreply.github.com>

--- plugins/jwtsso/JWTSSOPlugin.php
+++ plugins/jwtsso/JWTSSOPlugin.php
@@ -307,7 +307,7 @@ class JWTSSOPlugin extends Gdn_Plugin {
      */
     public function base_connectData_handler($sender, $args) {
         //If there is another provider that is the default, do nothing and return.
-            $defaultProvider = $this->authenticationProviderModel->getDefault();
+        $defaultProvider = $this->authenticationProviderModel->getDefault();
         if ($defaultProvider && $defaultProvider !== $this->provider()['AuthenticationKey']) {
             return;
         }
