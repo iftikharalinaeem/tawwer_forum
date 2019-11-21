b42c03e696460697002b538b2d259901a791ada0
Chris Chabilall <chris.c@vanillaforums.com>
2019-11-21 14:57:37 -0500

    Decode discussion name when reporting a comment

--- plugins/Reporting2/class.reporting2.plugin.php
+++ plugins/Reporting2/class.reporting2.plugin.php
@@ -195,10 +195,9 @@ class Reporting2Plugin extends Gdn_Plugin {
             // Create excerpt to show in form popup
             $row = getRecord($recordType, $iD);
 
-            if ($recordType === "discussion") {
-                $discussionModel = new DiscussionModel();
-                $row = $discussionModel->fixRow($row);
-            }
+            $discussionModel = new DiscussionModel();
+            $row = $discussionModel->fixRow($row);
+
 
             $quoteHtml = $this->renderQuote($row, $recordType, $iD);
             $sender->setData('quote', $quoteHtml);
