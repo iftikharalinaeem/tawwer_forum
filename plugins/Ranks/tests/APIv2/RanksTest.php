1780e302e4f91bc20ada28bb7bbd7534083d90d5
Todd Burry <todd@vanillaforums.com>
2019-12-01 13:39:12 -0500

    Fix PHP 7.4 deprecation
    Nested ternary if expressions are no longer supported without brackets.

--- plugins/Ranks/tests/APIv2/RanksTest.php
+++ plugins/Ranks/tests/APIv2/RanksTest.php
@@ -160,7 +160,7 @@ class RanksTest extends AbstractResourceTest {
             case 'abilities':
                 $expected = $updatedRow['abilities'][$field];
                 $actual = $rank['Abilities'][$expectedName];
-                $actual = $actual === 'yes' ? true : $actual === 'no' ? false : $actual;
+                $actual = $actual === 'yes' ? true : ($actual === 'no' ? false : $actual);
                 break;
             case 'criteria':
                 $expected = $updatedRow['criteria'][$field];
