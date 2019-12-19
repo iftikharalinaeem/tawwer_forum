618a42d8eaee142fece88bafe1e98c5b0719259c
Adam Charron <adam@charrondev.com>
2019-12-19 11:47:13 -0500

    Fix broken import

--- plugins/subcommunities/src/scripts/subcommunities/subcommunityErrors.ts
+++ plugins/subcommunities/src/scripts/subcommunities/subcommunityErrors.ts
@@ -4,6 +4,7 @@
  */
 
 import { IFieldError } from "@library/@types/api/core";
+import { t } from "@vanilla/i18n";
 
 export function noSubcommunitiesFieldError(field: string = ""): IFieldError {
     return { field, code: "No subcommunities", message: t("This product has no subcommunities.") };
