a77aff44f492867b624c8d18d1eb4813f7aa81e1
NeenaDev <neena.dev@vanillaforums.com>
2020-01-03 14:17:40 -0500

    Create theme-editor page

--- plugins/themingapi/src/scripts/theme/ThemeEditorPage.tsx
+++ plugins/themingapi/src/scripts/theme/ThemeEditorPage.tsx
@@ -8,10 +8,14 @@ import React, { useState } from "react";
 import { t } from "@vanilla/i18n";
 import { DashboardHeaderBlock } from "@dashboard/components/DashboardHeaderBlock";
 import { BrowserRouter } from "react-router-dom";
-const { HeadItem } = DashboardTable;
+import { ActionBar } from "@library/headers/ActionBar";
+import DropDownItemButton from "@library/flyouts/items/DropDownItemButton";
+import DropDown, { FlyoutType } from "@library/flyouts/DropDown";
 
-interface IProps {
-}
+import { DataTabs } from "@library/sectioning/Tabs";
+import TextEditor from "@library/textEditor/TextEditor";
+
+interface IProps {}
 
 interface IState {}
 
@@ -20,11 +24,34 @@ export default class ThemeEditorPage extends React.Component<IProps, IState> {
         super(props);
     }
     public render() {
+        const tabData = [
+            { label: "Header", panelData: "header" },
+            { label: "Footer", panelData: "footer" },
+            { label: "CSS", panelData: "css" },
+            { label: "JS", panelData: "js" },
+        ];
         return (
             <BrowserRouter>
-                <DashboardHeaderBlock
-                    title={t("Theme Editor")}
-                />
+                <React.Fragment>
+                    <DashboardHeaderBlock title={t("Theme Editor")} />
+                    <ActionBar
+                        callToActionTitle={"Save"}
+                        optionsMenu={
+                            <DropDown flyoutType={FlyoutType.LIST}>
+                                <DropDownItemButton onClick={() => {}}>someItem</DropDownItemButton>
+                            </DropDown>
+                        }
+                    />
+                    <DataTabs data={tabData}>
+                        <TextEditor
+                            height={"90vh"} // By default, it fully fits with its parent
+                            theme={"dark"}
+                            language={"html"}
+                            // editorDidMount={handleEditorDidMount}
+                            options={{ lineNumbers: "on" }}
+                        />
+                    </DataTabs>
+                </React.Fragment>
             </BrowserRouter>
         );
     }
