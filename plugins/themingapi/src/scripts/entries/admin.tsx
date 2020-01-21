598b1102ebd87994f93d4887b6834606d033e490
alexander.kim <kim.alexander.ca@gmail.com>
2020-01-21 16:54:43 -0500

    remove duplicated import

--- plugins/themingapi/src/scripts/entries/admin.tsx
+++ plugins/themingapi/src/scripts/entries/admin.tsx
@@ -6,7 +6,6 @@
 import ManageThemingPage from "@themingapi/theming-ui-settings/ManageThemingPage";
 import { addComponent } from "@library/utility/componentRegistry";
 import { registerReducer } from "@library/redux/reducerRegistry";
-import { themeSettingsReducer } from "@library/theming/themeSettingsReducer";
 import { themeEditorReducer } from "@themingapi/theme/themeEditorReducer";
 import { themeSettingsReducer } from "@library/theming/themeSettingsReducer";
 import ThemeEditorPage from "@themingapi/theme/ThemeEditorPage";
