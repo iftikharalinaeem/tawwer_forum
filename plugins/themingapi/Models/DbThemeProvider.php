7174bb29f61a3a3a574c0932a75fb3180bc32040
alexander.kim <kim.alexander.ca@gmail.com>
2020-01-23 09:44:22 -0500

    rename "parent theme" tot "template" for current theme info

--- plugins/themingapi/Models/DbThemeProvider.php
+++ plugins/themingapi/Models/DbThemeProvider.php
@@ -398,7 +398,7 @@ class DbThemeProvider implements ThemeProviderInterface {
         if (!($parentTheme instanceof Addon)) {
             $res['preview']['info']['Warning'] = ['type' => 'string', 'info' => 'Parent theme ('.$theme['parentTheme'].') is not valid'];
         } else {
-            $res['preview']['info']['Parent theme'] = ['type' => 'string', 'info' => $parentTheme->getInfoValue('name')];
+            $res['preview']['info']['Template'] = ['type' => 'string', 'info' => $parentTheme->getInfoValue('name')];
         }
         $res['preview']['info']['Created'] = ['type' => 'date', 'info' => $theme['dateInserted']->format('Y-m-d H:i:s')];
         $res['preview']['info']['Updated'] = ['type' => 'date', 'info' => $theme['dateUpdated']->format('Y-m-d H:i:s')];
