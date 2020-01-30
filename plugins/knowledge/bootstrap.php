4e295195ebbdd5635f58e5927cbf0f249f215eab
alexander.kim <kim.alexander.ca@gmail.com>
2020-01-30 12:15:55 -0500

    add kb.png

--- plugins/knowledge/bootstrap.php
+++ plugins/knowledge/bootstrap.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @author Alexander Kim <alexander.k@vanillaforums.com>
+ * @copyright 2009-2019 Vanilla Forums Inc.
+ * @license Proprietary
+ */
+
 namespace Vanilla\Knowledge;
 
 use Garden\Container\Reference;
