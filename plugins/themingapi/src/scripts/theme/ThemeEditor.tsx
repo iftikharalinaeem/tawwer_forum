dcabcc8ee484b5fc5f7442ca7696592924e2bb26
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2020-02-26 16:50:19 -0500

    Uncommented iframe

--- plugins/themingapi/src/scripts/theme/ThemeEditor.tsx
+++ plugins/themingapi/src/scripts/theme/ThemeEditor.tsx
@@ -14,12 +14,12 @@ export default function ThemeEditor(props: IProps) {
     return (
         <div className={classes.wrapper}>
             <div className={classes.frame}>
-                {/*<iframe*/}
-                {/*    src="https://dev.vanilla.localhost/theme/theme-settings/preview"*/}
-                {/*    width="100%"*/}
-                {/*    height="100%"*/}
-                {/*    scrolling="yes"*/}
-                {/*></iframe>*/}
+                <iframe
+                    src="https://dev.vanilla.localhost/theme/theme-settings/preview"
+                    width="100%"
+                    height="100%"
+                    scrolling="yes"
+                ></iframe>
             </div>
             <div className={classes.styleOptions}>
                 <ThemeBuilderForm />
