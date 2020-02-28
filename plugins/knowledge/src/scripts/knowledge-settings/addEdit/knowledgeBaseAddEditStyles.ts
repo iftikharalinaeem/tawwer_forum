1dd207e261e9beab86da8cf89b8ff56cbd1a7160
Adam Charron <adam@charrondev.com>
2020-02-28 12:47:21 -0500

    Implement universal form

--- plugins/knowledge/src/scripts/knowledge-settings/addEdit/knowledgeBaseAddEditStyles.ts
+++ plugins/knowledge/src/scripts/knowledge-settings/addEdit/knowledgeBaseAddEditStyles.ts
@@ -4,8 +4,11 @@
  */
 
 import { useThemeCache, styleFactory } from "@library/styles/styleUtils";
+import { globalVariables } from "@vanilla/library/src/scripts/styles/globalStyleVars";
+import { flexHelper } from "@vanilla/library/src/scripts/styles/styleHelpers";
 
 export const knowledgeBaseAddEditClasses = useThemeCache(() => {
+    const globalVars = globalVariables();
     const style = styleFactory("knowledgeBaseAddEdit");
 
     const heading = style("heading", {
@@ -17,5 +20,18 @@ export const knowledgeBaseAddEditClasses = useThemeCache(() => {
         },
     });
 
-    return { heading };
+    const searchContainer = style("searchContainer", {
+        ...flexHelper().middleLeft(),
+        justifyContent: "space-between",
+    });
+
+    const searchMeta = style("searchMeta", {});
+
+    const searchBar = style("searchBar", {
+        maxWidth: 600,
+        marginBottom: globalVars.gutter.size,
+        flex: 1,
+    });
+
+    return { heading, searchBar, searchContainer, searchMeta };
 });
