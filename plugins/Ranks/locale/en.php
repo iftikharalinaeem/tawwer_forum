2879846520e195ae6a4f71f18522c44c255320b0
Richard Flynn <richardflynn@gmail.com>
2020-03-17 17:04:49 -0400

    Create ranks locale file

---
+++ plugins/Ranks/locale/en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Richard Flynn <richard.flynn@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+$Definition['Recommend special ranks for admins and mods.'] =
+    'It\'s a good idea to have special ranks for moderators and administrators so that your community can easily see who\'s in charge.';
+$Definition['Recommend starting with five ranks.'] = 'You don\'t want to have too many ranks. We recommend starting with five. 
+    You can add more if your community is really large.';
