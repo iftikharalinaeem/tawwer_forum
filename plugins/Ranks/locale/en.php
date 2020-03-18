9d2ccc65fb1dc0f9130259176b7eb5d2eaf5d3ca
Richard Flynn <richard.flynn@vanillaforums.com>
2020-03-17 23:10:53 -0400

    Update plugins/Ranks/locale/en.php
    Co-Authored-By: Ryan <initvector@users.noreply.github.com>

--- plugins/Ranks/locale/en.php
+++ plugins/Ranks/locale/en.php
@@ -4,6 +4,7 @@
  * @copyright 2009-2020 Vanilla Forums Inc.
  * @license GPL-2.0-only
  */
+
 $Definition['Recommend special ranks for admins and mods.'] =
     'It\'s a good idea to have special ranks for moderators and administrators so that your community can easily see who\'s in charge.';
 $Definition['Recommend starting with five ranks.'] = 'You don\'t want to have too many ranks. We recommend starting with five. 
