774dfab735737ddab31890f905af4401cbe363fc
Richard Flynn <richardflynn@gmail.com>
2020-03-18 13:06:52 -0400

    Fit string onto one line

--- plugins/Ranks/locale/en.php
+++ plugins/Ranks/locale/en.php
@@ -7,5 +7,5 @@
 
 $Definition['Recommend special ranks for admins and mods.'] =
     'It\'s a good idea to have special ranks for moderators and administrators so that your community can easily see who\'s in charge.';
-$Definition['Recommend starting with five ranks.'] = 'You don\'t want to have too many ranks. We recommend starting with five. 
-    You can add more if your community is really large.';
+$Definition['Recommend starting with five ranks.'] =
+    'You don\'t want to have too many ranks. We recommend starting with five. You can add more if your community is really large.';
