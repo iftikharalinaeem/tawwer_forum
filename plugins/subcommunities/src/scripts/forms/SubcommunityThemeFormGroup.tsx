d5577b4744be10c112a91ab0d11d18dba472c37d
Chris Chabilall <chris.c@vanillaforums.com>
2020-03-23 12:47:23 -0400

    refactored types

--- plugins/subcommunities/src/scripts/forms/SubcommunityThemeFormGroup.tsx
+++ plugins/subcommunities/src/scripts/forms/SubcommunityThemeFormGroup.tsx
@@ -3,35 +3,23 @@
  * @license GPL-2.0-only
  */
 
-import { IComboBoxOption } from "@library/features/search/SearchBar";
-import SelectOne, { IMenuPlacement, MenuPlacement } from "@library/forms/select/SelectOne";
 import { t } from "@vanilla/i18n";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { ThemeChooserInput } from "@library/theming/ThemeChooserInput";
-import { DashboardFormGroup, useFormGroup } from "@dashboard/forms/DashboardFormGroup";
-import { useSubcommunities } from "@subcommunities/subcommunities/subcommunitySelectors";
-import { IFieldError, LoadStatus } from "@library/@types/api/core";
-import { DashboardSelect } from "@dashboard/forms/DashboardSelect";
-import { useThemeSettingsState } from "@library/theming/themeSettingsReducer";
-import { useThemeActions } from "@library/theming/ThemeActions";
-import { DashboardLabelType } from "@dashboard/forms/DashboardFormLabel";
-import Loader from "@library/loaders/Loader";
+import { DashboardFormGroup } from "@dashboard/forms/DashboardFormGroup";
 
-interface IProps extends IMenuPlacement {
+interface IProps {
     // Gdn_Form version (uncontrolled).
     initialValue: number | string | null | boolean; // Gdn_Form can give us some nasty values.
     formFieldName: string;
-
-    // Controlled react component
-    value?: string | string | null;
-    onChange?: (newValue: string | string | null) => void;
 }
 
 export function SubcommunityThemeFormGroup(props: IProps) {
     const { formFieldName, initialValue } = props;
-    const [value, setValue] = useState();
 
-    const setTheme = (selectedTheme: string | number | undefined | boolean) => {
+    const [value, setValue] = useState(initialValue && typeof initialValue !== "boolean" ? initialValue : null);
+
+    const setTheme = (selectedTheme: string | number | null) => {
         setValue(selectedTheme);
     };
 
@@ -39,7 +27,7 @@ export function SubcommunityThemeFormGroup(props: IProps) {
         <>
             <DashboardFormGroup label={t("Themes")} description={"Select a theme to be applied to this subcommunity."}>
                 <input name={formFieldName} type="hidden" value={value != null ? value : ""} />
-                <ThemeChooserInput initialValue={initialValue} onChange={setTheme} value={undefined} />
+                <ThemeChooserInput initialValue={value} onChange={setTheme} value={value} />
             </DashboardFormGroup>
         </>
     );
