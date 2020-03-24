e21bcccaa49f35d7aaeee3abe67c541c239cf181
dannimajdalani <dani.m@vanillaforums.com>
2020-03-24 17:18:40 -0400

    webhooks add-edit

---
+++ plugins/webhooks/src/scripts/WebhookAddEdit.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface IProps {
+    webhookID?: number;
+    onBack?: () => void;
+}
+
+export function WebhookAddEdit(props: IProps) {
+
+    const onBack = () => {
+        alert(1);
+        props.onBack();
+    };
+
+    return (
+        <>
+            hello from the form
+        </>
+    )
+}
---
+++ plugins/webhooks/src/scripts/WebhookDashboardHeaderBlock.tsx
@@ -0,0 +1,28 @@
+
+import React, { useState } from "react";
+import Button from "@library/forms/Button";
+import { ButtonTypes } from "@library/forms/buttonStyles";
+import { LeftChevronCompactIcon } from "@library/icons/common";
+
+interface IProps {
+    showBackLink?: boolean;
+    title: string;
+    actionButtons?: React.ReactNode;
+    onBack: () => void; 
+}
+
+export function WebhookDashboardHeaderBlock(props: IProps) {
+    return (
+        <header className="header-block">
+            <div className="title-block">
+                {props.showBackLink && (
+                    <Button baseClass={ButtonTypes.ICON} aria-label="Return" onClick={props.onBack}>
+                        <LeftChevronCompactIcon />
+                    </Button>
+                )}
+                <h1>{props.title}</h1>
+            </div>
+            {props.actionButtons}
+        </header>
+    );
+}
