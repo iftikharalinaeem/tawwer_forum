799ec9d2fc353c9f63054628a114b80464522897
Adam Charron <adam@charrondev.com>
2020-03-24 18:06:37 -0400

    Add help asset to manage themes page

--- plugins/themingapi/src/scripts/theming-ui-settings/ManageThemingPage.tsx
+++ plugins/themingapi/src/scripts/theming-ui-settings/ManageThemingPage.tsx
@@ -17,6 +17,8 @@ import { ThemeEditorRoute } from "@themingapi/routes/themeEditorRoutes";
 import { PlusIcon } from "@vanilla/library/src/scripts/icons/common";
 import { manageThemingClasses } from "@themingapi/theming-ui-settings/manageThemingStyles";
 import { themeItemClasses } from "@themingapi/theming-ui-settings/themeItemStyles";
+import { DashboardHelpAsset } from "@dashboard/forms/DashboardHelpAsset";
+import SmartLink from "@vanilla/library/src/scripts/routing/links/SmartLink";
 
 export default function ManageThemingPage(props) {
     const themeSettingsState = useThemeSettingsState();
@@ -28,8 +30,45 @@ export default function ManageThemingPage(props) {
         }
     });
 
+    const helpAsset = (
+        <DashboardHelpAsset>
+            <h3>{t("Heads Up!")}</h3>
+            <p>
+                {t(
+                    "Welcome to Vanilla's theming UI.",
+                    "Welcome to Vanilla's theming UI. This page lists all of your available themes, and allows you to copy or edit them.",
+                )}
+            </p>
+            <p>
+                {t(
+                    "Some older themes don't support full editing capability.",
+                    "Some older themes don't support full editing capability. To see what a theme supports you can hover over its name to see where edits will take effect.",
+                )}
+            </p>
+            <h3>{t("Need More Help?")}</h3>
+            <p>
+                <SmartLink to={"https://success.vanillaforums.com/kb/theme-guide"}>{t("Theming Guide")}</SmartLink>
+            </p>
+            <h3>{t("Old Theming UI")}</h3>
+            <p>
+                {t(
+                    "If you have an old theme",
+                    "If you have an old theme and need to set a separate desktop and mobile theme you can do so with the old theming UI.",
+                )}
+            </p>
+            <p>
+                <SmartLink to={"/settings/themes"}>{t("Old Theming UI")}</SmartLink>
+            </p>
+        </DashboardHelpAsset>
+    );
+
     if (!themeSettingsState.themes.data || themeSettingsState.themes.status === LoadStatus.LOADING) {
-        return <Loader />;
+        return (
+            <>
+                <Loader />
+                {helpAsset}
+            </>
+        );
     }
 
     const { currentTheme, templates, themes } = themeSettingsState.themes.data;
@@ -37,6 +76,7 @@ export default function ManageThemingPage(props) {
 
     return (
         <BrowserRouter>
+            {helpAsset}
             <CurrentTheme currentTheme={currentTheme || {}} />
             <div className="subheading-title">
                 <h2 className="subheading-title">{t("Templates")}</h2>
