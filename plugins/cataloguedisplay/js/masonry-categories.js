6571d37fefd5dbd4f4772234347a4955cc3911d7
Raphael BERGINA <raphael.bergina@vanillaforums.com>
2020-03-27 11:25:41 -0400

    Update css to look nice Update code standard

--- plugins/cataloguedisplay/js/masonry-categories.js
+++ plugins/cataloguedisplay/js/masonry-categories.js
@@ -1,13 +1,12 @@
-;(function ($, window, document, undefined) {
-
-  $(function() {
-    $('.DataList.Discussions').imagesLoaded(function($images, $proper, $broken) {
-      $('.placeholder-image', this).animate({ opacity: 1 });
-      this.masonry({
-        itemSelector: '.Item.ItemDiscussion',
-        animate: true,
-      });
+;(function($, window, document, undefined) {
+    $(function() {
+        $(".DataList.Discussions").imagesLoaded(function($images, $proper, $broken) {
+            $(".placeholder-image", this).animate({ opacity: 1 });
+            this.masonry({
+                itemSelector: ".Item.ItemDiscussion",
+                animate: true,
+                glutter: 10
+            });
+        });
     });
-  });
-
 })(window.jQuery, window, document);
