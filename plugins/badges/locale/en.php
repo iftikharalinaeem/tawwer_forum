e271ae432440021169954d72967e70bfe0427667
Richard Flynn <richardflynn@gmail.com>
2020-03-30 20:59:10 -0400

    add locale dir and en file

---
+++ plugins/badges/locale/en.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author Richard Flynn <richard.flynn@vanillaforums.com>
+ * @copyright 2009-2020 Vanilla Forums Inc.
+ * @license GPL-2.0-only
+ */
+
+$Definition['Thanks for sticking with us.'] = 'Thanks for sticking with us for';
