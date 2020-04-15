8610b521030833e0eed664d3e5dd3350544cb689
Chris Chabilall <chris.c@vanillaforums.com>
2020-04-15 11:38:48 -0400

    add styles

--- plugins/knowledge/src/scripts/navigation/DropDownNavPanelPlaceholder.tsx
+++ plugins/knowledge/src/scripts/navigation/DropDownNavPanelPlaceholder.tsx
@@ -5,21 +5,26 @@
 
 import React from "react";
 import { LoadingRectange, LoadingSpacer } from "@vanilla/library/src/scripts/loaders/LoadingRectangle";
+import { dropDownClasses } from "@library/flyouts/dropDownStyles";
+import classNames from "classnames";
 
 export function DropDownNavPanelPlaceholder() {
+    const classes = dropDownClasses();
     return (
-        <div style={{ padding: "10px" }}>
-            <LoadingRectange height={1} width={"100%"} />
-            <LoadingSpacer height={18} />
-            <LoadingRectange height={12} width={"75%"} />
-            <LoadingSpacer height={18} />
-            <LoadingRectange height={12} width={"70%"} />
-            <LoadingSpacer height={18} />
-            <LoadingRectange height={12} width={"80%"} />
-            <LoadingSpacer height={18} />
-            <LoadingRectange height={12} width={"80%"} />
-            <LoadingSpacer height={18} />
-            <LoadingRectange height={12} width={"80%"} />
+        <div className={classes.panelContent}>
+            <LoadingRectange height={0.5} width={"100%"} />
+            <div className={classNames(classes.paddedFrame, classes.items)}>
+                <LoadingSpacer height={18} />
+                <LoadingRectange height={12} width={"75%"} />
+                <LoadingSpacer height={18} />
+                <LoadingRectange height={12} width={"70%"} />
+                <LoadingSpacer height={18} />
+                <LoadingRectange height={12} width={"80%"} />
+                <LoadingSpacer height={18} />
+                <LoadingRectange height={12} width={"80%"} />
+                <LoadingSpacer height={18} />
+                <LoadingRectange height={12} width={"80%"} />
+            </div>
         </div>
     );
 }
