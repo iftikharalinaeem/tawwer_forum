2551f6cec1aa935a843ac7c6ec4735f4ac99d9ac
Chris Chabilall <chris.c@vanillaforums.com>
2020-04-23 20:50:58 -0400

    use state to get revisions

--- plugins/themingapi/src/scripts/theme/ThemeRevisionsPanel.tsx
+++ plugins/themingapi/src/scripts/theme/ThemeRevisionsPanel.tsx
@@ -3,11 +3,30 @@
  * @license Proprietary
  */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { themeBuilderClasses } from "@library/forms/themeEditor/ThemeBuilder.styles";
 import { ThemeBuilderSectionGroup } from "@library/forms/themeEditor/ThemeBuilderSectionGroup";
+import { useGetThemeState } from "@library/theming/themeReducer";
+import { useThemeActions } from "@library/theming/ThemeActions";
+import { LoadStatus } from "@library/@types/api/core";
 
-export function ThemeRevisionsPanel() {
+export interface IProps {
+    themeID: number;
+}
+
+export function ThemeRevisionsPanel(props: IProps) {
+    const themeState = useGetThemeState();
+    const actions = useThemeActions();
+
+    useEffect(() => {
+        if (themeState.themeRevisions.status === LoadStatus.PENDING) {
+            actions.getThemeRevisions(props.themeID);
+        }
+    }, [themeState]);
+
+    const revisions = themeState.themeRevisions?.data;
+    console.log(revisions);
+
     const classes = themeBuilderClasses();
     return (
         <>
