a52d5dce82d5dc5746b4d5993f542e9a6b49c532
dannimajdalani <dani.m@vanillaforums.com>
2020-04-29 15:18:00 -0400

    Fix polls options dropdown

--- plugins/Polls/class.polls.plugin.php
+++ plugins/Polls/class.polls.plugin.php
@@ -296,6 +296,7 @@ class PollsPlugin extends Gdn_Plugin {
     public function postController_afterForms_handler($sender) {
         $forms = $sender->data('Forms');
         $forms[] = ['Name' => 'Poll', 'Label' => sprite('SpPoll').t('New Poll'), 'Url' => 'post/poll'];
+        $sender->addJsFile('polls.js', 'plugins/Polls');
         $sender->setData('Forms', $forms);
     }
 
