62dd8c18d2347b5bb3bdab70b5fc3e6d7784c689
igraziatto <isis.graziatto@gmail.com>
2020-06-01 12:00:52 -0400

    add settings

--- plugins/private-discussions/PrivateDiscussionsPlugin.php
+++ plugins/private-discussions/PrivateDiscussionsPlugin.php
@@ -6,6 +6,8 @@
 
 namespace Vanilla\Plugins\PrivateDiscussions;
 
+use Vanilla\Contracts\ConfigurationInterface;
+
 /**
  * Class PrivateDiscussionsPlugin
  *
@@ -16,12 +18,35 @@ class PrivateDiscussionsPlugin extends \Gdn_Plugin {
     /** @var string */
     const ADDON_PATH = 'plugins/private-discussions';
 
+    /** @var int */
+    const WORDCOUNT_DEFAULT = 100;
+
+    /** @var bool */
+    const STRIPEMBEDS_DEFAULT = false;
+
     /**
      * PrivateDiscussionsPlugin constructor.
-     *
      */
-    public function __construct() {
+    public function __construct(ConfigurationInterface $configuration) {
         parent::__construct();
+        $this->config = $configuration;
+    }
+
+    /**
+     * Return StripEmbeds
+     * @return mixed
+     */
+    public function getStripEmbeds() {
+        return $this->config->get('Plugins.PrivateDiscussions.StripEmbeds', self::STRIPEMBEDS_DEFAULT);
+    }
+
+    /**
+     * Return WordCount
+     *
+     * @return mixed
+     */
+    public function getWordCount() {
+        return $this->config->get('Plugins.PrivateDiscussions.WordCount', self::WORDCOUNT_DEFAULT);
     }
 
     /**
@@ -32,5 +57,26 @@ class PrivateDiscussionsPlugin extends \Gdn_Plugin {
     public function settingsController_privatediscussions_create(\Gdn_Controller $sender) {
         $sender->permission('Garden.Settings.Manage');
         $sender->setData('Title', t('Private Discussions Settings'));
+
+        if ($sender->Form->authenticatedPostBack()) {
+            $sender->Form->validateRule('Plugins.PrivateDiscussions.WordCount', 'function:ValidateInteger', 'Word Count must be numeric');
+            $sender->Form->validateRule('Plugins.PrivateDiscussions.WordCount', 'function:ValidateRequired', 'Word Count is required');
+        }
+
+        $configurationModule = new \ConfigurationModule($sender);
+        $configurationModule->initialize([
+            'Plugins.PrivateDiscussions.StripEmbeds' => [
+                'LabelCode' => 'Strip Embeds',
+                'Control' => 'Toggle',
+                'Default' => self::STRIPEMBEDS_DEFAULT
+            ],
+            'Plugins.PrivateDiscussions.WordCount' => [
+                'LabelCode' => 'Word Count',
+                'Control' => 'TextBox',
+                'Default' => self::WORDCOUNT_DEFAULT
+            ]
+        ]);
+
+        $configurationModule->renderAll();
     }
 }
