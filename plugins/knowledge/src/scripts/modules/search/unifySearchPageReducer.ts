a547b2e085b37ba8afbaaa34eacc260e9bdd70ab
Adam Charron <adam@charrondev.com>
2020-06-19 13:25:44 -0400

    Fix result type

--- plugins/knowledge/src/scripts/modules/search/unifySearchPageReducer.ts
+++ plugins/knowledge/src/scripts/modules/search/unifySearchPageReducer.ts
@@ -47,7 +47,7 @@ export interface IUnifySearchFormState {
 
 export interface IUnifySearchPageState {
     form: IUnifySearchFormState;
-    results: ILoadable<IUnifySearchResponseBody>;
+    results: ILoadable<IUnifySearchResponseBody[]>;
     pages: ILinkPages;
 }
 
