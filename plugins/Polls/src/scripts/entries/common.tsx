9d651142be8f22819653b842eb0a56c54b69d985
Adam Charron <adam@charrondev.com>
2020-06-22 12:43:13 -0400

    CommunityPostTypeFilter

--- plugins/Polls/src/scripts/entries/common.tsx
+++ plugins/Polls/src/scripts/entries/common.tsx
@@ -3,9 +3,11 @@
  * @license GPL-2.0-only
  */
 
+import React from "react";
 import { onReady, t } from "@vanilla/library/src/scripts/utility/appUtils";
 import { SearchFormContextProvider } from "@vanilla/library/src/scripts/search/SearchFormContext";
 import { TypePollsIcon } from "@vanilla/library/src/scripts/icons/searchIcons";
+import { CommunityPostTypeFilter } from "@vanilla/addon-vanilla/search/CommunityPostTypeFilter";
 
 onReady(() => {
     SearchFormContextProvider.addSubType({
@@ -13,5 +15,10 @@ onReady(() => {
         icon: <TypePollsIcon />,
         recordType: "poll",
         type: "poll",
+    });
+
+    CommunityPostTypeFilter.addPostType({
+        label: t("Polls"),
+        values: ["poll"],
     });
 });
