d18fd9359618074ac04ca14d28eba7abd1479c6a
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2020-07-06 07:47:51 -0400

    Refactored article TOC to functionnal component

--- cloud/plugins/knowledge/src/scripts/modules/article/components/ArticleTOC.tsx
+++ cloud/plugins/knowledge/src/scripts/modules/article/components/ArticleTOC.tsx
@@ -11,8 +11,9 @@ import classNames from "classnames";
 import { IOutlineItem } from "@knowledge/@types/api/article";
 import { panelListClasses } from "@library/layout/panelListStyles";
 import { articleTOCClasses } from "@knowledge/modules/article/components/articleTOCStyles";
-import { uniqueIDFromPrefix } from "@library/utility/idUtils";
+import { useUniqueID } from "@library/utility/idUtils";
 import { useLayout } from "@library/layout/LayoutContext";
+import { useEffect, useState } from "react";
 
 interface IProps {
     items: IOutlineItem[];
@@ -21,74 +22,60 @@ interface IProps {
 /**
  * Implements the table of contents component
  */
-export default class ArticleTOC extends React.Component<IProps> {
-    private static readonly MINIMUM_CHILD_COUNT = 1;
+export default function ArticleTOC(props: IProps) {
+    const MINIMUM_CHILD_COUNT = 1;
+    const [rerender, setRerender] = useState(false);
+    const { mediaQueries } = useLayout();
+    const titleID = useUniqueID("articleTOC");
 
-    public render() {
-        if (this.props.items.length < ArticleTOC.MINIMUM_CHILD_COUNT) {
-            return null;
-        }
-        const classes = articleTOCClasses();
-        const classesPanelList = panelListClasses(useLayout().mediaQueries);
-        const contents = this.props.items
-            .filter(item => item.level === 2)
-            .map(item => {
-                const href = "#" + item.ref;
-                const isActive = window.location.hash === href;
-                return (
-                    <li
-                        className={classNames("panelList-item", classesPanelList.item, classes.item, {
-                            isActive,
-                        })}
-                        key={item.ref}
-                    >
-                        <a href={href} onClick={this.forceHashChange} className={classes.link} title={item.text}>
-                            {item.text}
-                        </a>
-                    </li>
-                );
-            });
-        const titleID = uniqueIDFromPrefix("articleTOC");
-
-        return (
-            <nav
-                aria-labelledby={titleID}
-                className={classNames("panelList", "tableOfContents", classesPanelList.root)}
-            >
-                <Heading
-                    id={titleID}
-                    title={t("On This Page")}
-                    className={classNames(classesPanelList.title, "panelList-title", "tableOfContents-title")}
-                />
-                <ul className={classNames("panelList-items", classesPanelList.items)}>{contents}</ul>
-            </nav>
-        );
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public componentDidMount() {
-        window.addEventListener("hashchange", this.handleHashChange);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public componentWillUnmount() {
-        window.removeEventListener("hashchange", this.handleHashChange);
-    }
-
-    private handleHashChange = () => {
-        this.forceUpdate();
+    const handleHashChange = () => {
+        setRerender(!rerender);
     };
 
-    /**
-     * Force a hash change event to occur.
-     *
-     * This is so that clicking a hash link __always__ results in scrolling to that link.
-     */
-    private forceHashChange = () => {
-        window.dispatchEvent(new Event("hashchange"));
-    };
+    useEffect(() => {
+        window.addEventListener("hashchange", handleHashChange);
+        return () => {
+            window.removeEventListener("hashchange", handleHashChange);
+        };
+    }, [handleHashChange]);
+
+    if (props.items.length < MINIMUM_CHILD_COUNT) {
+        return null;
+    }
+    const classes = articleTOCClasses();
+    const classesPanelList = panelListClasses(mediaQueries);
+
+    const contents = props.items
+        .filter(item => item.level === 2)
+        .map(item => {
+            const href = "#" + item.ref;
+            const isActive = window.location.hash === href;
+            return (
+                <li
+                    className={classNames("panelList-item", classesPanelList.item, classes.item, {
+                        isActive,
+                    })}
+                    key={item.ref}
+                >
+                    <a href={href} onClick={handleHashChange} className={classes.link} title={item.text}>
+                        {item.text}
+                    </a>
+                </li>
+            );
+        });
+
+    return (
+        <nav
+            key={rerender ? 1 : 0}
+            aria-labelledby={titleID}
+            className={classNames("panelList", "tableOfContents", classesPanelList.root)}
+        >
+            <Heading
+                id={titleID}
+                title={t("On This Page")}
+                className={classNames(classesPanelList.title, "panelList-title", "tableOfContents-title")}
+            />
+            <ul className={classNames("panelList-items", classesPanelList.items)}>{contents}</ul>
+        </nav>
+    );
 }
