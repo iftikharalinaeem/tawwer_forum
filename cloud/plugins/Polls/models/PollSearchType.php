35951234bd85f934fa40f94787d62228635b5964
Chris Chabilall <chris.c@vanillaforums.com>
2020-07-07 15:54:46 -0400

    remove unescessay method

--- cloud/plugins/Polls/models/PollSearchType.php
+++ cloud/plugins/Polls/models/PollSearchType.php
@@ -6,12 +6,8 @@
 
 namespace Vanilla\Polls\Models;
 
-use Garden\Web\Exception\HttpException;
-use Vanilla\Forum\Navigation\ForumCategoryRecordType;
 use Vanilla\Forum\Search\DiscussionSearchType;
 use Vanilla\Navigation\BreadcrumbModel;
-use Vanilla\Search\SearchResultItem;
-use Vanilla\Utility\ArrayUtils;
 
 /**
  * Class PollSearchType
@@ -70,32 +66,5 @@ class PollSearchType extends DiscussionSearchType {
      */
     public function getType(): string {
         return 'poll';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getResultItems(array $recordIDs): array {
-        try {
-            $results = $this->discussionsApi->index([
-                'discussionID' => implode(",", $recordIDs),
-                'limit' => 100,
-            ]);
-            $results = $results->getData();
-
-            $resultItems = array_map(function ($result) {
-                $mapped = ArrayUtils::remapProperties($result, [
-                    'recordID' => 'discussionID',
-                ]);
-                $mapped['recordType'] = $this->getSearchGroup();
-                $mapped['type'] = $this->getType();
-                $mapped['breadcrumbs'] = $this->breadcrumbModel->getForRecord(new ForumCategoryRecordType($mapped['categoryID']));
-                return new SearchResultItem($mapped);
-            }, $results);
-            return $resultItems;
-        } catch (HttpException $exception) {
-            trigger_error($exception->getMessage(), E_USER_WARNING);
-            return [];
-        }
     }
 }
